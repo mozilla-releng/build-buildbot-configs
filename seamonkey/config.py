2a78487fa3e0c6c81467b7008d129eb46fc539c3
Robert Kaiser <kairo@kairo.at>
2008-07-22 14:04:37 +0200

    switch config to comm-central, turn on pollers, turn off -save-temps on Mac

--- seamonkey/config.py
+++ seamonkey/config.py
@@ -1,6 +1,7 @@
-HGURL = 'http://hg.mozilla.org/users/kairo_kairo.at/'
+HGURL = 'http://hg.mozilla.org/'
 # for chatzilla/venkman
 CVSROOT = ':ext:seabld@cvs.mozilla.org:/cvsroot'
+ADD_POLL_BRANCH = 'mozilla-central'
 CONFIG_REPO_URL = 'http://hg.mozilla.org/build/buildbot-configs'
 CONFIG_SUBDIR = 'seamonkey'
 OBJDIR = 'objdir'
@@ -16,52 +17,52 @@ DOWNLOAD_BASE_URL = 'http://ftp.mozilla.org/pub/mozilla.org/seamonkey'
 
 # All branches that are to be built MUST be listed here.
 BRANCHES = {
-    'calemaisu-test': {}
+    'comm-central': {}
 }
 
 ######## seamonkey-hg
 # All platforms being built for this branch MUST be listed here.
-BRANCHES['calemaisu-test']['platforms'] = {
+BRANCHES['comm-central']['platforms'] = {
     'linux': {},
     'win32': {},
     'macosx': {}
 }
-BRANCHES['calemaisu-test']['platforms']['linux']['base_name'] = 'Linux sm-hg-test'
-BRANCHES['calemaisu-test']['platforms']['win32']['base_name'] = 'Win2k3 sm-hg-test'
-BRANCHES['calemaisu-test']['platforms']['macosx']['base_name'] = 'MacOSX 10.4 sm-hg-test'
-BRANCHES['calemaisu-test']['platforms']['linux']['profiled_build'] = False
-BRANCHES['calemaisu-test']['platforms']['win32']['profiled_build'] = False
-BRANCHES['calemaisu-test']['platforms']['macosx']['profiled_build'] = False
+BRANCHES['comm-central']['platforms']['linux']['base_name'] = 'Linux comm-central'
+BRANCHES['comm-central']['platforms']['win32']['base_name'] = 'Win2k3 comm-central'
+BRANCHES['comm-central']['platforms']['macosx']['base_name'] = 'MacOSX 10.4 comm-central'
+BRANCHES['comm-central']['platforms']['linux']['profiled_build'] = False
+BRANCHES['comm-central']['platforms']['win32']['profiled_build'] = False
+BRANCHES['comm-central']['platforms']['macosx']['profiled_build'] = False
 # If True, a complete update snippet for this branch will be generated and
 # uploaded to. Any platforms with 'debug' in them will not have snippets
 # generated.
-BRANCHES['calemaisu-test']['create_snippet'] = True
-BRANCHES['calemaisu-test']['aus2_base_upload_dir'] = '/opt/aus2/build/0/SeaMonkey/hg-test'
-BRANCHES['calemaisu-test']['platforms']['linux']['update_platform'] = 'Linux_x86-gcc3'
-BRANCHES['calemaisu-test']['platforms']['win32']['update_platform'] = 'WINNT_x86-msvc'
-BRANCHES['calemaisu-test']['platforms']['macosx']['update_platform'] = 'Darwin_Universal-gcc3'
+BRANCHES['comm-central']['create_snippet'] = True
+BRANCHES['comm-central']['aus2_base_upload_dir'] = '/opt/aus2/build/0/SeaMonkey/trunk'
+BRANCHES['comm-central']['platforms']['linux']['update_platform'] = 'Linux_x86-gcc3'
+BRANCHES['comm-central']['platforms']['win32']['update_platform'] = 'WINNT_x86-msvc'
+BRANCHES['comm-central']['platforms']['macosx']['update_platform'] = 'Darwin_Universal-gcc3'
 # If True, 'make buildsymbols' and 'make uploadsymbols' will be run
 # SYMBOL_SERVER_* variables are setup in the environment section below
-BRANCHES['calemaisu-test']['platforms']['linux']['upload_symbols'] = True
-BRANCHES['calemaisu-test']['platforms']['win32']['upload_symbols'] = True
-BRANCHES['calemaisu-test']['platforms']['macosx']['upload_symbols'] = True
-BRANCHES['calemaisu-test']['tinderbox_tree'] = 'SeaMonkey-Ports'
-BRANCHES['calemaisu-test']['platforms']['linux']['slaves'] = [
+BRANCHES['comm-central']['platforms']['linux']['upload_symbols'] = True
+BRANCHES['comm-central']['platforms']['win32']['upload_symbols'] = True
+BRANCHES['comm-central']['platforms']['macosx']['upload_symbols'] = True
+BRANCHES['comm-central']['tinderbox_tree'] = 'SeaMonkey'
+BRANCHES['comm-central']['platforms']['linux']['slaves'] = [
     'cb-sea-linux-tbox'
 ]
-BRANCHES['calemaisu-test']['platforms']['win32']['slaves'] = [
+BRANCHES['comm-central']['platforms']['win32']['slaves'] = [
     'cb-sea-win32-tbox'
 ]
-BRANCHES['calemaisu-test']['platforms']['macosx']['slaves'] = [
+BRANCHES['comm-central']['platforms']['macosx']['slaves'] = [
     'cb-xserve02'
 ]
 # This is used in a bunch of places where something needs to be run from
 # the objdir. This is necessary because of universal builds on Mac
 # creating subdirectories inside of the objdir.
-BRANCHES['calemaisu-test']['platforms']['linux']['platform_objdir'] = OBJDIR
-BRANCHES['calemaisu-test']['platforms']['win32']['platform_objdir'] = OBJDIR
-BRANCHES['calemaisu-test']['platforms']['macosx']['platform_objdir'] = '%s/ppc' % OBJDIR
-BRANCHES['calemaisu-test']['platforms']['linux']['env'] = {
+BRANCHES['comm-central']['platforms']['linux']['platform_objdir'] = OBJDIR
+BRANCHES['comm-central']['platforms']['win32']['platform_objdir'] = OBJDIR
+BRANCHES['comm-central']['platforms']['macosx']['platform_objdir'] = '%s/ppc' % OBJDIR
+BRANCHES['comm-central']['platforms']['linux']['env'] = {
     'MOZ_OBJDIR': OBJDIR,
     'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
     'SYMBOL_SERVER_USER': 'seabld',
@@ -69,7 +70,7 @@ BRANCHES['calemaisu-test']['platforms']['linux']['env'] = {
     'SYMBOL_SERVER_SSH_KEY': "/home/seabld/.ssh/seabld_dsa",
     'TINDERBOX_OUTPUT': '1'
 }
-BRANCHES['calemaisu-test']['platforms']['win32']['env'] = {'CVS_RSH': 'ssh',
+BRANCHES['comm-central']['platforms']['win32']['env'] = {'CVS_RSH': 'ssh',
     'MOZ_OBJDIR': OBJDIR,
     'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
     'SYMBOL_SERVER_USER': 'seabld',
@@ -77,7 +78,7 @@ BRANCHES['calemaisu-test']['platforms']['win32']['env'] = {'CVS_RSH': 'ssh',
     'SYMBOL_SERVER_SSH_KEY': "/c/Documents and Settings/seabld/.ssh/seabld_dsa",
     'TINDERBOX_OUTPUT': '1'
 }
-BRANCHES['calemaisu-test']['platforms']['macosx']['env'] = {
+BRANCHES['comm-central']['platforms']['macosx']['env'] = {
     'MOZ_OBJDIR': OBJDIR,
     'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
     'SYMBOL_SERVER_USER': 'seabld',
