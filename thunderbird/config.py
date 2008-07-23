974d29f8d6482ec8bb05e91c3bfad4a9fe22d6e6
Philippe M. Chiasson <gozer@mozillamessaging.com>
2008-07-23 15:22:15 -0400

    Copy from seamonkey's nightly builders to get us started. Keep actual bit publishing steps disabled.

---
+++ thunderbird/config.py
@@ -0,0 +1,87 @@
+HGURL = 'http://hg.mozilla.org/'
+ADD_POLL_BRANCH = 'mozilla-central'
+CONFIG_REPO_URL = 'http://hg.mozilla.org/build/buildbot-configs'
+CVSROOT = ':ext:tbirdbld@cvs.mozilla.org:/cvsroot'
+CONFIG_SUBDIR = 'thunderbird'
+OBJDIR = 'objdir-tb'
+STAGE_USERNAME = 'tbirdbld'
+STAGE_SERVER = 'stage.mozilla.org'
+STAGE_BASE_PATH = '/home/ftp/pub/thunderbird'
+STAGE_GROUP = 'thunderbird'
+STAGE_SSH_KEY = 'tbirdbld_dsa'
+AUS2_USER = 'tbirdbld'
+AUS2_HOST = 'aus2-community.mozilla.org'
+DOWNLOAD_BASE_URL = 'http://ftp.mozilla.org/pub/mozilla.org/thunderbird'
+
+
+# All branches that are to be built MUST be listed here.
+BRANCHES = {
+    'comm-central': {}
+}
+
+######## thunderbird-hg
+# All platforms being built for this branch MUST be listed here.
+BRANCHES['comm-central']['platforms'] = {
+    'linux': {},
+    'win32': {},
+    'macosx': {}
+}
+BRANCHES['comm-central']['platforms']['linux']['base_name'] = 'Linux comm-central'
+BRANCHES['comm-central']['platforms']['win32']['base_name'] = 'Win2k3 comm-central'
+BRANCHES['comm-central']['platforms']['macosx']['base_name'] = 'MacOSX 10.4 comm-central'
+BRANCHES['comm-central']['platforms']['linux']['profiled_build'] = False
+BRANCHES['comm-central']['platforms']['win32']['profiled_build'] = False
+BRANCHES['comm-central']['platforms']['macosx']['profiled_build'] = False
+# If True, a complete update snippet for this branch will be generated and
+# uploaded to. Any platforms with 'debug' in them will not have snippets
+# generated.
+BRANCHES['comm-central']['create_snippet'] = True
+BRANCHES['comm-central']['aus2_base_upload_dir'] = '/opt/aus2/build/0/SeaMonkey/trunk'
+BRANCHES['comm-central']['platforms']['linux']['update_platform'] = 'Linux_x86-gcc3'
+BRANCHES['comm-central']['platforms']['win32']['update_platform'] = 'WINNT_x86-msvc'
+BRANCHES['comm-central']['platforms']['macosx']['update_platform'] = 'Darwin_Universal-gcc3'
+# If True, 'make buildsymbols' and 'make uploadsymbols' will be run
+# SYMBOL_SERVER_* variables are setup in the environment section below
+BRANCHES['comm-central']['platforms']['linux']['upload_symbols'] = True
+BRANCHES['comm-central']['platforms']['win32']['upload_symbols'] = True
+BRANCHES['comm-central']['platforms']['macosx']['upload_symbols'] = True
+BRANCHES['comm-central']['tinderbox_tree'] = 'Thunderbird'
+BRANCHES['comm-central']['platforms']['linux']['slaves'] = [
+    'tb-linux-tbox',
+]
+BRANCHES['comm-central']['platforms']['win32']['slaves'] = [
+    'tbnewref-win32-tbox'
+]
+BRANCHES['comm-central']['platforms']['macosx']['slaves'] = [
+    'bm-xserve07'
+]
+# This is used in a bunch of places where something needs to be run from
+# the objdir. This is necessary because of universal builds on Mac
+# creating subdirectories inside of the objdir.
+BRANCHES['comm-central']['platforms']['linux']['platform_objdir'] = OBJDIR
+BRANCHES['comm-central']['platforms']['win32']['platform_objdir'] = OBJDIR
+BRANCHES['comm-central']['platforms']['macosx']['platform_objdir'] = '%s/ppc' % OBJDIR
+BRANCHES['comm-central']['platforms']['linux']['env'] = {
+    'MOZ_OBJDIR': OBJDIR,
+    'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
+    'SYMBOL_SERVER_USER': 'tbirdbld',
+    'SYMBOL_SERVER_PATH': '/mnt/netapp/breakpad/symbols_tbrd/',
+    'SYMBOL_SERVER_SSH_KEY': "/home/tbirdbld/.ssh/tbirdbld_dsa",
+    'TINDERBOX_OUTPUT': '1'
+}
+BRANCHES['comm-central']['platforms']['win32']['env'] = {'CVS_RSH': 'ssh',
+    'MOZ_OBJDIR': OBJDIR,
+    'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
+    'SYMBOL_SERVER_USER': 'tbirdbld',
+    'SYMBOL_SERVER_PATH': '/mnt/netapp/breakpad/symbols_tbrd/',
+    'SYMBOL_SERVER_SSH_KEY': "/c/Documents and Settings/tbirdbld/.ssh/tbirdbld_dsa",
+    'TINDERBOX_OUTPUT': '1'
+}
+BRANCHES['comm-central']['platforms']['macosx']['env'] = {
+    'MOZ_OBJDIR': OBJDIR,
+    'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
+    'SYMBOL_SERVER_USER': 'tbirdbld',
+    'SYMBOL_SERVER_PATH': '/mnt/netapp/breakpad/symbols_tbrd/',
+    'SYMBOL_SERVER_SSH_KEY': "/Users/tbirdbld/.ssh/tbirdbld_dsa",
+    'TINDERBOX_OUTPUT': '1'
+}
