39e103aae56a6cf14a373f5534652b69b05fe19f
Philippe M. Chiasson <gozer@mozillamessaging.com>
2008-09-09 15:32:20 -0400

    make mozconfig name configurable per branch

--- thunderbird/config.py
+++ thunderbird/config.py
@@ -31,6 +31,7 @@ BRANCHES['comm-central']['platforms'] = {
 
 BRANCHES['comm-central']['client_py_args'] = ['--skip-comm', '--skip-chatzilla', '--skip-venkman', '--skip-calendar']
 BRANCHES['comm-central']['cvsroot'] = ':ext:tbirdbld@cvs.mozilla.org:/cvsroot'
+BRANCHES['comm-central']['mozconfig'] = 'mozconfig'
 BRANCHES['comm-central']['platforms']['linux']['base_name'] = 'Linux comm-central'
 BRANCHES['comm-central']['platforms']['win32']['base_name'] = 'Win2k3 comm-central'
 BRANCHES['comm-central']['platforms']['macosx']['base_name'] = 'MacOSX 10.4 comm-central'
