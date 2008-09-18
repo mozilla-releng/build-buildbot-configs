4b7b4c551334840459da153298724a4340890349
Ben Hearsum <bhearsum@mozilla.com>
2008-09-18 08:24:34 -0400

    bug 454205: updates/fix to staging release config after first test-run. r=nrthomas, patch=me

--- mozilla2-staging/release_master.py
+++ mozilla2-staging/release_master.py
@@ -7,11 +7,13 @@ reload(buildbotcustom.misc)
 reload(buildbotcustom.process.factory)
 
 from buildbotcustom.misc import get_l10n_repositories
-from buildbotcustom.process.factory import ReleaseTaggingFactory, \
-  SingleSourceFactory
+from buildbotcustom.process.factory import StagingRepositorySetupFactory, \
+  ReleaseTaggingFactory, SingleSourceFactory
 
 # this is where all of our important configuration is stored. build number,
 # version number, sign-off revisions, etc.
+import release_config
+reload(release_config)
 from release_config import *
 
 builders = []
@@ -22,11 +24,16 @@ status = []
 ##### Change sources and Schedulers
 change_source.append(PBChangeSource())
 
-tag_scheduler = Scheduler(
-    name='tag',
+repo_setup_scheduler = Scheduler(
+    name='repo_setup',
     branch='release',
     treeStableTimer=0,
-    builderNames=['tag'],
+    builderNames=['repo_setup']
+)
+tag_scheduler = Dependent(
+    name='tag',
+    upstream=repo_setup_scheduler,
+    builderNames=['tag']
 )
 build_scheduler = Dependent(
     name='build',
@@ -34,6 +41,7 @@ build_scheduler = Dependent(
     builderNames=['source']
 )
 
+schedulers.append(repo_setup_scheduler)
 schedulers.append(tag_scheduler)
 schedulers.append(build_scheduler)
 
@@ -50,6 +58,22 @@ l10n_repos = get_l10n_repositories(l10nRevisionFile, l10nCentral,
                                    relbranchOverride)
 repositories.update(l10n_repos)
 
+repository_setup_factory = StagingRepositorySetupFactory(
+    hgHost=hgHost,
+    username=hgUsername,
+    sshKey=hgSshKey,
+    repositories=repositories
+)
+
+builders.append({
+    'name': 'repo_setup',
+    'slavenames': ['moz2-linux-slave04'],
+    'category': 'release',
+    'builddir': 'repo_setup',
+    'factory': repository_setup_factory
+})
+
+
 tag_factory = ReleaseTaggingFactory(
     repositories=repositories,
     buildToolsRepo=buildTools,
@@ -58,7 +82,9 @@ tag_factory = ReleaseTaggingFactory(
     appVersion=appVersion,
     milestone=milestone,
     baseTag=baseTag,
-    buildNumber=buildNumber
+    buildNumber=buildNumber,
+    hgUsername=hgUsername,
+    hgSshKey=hgSshKey
 )
 
 builders.append({
