83b2cb1f983ca48df4ca48a51ca2e5b03fc021f7
Ben Hearsum <bhearsum@mozilla.com>
2008-11-03 13:06:09 -0500

    bug 460791: enable l10n repacks on Firefox 3.1 (in staging). r=ccooper, patch=armenzg

---
+++ mozilla2-staging/l10n_config.py
@@ -0,0 +1,5 @@
+#make sure it has an ending slash
+uploadPath     = '/home/ftp/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n/'
+enUS_binaryURL = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central'
+allLocalesURL  = 'http://hg.mozilla.org/mozilla-central/index.cgi/raw-file/tip/browser/locales/all-locales'
+ftpserver      = 'staging-stage.build.mozilla.org'
---
+++ mozilla2-staging/l10n_master.py
@@ -0,0 +1,105 @@
+# -*- python -*-
+# ex: set syntax=python:
+
+#configuration file for l10n
+import l10n_config
+reload(l10n_config)
+from l10n_config import *
+
+# l10n logic
+import buildbotcustom.l10n
+reload(buildbotcustom.l10n)
+
+builders = []
+
+# custom classes used
+from buildbotcustom.l10n.l10n import BuildL10n
+
+from buildbot.process import factory
+from buildbot.steps.shell import ShellCommand, Compile, WithProperties
+
+#### Factory
+BRANCH = 'mozilla-central' 
+PROJECT = 'firefox'
+
+L10nNightlyFactory = factory.BuildFactory()
+#We associate the custom-made BuildL10n class to the factory to allow paralelization
+L10nNightlyFactory.buildClass = BuildL10n
+L10nNightlyFactory.addStep(ShellCommand,
+# This will remove the folder from which we upload the packages that are generated
+    command=['rm','-rf', BRANCH+'/dist/upload'],
+    workdir=".",
+    haltOnFailure = True)
+L10nNightlyFactory.addStep(ShellCommand,
+    command = ['sh', '-c', 'mkdir', '-p', 'l10n-central'],
+    descriptionDone="mkdir l10n-central",
+    workdir="."
+)
+L10nNightlyFactory.addStep(ShellCommand,
+    command = ['sh', '-c',
+                 'if [ -d '+BRANCH+' ]; then ' +
+                 'hg -R '+BRANCH+' pull -r tip ; ' +
+                 'else ' +
+                 'hg clone http://hg.mozilla.org/'+BRANCH+'/ ; ' +
+                 'fi '
+                 '&& hg -R '+BRANCH+' update'],
+    descriptionDone = BRANCH + "'s source", 
+    workdir = ".",
+    haltOnFailure = True
+)
+L10nNightlyFactory.addStep(ShellCommand,
+    command = ['sh', '-c',
+               WithProperties('if [ -d %(locale)s ]; then ' +
+                              'hg -R %(locale)s pull -r tip ; ' +
+                              'else ' +
+                              'hg clone http://hg.mozilla.org/l10n-central/%(locale)s/ ; ' +
+                              'fi '
+                              '&& hg -R %(locale)s update')],
+    descriptionDone = "locale's source",
+    workdir="l10n-central"
+)
+L10nNightlyFactory.addStep(ShellCommand,
+    command = ['sh','-c','autoconf-2.13'],
+    haltOnFailure=True,
+    descriptionDone="autoconf",
+    workdir = BRANCH
+)
+L10nNightlyFactory.addStep(Compile,
+    command = ['sh', '--',
+               './configure', '--enable-application=browser',
+               '--disable-compile-environment',
+               '--with-l10n-base=../l10n-central'],
+    description="configure",
+    descriptionDone="configure done",
+    haltOnFailure=True,
+    workdir=BRANCH,
+)
+L10nNightlyFactory.addStep(ShellCommand,
+    command=["make", "wget-en-US"],
+    descriptionDone="wget en-US",
+    env={'EN_US_BINARY_URL':enUS_binaryURL},
+    haltOnFailure = True,
+    workdir=BRANCH+"/browser/locales"
+)
+#It seems that I need to set env={'PKG_DMG_SOURCE':PROJECT}
+#because of /packager.mk#144
+L10nNightlyFactory.addStep(ShellCommand,
+    command=["make", WithProperties("installers-%(locale)s")],
+    env={'PKG_DMG_SOURCE':PROJECT},
+    haltOnFailure = True,
+    workdir=BRANCH+"/browser/locales"
+)
+L10nNightlyFactory.addStep(ShellCommand,
+    command=["make", WithProperties("prepare-upload-latest-%(locale)s")],
+    haltOnFailure=True,
+    workdir=BRANCH+"/browser/locales"
+)
+# This will upload everything in dist/upload, I assume that the first 
+# step run in this build is "rm -rf BRANCH/dist/upload"
+L10nNightlyFactory.addStep(ShellCommand,
+    name = "upload locale",
+    command=['sh','-c','scp -r * '+ftpserver+":"+uploadPath],
+    description="uploading packages", 
+    descriptionDone="uploaded packages",
+    workdir=BRANCH+"/dist/upload/latest"
+)
