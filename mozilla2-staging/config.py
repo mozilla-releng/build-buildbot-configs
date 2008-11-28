4e7ce8953087190bca838de8c6ea0cba811f1e5a
Chris Cooper <ccooper@deadsquid.com>
2008-11-28 16:33:11 -0500

    Bug 466991 - increase build timer from 2 to 6 hours, p=catlee, r=ccooper

--- mozilla2-staging/config.py
+++ mozilla2-staging/config.py
@@ -66,7 +66,7 @@ BRANCHES['mozilla-central']['allLocalesFile'] = 'browser/locales/all-locales'
 # generated.
 BRANCHES['mozilla-central']['create_snippet'] = True
 BRANCHES['mozilla-central']['aus2_base_upload_dir'] = '/opt/aus2/build/0/Firefox/mozilla-central'
-BRANCHES['mozilla-central']['periodic_scheduler_period'] = 60*60*2   # 2 hours
+BRANCHES['mozilla-central']['periodic_scheduler_period'] = 60*60*6   # 6 hours
 BRANCHES['mozilla-central']['platforms']['linux']['update_platform'] = 'Linux_x86-gcc3'
 # We're actually using gcc4, but Firefox hardcodes gcc3
 BRANCHES['mozilla-central']['platforms']['linux64']['update_platform'] = 'Linux_x86_64-gcc3'
@@ -292,7 +292,7 @@ BRANCHES['mozilla-1.9.1']['allLocalesFile'] = 'browser/locales/all-locales'
 # generated.
 BRANCHES['mozilla-1.9.1']['create_snippet'] = True
 BRANCHES['mozilla-1.9.1']['aus2_base_upload_dir'] = '/opt/aus2/build/0/Firefox/mozilla-1.9.1'
-BRANCHES['mozilla-1.9.1']['periodic_scheduler_period'] = 60*60*2   # 2 hours
+BRANCHES['mozilla-1.9.1']['periodic_scheduler_period'] = 60*60*6   # 6 hours
 BRANCHES['mozilla-1.9.1']['platforms']['linux']['update_platform'] = 'Linux_x86-gcc3'
 # We're actually using gcc4, but Firefox hardcodes gcc3
 BRANCHES['mozilla-1.9.1']['platforms']['linux64']['update_platform'] = 'Linux_x86_64-gcc3'
