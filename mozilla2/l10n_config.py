579f9147185852684943c717cf5360b33893cf05
Ben Hearsum <bhearsum@mozilla.com>
2008-12-05 14:21:34 -0500

    bug 460791: remove unused l10n_* configuration files. r=ccooper, patch=me

--- mozilla2/l10n_config.py
+++
@@ -1,6 +0,0 @@
-#make sure it has an ending slash
-uploadPath     = '/home/ftp/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n/'
-enUS_binaryURL = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central'
-allLocalesURL  = 'http://hg.mozilla.org/mozilla-central/index.cgi/raw-file/tip/browser/locales/all-locales'
-branch         = 'mozilla-central'
-project        = 'firefox'
