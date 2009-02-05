124d0691ba2f8aad7587f2f2e6e5bdc92a3b8ac7
Philippe M. Chiasson <gozer@mozillamessaging.com>
2009-02-04 21:53:36 -0500

    Enable update snippets on comm-central trunk, shipping to the new/alternate AUS. r=me

--- thunderbird/config.py
+++ thunderbird/config.py
@@ -148,9 +148,10 @@ BRANCHES['comm-central-trunk']['platforms']['win32']['profiled_build'] = False
 # If True, a complete update snippet for this branch will be generated and
 # uploaded to. Any platforms with 'debug' in them will not have snippets
 # generated.
-BRANCHES['comm-central-trunk']['create_snippet'] = False
+BRANCHES['comm-central-trunk']['create_snippet'] = True
 BRANCHES['comm-central-trunk']['create_l10n_snippets'] = False
-BRANCHES['comm-central-trunk']['aus2_base_upload_dir'] = '/opt/aus2/build/0/Thunderbird/comm-central'
+BRANCHES['comm-central-trunk']['aus2_host'] = 'aus-staging.sj.mozillamessaging.com'
+BRANCHES['comm-central-trunk']['aus2_base_upload_dir'] = '/opt/aus/build/0/Thunderbird/comm-central'
 BRANCHES['comm-central-trunk']['platforms']['linux']['update_platform'] = 'Linux_x86-gcc3'
 BRANCHES['comm-central-trunk']['platforms']['win32']['update_platform'] = 'WINNT_x86-msvc'
 #BRANCHES['comm-central-trunk']['platforms']['macosx']['update_platform'] = 'Darwin_Universal-gcc3'
