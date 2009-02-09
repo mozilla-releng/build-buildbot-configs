8536161660f7cf21e9adf5f11367ff8bbfb40fde
Alice Nodelman <alice@mozilla.com>
2009-02-09 14:55:03 -0800

    Bug 458093 - Update talos to send graph server new test information, p=anodelman r=catlee

--- talos-staging/perfrunner.py
+++ talos-staging/perfrunner.py
@@ -274,6 +274,7 @@ class MozillaUpdateConfig(ShellCommand):
         assert 'executablePath' in kwargs
         assert 'branch' in kwargs
         self.branch = kwargs['branch']
+        self.branchName = kwargs['branchName']
         self.exePath = kwargs['executablePath']
         if 'addOptions' in kwargs:
             self.addOptions = kwargs['addOptions']
@@ -285,7 +286,7 @@ class MozillaUpdateConfig(ShellCommand):
         self.changes = build.source.changes
         self.buildid = strftime("%Y%m%d%H%M", localtime(self.changes[-1].when))
         if not self.command:
-            self.setCommand(["python", "PerfConfigurator.py", "-v", "-e", self.exePath, "-t", self.title, "-b", self.branch, "-d", self.buildid] + self.addOptions)
+            self.setCommand(["python", "PerfConfigurator.py", "-v", "-e", self.exePath, "-t", self.title, "-b", self.branch, "-d", self.buildid, '--branchName', self.branchName] + self.addOptions)
 
     def describe(self, done=False):
         return ["Update config"]
@@ -580,7 +581,7 @@ class TalosFactory(BuildFactory):
     macClean       = "rm -vrf *"
     linuxClean     = "rm -vrf *"
 
-    def __init__(self, OS, envName, buildBranch, configOptions, buildPath, talosCmd, customManifest='', cvsRoot=":pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot"):
+    def __init__(self, OS, envName, buildBranch, branchName, configOptions, buildPath, talosCmd, customManifest='', cvsRoot=":pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot"):
         BuildFactory.__init__(self)
         if OS in ('linux', 'linuxbranch',):
             cleanCmd = self.linuxClean
@@ -684,6 +685,7 @@ class TalosFactory(BuildFactory):
         self.addStep(MozillaUpdateConfig(
                            workdir="talos/",
                            branch=buildBranch,
+                           branchName=branchName,
                            haltOnFailure=True,
                            executablePath=buildPath,
                            addOptions=configOptions,
