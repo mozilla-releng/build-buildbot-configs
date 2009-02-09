979a75e875afd80f4ab5d15fd93a333f7cdf9e47
Ben Hearsum <bhearsum@mozilla.com>
2009-02-09 09:03:40 -0500

    bug 417887: periodic reboot code - master side, disabled in production. r=catlee

--- mozilla2/config.py
+++ mozilla2/config.py
@@ -78,6 +78,13 @@ BRANCHES['mozilla-central']['platforms']['macosx']['build_space'] = 10
 BRANCHES['mozilla-central']['platforms']['linux-debug']['build_space'] = 2
 BRANCHES['mozilla-central']['platforms']['win32-debug']['build_space'] = 2
 BRANCHES['mozilla-central']['platforms']['macosx-debug']['build_space'] = 2
+BRANCHES['mozilla-central']['platforms']['linux']['builds_before_reboot'] = None
+BRANCHES['mozilla-central']['platforms']['linux64']['builds_before_reboot'] = None
+BRANCHES['mozilla-central']['platforms']['win32']['builds_before_reboot'] = None
+BRANCHES['mozilla-central']['platforms']['macosx']['builds_before_reboot'] = None
+BRANCHES['mozilla-central']['platforms']['linux-debug']['builds_before_reboot'] = None
+BRANCHES['mozilla-central']['platforms']['win32-debug']['builds_before_reboot'] = None
+BRANCHES['mozilla-central']['platforms']['macosx-debug']['builds_before_reboot'] = None
 # Enable unit tests
 BRANCHES['mozilla-central']['enable_unittests'] = True
 # L10n configuration
@@ -311,6 +318,13 @@ BRANCHES['mozilla-1.9.1']['platforms']['macosx']['build_space'] = 15
 BRANCHES['mozilla-1.9.1']['platforms']['linux-debug']['build_space'] = 2
 BRANCHES['mozilla-1.9.1']['platforms']['win32-debug']['build_space'] = 2
 BRANCHES['mozilla-1.9.1']['platforms']['macosx-debug']['build_space'] = 2
+BRANCHES['mozilla-1.9.1']['platforms']['linux']['builds_before_reboot'] = None
+BRANCHES['mozilla-1.9.1']['platforms']['linux64']['builds_before_reboot'] = None
+BRANCHES['mozilla-1.9.1']['platforms']['win32']['builds_before_reboot'] = None
+BRANCHES['mozilla-1.9.1']['platforms']['macosx']['builds_before_reboot'] = None
+BRANCHES['mozilla-1.9.1']['platforms']['linux-debug']['builds_before_reboot'] = None
+BRANCHES['mozilla-1.9.1']['platforms']['win32-debug']['builds_before_reboot'] = None
+BRANCHES['mozilla-1.9.1']['platforms']['macosx-debug']['builds_before_reboot'] = None
 # Enable unit tests
 BRANCHES['mozilla-1.9.1']['enable_unittests'] = True
 # L10n configuration
@@ -514,6 +528,9 @@ BRANCHES['tracemonkey']['platforms']['macosx']['profiled_build'] = False
 BRANCHES['tracemonkey']['platforms']['linux']['build_space'] = 5
 BRANCHES['tracemonkey']['platforms']['win32']['build_space'] = 4
 BRANCHES['tracemonkey']['platforms']['macosx']['build_space'] = 15
+BRANCHES['tracemonkey']['platforms']['linux']['builds_before_reboot'] = None
+BRANCHES['tracemonkey']['platforms']['win32']['builds_before_reboot'] = None
+BRANCHES['tracemonkey']['platforms']['macosx']['builds_before_reboot'] = None
 BRANCHES['tracemonkey']['platforms']['linux']['upload_symbols'] = True
 BRANCHES['tracemonkey']['platforms']['win32']['upload_symbols'] = True
 BRANCHES['tracemonkey']['platforms']['macosx']['upload_symbols'] = True
