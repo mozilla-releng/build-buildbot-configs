d3aed1722da1dd934251d279aa7ce9ba53d609fc
Nick Thomas <nrthomas@gmail.com>
2009-04-24 13:23:53 +1200

    Bug 486459 - automation config for 3.5b4 build1, r=bhearsum

--- mozilla2/release_config.py
+++ mozilla2/release_config.py
@@ -2,8 +2,8 @@ hgUsername         = 'ffxbld'
 hgSshKey           = '~cltbld/.ssh/ffxbld_dsa'
 sourceRepoName     = 'mozilla-1.9.1'
 sourceRepoPath     = 'releases/mozilla-1.9.1'
-sourceRepoRevision = '3d9704097cd8'
-relbranchOverride  = 'GECKO191b3_20090304_RELBRANCH'
+sourceRepoRevision = '5b61f163f2fd'
+relbranchOverride  = ''
 l10nRepoPath       = 'releases/l10n-mozilla-1.9.1'
 l10nRevisionFile   = 'l10n-changesets'
 cvsroot            = ':ext:cltbld@cvs.mozilla.org:/cvsroot'
@@ -14,15 +14,15 @@ appName            = 'browser'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version            = '3.1b3'
+version            = '3.5b4'
 appVersion         = version
-milestone          = '1.9.1b3'
-buildNumber        = 2
-baseTag            = 'FIREFOX_3_1b3'
-oldVersion         = '3.1b2'
+milestone          = '1.9.1b4'
+buildNumber        = 1
+baseTag            = 'FIREFOX_3_5b4'
+oldVersion         = '3.1b3'
 oldAppVersion      = oldVersion
 oldBuildNumber     = 2
-oldBaseTag         = 'FIREFOX_3_1b2'
+oldBaseTag         = 'FIREFOX_3_1b3'
 releasePlatforms   = ('linux', 'win32', 'macosx')
 patcherConfig      = 'moz191-branch-patcher2.cfg'
 patcherToolsTag    = 'UPDATE_PACKAGING_R7'
