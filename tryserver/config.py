f6ce46e0d77b35f167f1cc9f4c6cda06ef84affb
Ben Hearsum <bhearsum@mozilla.com>
2009-05-12 10:36:26 -0400

    [mq]: sendchange

--- tryserver/config.py
+++ tryserver/config.py
@@ -2,7 +2,7 @@ CVSROOT      = ":ext:trybld@cvs.mozilla.org:/cvsroot"
 OBJDIR       = "objdir"
 PKG_BASENAME = "firefox-try"
 SCP_STRING   = "trybld@build.mozilla.org:/builds/tryserver"
-TALOS_TRY_MASTER = "qm-rhel02.mozilla.org:9985"
+TALOS_TRY_MASTERS = [("qm-rhel02.mozilla.org:9985", True)]
 PACKAGE_URL  = "http://build.mozilla.org/tryserver-builds/%(uploadpath)s"
 WIN32_ENVIRONMENT = {
     'CVS_RSH': 'ssh',
