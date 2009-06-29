839990234da77e1bf587891f9283dfa62dfd2f99
Ben Hearsum <bhearsum@mozilla.com>
2009-06-29 11:40:58 -0400

    bug 500442: tracking bug for release of firefox 3.5 final - config bumps for tag & source for 3.5 final. r=ccooper

--- mozilla2/release_config.py
+++ mozilla2/release_config.py
@@ -2,7 +2,7 @@ hgUsername         = 'ffxbld'
 hgSshKey           = '~cltbld/.ssh/ffxbld_dsa'
 sourceRepoName     = 'mozilla-1.9.1'
 sourceRepoPath     = 'releases/mozilla-1.9.1'
-sourceRepoRevision = 'a625a31a0ad1'
+sourceRepoRevision = 'FIREFOX_3_5rc3_RELEASE'
 relbranchOverride  = 'GECKO191_20090623_RELBRANCH'
 l10nRepoPath       = 'releases/l10n-mozilla-1.9.1'
 l10nRevisionFile   = 'l10n-changesets'
@@ -14,15 +14,15 @@ appName            = 'browser'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version            = '3.5rc3'
+version            = '3.5'
 appVersion         = '3.5'
 milestone          = '1.9.1'
 buildNumber        = 2
-baseTag            = 'FIREFOX_3_5rc3'
-oldVersion         = '3.5rc2'
+baseTag            = 'FIREFOX_3_5'
+oldVersion         = '3.5rc3'
 oldAppVersion      = '3.5'
 oldBuildNumber     = 2
-oldBaseTag         = 'FIREFOX_3_5rc2'
+oldBaseTag         = 'FIREFOX_3_5rc3'
 releasePlatforms   = ('linux', 'win32', 'macosx')
 patcherConfig      = 'moz191-branch-patcher2.cfg'
 patcherToolsTag    = 'UPDATE_PACKAGING_R8'
