da4561a1130264d6a9aec53d68ad25001539e5a0
Robert Kaiser <kairo@kairo.at>
2009-07-02 20:35:12 +0200

    reduce seamonkey-unittest to mac-only for Tiger machine continuing to run in parallel to bug 485821 machines while Mac VMs are still not completely stable for unit test slaves

--- seamonkey-unittest/config.py
+++ seamonkey-unittest/config.py
@@ -48,38 +48,20 @@ BRANCHES['comm-central']['app_name'] = 'suite'
 BRANCHES['comm-central']['brand_name'] = 'SeaMonkey'
 # All platforms being built for this branch MUST be listed here.
 BRANCHES['comm-central']['platforms'] = {
-    'linux': {},
-    'win32': {},
     'macosx': {},
 }
 # The mozconfig file to use, relative to CONFIG_REPO_URL/CONFIG_SUBDIR
-BRANCHES['comm-central']['platforms']['linux']['mozconfig'] = 'linux/comm-central/nightly'
 BRANCHES['comm-central']['platforms']['macosx']['mozconfig'] = 'macosx/comm-central/nightly'
-BRANCHES['comm-central']['platforms']['win32']['mozconfig'] = 'win32/comm-central/nightly'
-BRANCHES['comm-central']['platforms']['linux']['base_name'] = 'Linux comm-central'
-BRANCHES['comm-central']['platforms']['win32']['base_name'] = 'WINNT 5.2 comm-central'
 BRANCHES['comm-central']['platforms']['macosx']['base_name'] = 'OS X 10.4 comm-central'
-BRANCHES['comm-central']['platforms']['linux']['profiled_build'] = False
-BRANCHES['comm-central']['platforms']['win32']['profiled_build'] = False
 BRANCHES['comm-central']['platforms']['macosx']['profiled_build'] = False
-BRANCHES['comm-central']['platforms']['linux']['build_space'] = 5
-BRANCHES['comm-central']['platforms']['win32']['build_space'] = 7
 BRANCHES['comm-central']['platforms']['macosx']['build_space'] = 5
-BRANCHES['comm-central']['platforms']['linux']['builds_before_reboot'] = None
-BRANCHES['comm-central']['platforms']['win32']['builds_before_reboot'] = None
 BRANCHES['comm-central']['platforms']['macosx']['builds_before_reboot'] = None
 # Enable unit tests
 BRANCHES['comm-central']['enable_unittests'] = True
 BRANCHES['comm-central']['enable_mac_a11y'] = False
 BRANCHES['comm-central']['unittest_build_space'] = 5
-BRANCHES['comm-central']['platforms']['linux']['mochitest_leak_threshold'] = 0
-BRANCHES['comm-central']['platforms']['win32']['mochitest_leak_threshold'] = 200
 BRANCHES['comm-central']['platforms']['macosx']['mochitest_leak_threshold'] = 0
-BRANCHES['comm-central']['platforms']['linux']['mochichrome_leak_threshold'] = 0
-BRANCHES['comm-central']['platforms']['win32']['mochichrome_leak_threshold'] = 0
 BRANCHES['comm-central']['platforms']['macosx']['mochichrome_leak_threshold'] = 0
-BRANCHES['comm-central']['platforms']['linux']['mochibrowser_leak_threshold'] = 0
-BRANCHES['comm-central']['platforms']['win32']['mochibrowser_leak_threshold'] = 0
 BRANCHES['comm-central']['platforms']['macosx']['mochibrowser_leak_threshold'] = 0
 # And code coverage
 BRANCHES['comm-central']['enable_codecoverage'] = False
@@ -97,23 +79,11 @@ BRANCHES['comm-central']['allLocalesFile'] = 'suite/locales/all-locales'
 BRANCHES['comm-central']['create_snippet'] = True
 BRANCHES['comm-central']['aus2_base_upload_dir'] = '/opt/aus2/build/0/SeaMonkey/trunk'
 BRANCHES['comm-central']['idle_timeout'] = 60*60*2   # 2 hours
-BRANCHES['comm-central']['platforms']['linux']['update_platform'] = 'Linux_x86-gcc3'
-BRANCHES['comm-central']['platforms']['win32']['update_platform'] = 'WINNT_x86-msvc'
 BRANCHES['comm-central']['platforms']['macosx']['update_platform'] = 'Darwin_Universal-gcc3'
 # If True, 'make buildsymbols' and 'make uploadsymbols' will be run
 # SYMBOL_SERVER_* variables are setup in the environment section below
-BRANCHES['comm-central']['platforms']['linux']['upload_symbols'] = False
-BRANCHES['comm-central']['platforms']['win32']['upload_symbols'] = False
 BRANCHES['comm-central']['platforms']['macosx']['upload_symbols'] = False
 BRANCHES['comm-central']['tinderbox_tree'] = 'SeaMonkey'
-BRANCHES['comm-central']['platforms']['linux']['slaves'] = [
-#    'cb-sea-linux-tbox',
-    'cn-sea-qm-centos5-01'
-]
-BRANCHES['comm-central']['platforms']['win32']['slaves'] = [
-#    'cb-sea-win32-tbox',
-    'cn-sea-qm-win2k3-01'
-]
 BRANCHES['comm-central']['platforms']['macosx']['slaves'] = [
 #    'cb-xserve02',
     'cb-sea-miniosx01'
@@ -121,29 +91,7 @@ BRANCHES['comm-central']['platforms']['macosx']['slaves'] = [
 # This is used in a bunch of places where something needs to be run from
 # the objdir. This is necessary because of universal builds on Mac
 # creating subdirectories inside of the objdir.
-BRANCHES['comm-central']['platforms']['linux']['platform_objdir'] = OBJDIR
-BRANCHES['comm-central']['platforms']['win32']['platform_objdir'] = OBJDIR
 BRANCHES['comm-central']['platforms']['macosx']['platform_objdir'] = '%s/ppc' % OBJDIR
-BRANCHES['comm-central']['platforms']['linux']['env'] = {
-    'MOZ_OBJDIR': OBJDIR,
-    'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
-    'SYMBOL_SERVER_USER': 'seabld',
-    'SYMBOL_SERVER_PATH': '/mnt/netapp/breakpad/symbols_sea/',
-    'SYMBOL_SERVER_SSH_KEY': "/home/seabld/.ssh/seabld_dsa",
-    'TINDERBOX_OUTPUT': '1',
-    'MOZ_CRASHREPORTER_NO_REPORT': '1',
-}
-BRANCHES['comm-central']['platforms']['win32']['env'] = {'CVS_RSH': 'ssh',
-    'MOZ_OBJDIR': OBJDIR,
-    'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
-    'SYMBOL_SERVER_USER': 'seabld',
-    'SYMBOL_SERVER_PATH': '/mnt/netapp/breakpad/symbols_sea/',
-    'SYMBOL_SERVER_SSH_KEY': "/c/Documents and Settings/seabld/.ssh/seabld_dsa",
-    'TINDERBOX_OUTPUT': '1',
-    'MOZ_CRASHREPORTER_NO_REPORT': '1',
-    'BUILD_PALMSYNC': '1',
-    'PALM_CDK_DIR': 'C:\PALMCDK403',
-}
 BRANCHES['comm-central']['platforms']['macosx']['env'] = {
     'MOZ_OBJDIR': OBJDIR,
     'SYMBOL_SERVER_HOST': 'dm-symbolpush01.mozilla.org',
