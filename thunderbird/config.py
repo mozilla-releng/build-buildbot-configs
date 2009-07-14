952a04cc488b831fb37a0f2a285de44d5176172e
Philippe M. Chiasson <gozer@mozillamessaging.com>
2009-07-14 15:10:30 -0400

    Bug 502335. Temporarly switch Sunbird to our builders while the calendar xserve is down

--- thunderbird/config.py
+++ thunderbird/config.py
@@ -466,7 +466,9 @@ BRANCHES['comm-central-sunbird']['platforms']['win32']['slaves'] = [
 ]
 BRANCHES['comm-central-sunbird']['platforms']['macosx']['slaves'] = [
     'cb-xserve03',
+    'bm-xserve07',
 ]
+BRANCHES['comm-central-sunbird']['platforms']['macosx']['cvsroot'] = ':ext:tbirdbld@cvs.mozilla.org:/cvsroot'
 
 # This is used in a bunch of places where something needs to be run from
 # the objdir. This is necessary because of universal builds on Mac
