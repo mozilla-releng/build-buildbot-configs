f00ffec4af36c423aece1f191724c048d1fa5664
Ben Hearsum <bhearsum@mozilla.com>
2009-07-15 06:15:24 -0700

    bug 504094: tracking bug for build and release of firefox 3.5.1 - release_config.py bumps. r=firedrill

--- mozilla2/release_config.py
+++ mozilla2/release_config.py
@@ -2,8 +2,8 @@ hgUsername         = 'ffxbld'
 hgSshKey           = '~cltbld/.ssh/ffxbld_dsa'
 sourceRepoName     = 'mozilla-1.9.1'
 sourceRepoPath     = 'releases/mozilla-1.9.1'
-sourceRepoRevision = 'FIREFOX_3_5rc3_RELEASE'
-relbranchOverride  = 'GECKO191_20090623_RELBRANCH'
+sourceRepoRevision = 'b91c9ee69e6e'
+relbranchOverride  = ''
 l10nRepoPath       = 'releases/l10n-mozilla-1.9.1'
 l10nRevisionFile   = 'l10n-changesets'
 cvsroot            = ':ext:cltbld@cvs.mozilla.org:/cvsroot'
@@ -14,18 +14,18 @@ appName            = 'browser'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version            = '3.5'
-appVersion         = '3.5'
-milestone          = '1.9.1'
-buildNumber        = 2
-baseTag            = 'FIREFOX_3_5'
+version            = '3.5.1'
+appVersion         = '3.5.1'
+milestone          = '1.9.1.1'
+buildNumber        = 1
+baseTag            = 'FIREFOX_3_5_1'
 oldVersion         = '3.5rc3'
 oldAppVersion      = '3.5'
 oldBuildNumber     = 2
-oldBaseTag         = 'FIREFOX_3_5rc3'
+oldBaseTag         = 'FIREFOX_3_5'
 releasePlatforms   = ('linux', 'win32', 'macosx')
 patcherConfig      = 'moz191-branch-patcher2.cfg'
-patcherToolsTag    = 'UPDATE_PACKAGING_R8'
+patcherToolsTag    = 'UPDATE_PACKAGING_R9'
 ftpServer          = 'ftp.mozilla.org'
 stagingServer      = 'stage-old.mozilla.org'
 bouncerServer      = 'download.mozilla.org'
