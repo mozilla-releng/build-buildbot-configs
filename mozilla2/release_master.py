ef984ddc036fead77dffacf98258905c670928f7
Ben Hearsum <bhearsum@mozilla.com>
2009-08-10 18:07:54 -0400

    Last round of fixes from nthomas and catlee's reviews.
    --HG--
    extra : rebase_source : a6259460d2bb6c9578d6f9ccf6a7e43896adcc9f

--- mozilla2/release_master.py
+++ mozilla2/release_master.py
@@ -113,7 +113,7 @@ repositories = {
 }
 l10n_repos = get_l10n_repositories(l10nRevisionFile, l10nRepoPath,
                                    relbranchOverride)
-#repositories.update(l10n_repos)
+repositories.update(l10n_repos)
 
 tag_factory = ReleaseTaggingFactory(
     hgHost=branchConfig['hghost'],
