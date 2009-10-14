d805d9f2c3b4380a12f5fbdd52ded88761194464
Nick Thomas <nthomas@mozilla.com>
2009-10-14 11:36:28 -0400

    bug 518496: Tracking bug for build and release of Firefox 3.6b1 - automation config for 3.6b1build1. r=bhearsum.

--- mozilla2/release-firefox-mozilla-1.9.2.py
+++ mozilla2/release-firefox-mozilla-1.9.2.py
@@ -2,11 +2,10 @@ hgUsername         = 'ffxbld'
 hgSshKey           = '~cltbld/.ssh/ffxbld_dsa'
 sourceRepoName     = 'mozilla-1.9.2'
 sourceRepoPath     = 'releases/mozilla-1.9.2'
-# NB: tagging was done manually for 3.6a2
-sourceRepoRevision = 'bf28b5220348'
-relbranchOverride  = 'GECKO192a2_20090915_RELBRANCH'
+sourceRepoRevision = 'f789bb32468d'
+relbranchOverride  = ''
 l10nRepoPath       = 'releases/l10n-mozilla-1.9.2'
-l10nRevisionFile   = 'l10n-changesets'
+l10nRevisionFile   = 'l10n-changesets_mozilla-1.9.2'
 cvsroot            = ':ext:cltbld@cvs.mozilla.org:/cvsroot'
 productName        = 'firefox'
 appName            = 'browser'
@@ -15,27 +14,24 @@ appName            = 'browser'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version            = '3.6a2'
+version            = '3.6b1'
 appVersion         = version
-milestone          = '1.9.2a2'
-buildNumber        = 2
-baseTag            = 'FIREFOX_3_6a2'
+milestone          = '1.9.2b1'
+buildNumber        = 1
+baseTag            = 'FIREFOX_3_6b1'
 oldVersion         = '3.6a1'
 oldAppVersion      = oldVersion
 oldBuildNumber     = 1
 oldBaseTag         = 'FIREFOX_3_6a1'
-# TODO: restore these tuples to full platform list before 3.6b1
-enUSPlatforms      = ('wince', )
-l10nPlatforms      = ()
-# TODO: create this file before 3.6b1
+enUSPlatforms      = ('linux', 'win32', 'macosx', 'wince')
+l10nPlatforms      = ('linux', 'win32', 'macosx')
 patcherConfig      = 'moz192-branch-patcher2.cfg'
-patcherToolsTag    = 'UPDATE_PACKAGING_R9'
+patcherToolsTag    = 'UPDATE_PACKAGING_R10'
 ftpServer          = 'ftp.mozilla.org'
 stagingServer      = 'stage-old.mozilla.org'
 bouncerServer      = 'download.mozilla.org'
 ausServerUrl       = 'https://aus2.mozilla.org'
 useBetaChannel     = 0
-# TODO: create these files before 3.6b1
 verifyConfigs      = {'linux':  'moz192-firefox-linux.cfg',
                       'macosx': 'moz192-firefox-mac.cfg',
                       'win32':  'moz192-firefox-win32.cfg',
