c66804799ff6cbba53b1dd082efe65c007dd4902
Aki Sasaki <asasaki@mozilla.com>
2009-10-21 13:13:41 -0700

    enable tp4 for mobile. bug 509879, r=catlee

--- mobile-staging/config.py
+++ mobile-staging/config.py
@@ -12,7 +12,7 @@ BRANCHES = {
 # {{{1 mozilla-central
 #
 BRANCHES['mozilla-central']['tinderbox_tree'] = "MozillaTest"
-BRANCHES['mozilla-central']['graph_server'] = "graphs-stage-old.mozilla.org"
+BRANCHES['mozilla-central']['graph_server'] = "graphs-stage.mozilla.org"
 BRANCHES['mozilla-central']['platforms'] = {
     'n810': {},
 }
@@ -30,38 +30,45 @@ BRANCHES['mozilla-central']['platforms']['n810']['talos_build_dirs'] = [
     'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-trunk/',
 ]
 BRANCHES['mozilla-central']['platforms']['n810']['poller_string'] = 'fennec-.*\.en-US\.linux.*arm\.tar\.bz2'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_scripts'] = 'http://staging-mobile-master.mv.mozilla.com/maemo/talos.tar.bz2'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_pageloader'] = 'http://staging-mobile-master.mv.mozilla.com/maemo/pageloader.tar.bz2'
 BRANCHES['mozilla-central']['platforms']['n810']['talos_suites'] = {
-    'tp3': {},
-    'non_tp1': {},
-    'non_tp2': {},
+    'tp4': {},
+    'ts': {},
+    'twinopen': {},
+    'tdhtml': {},
+    'tsvg': {},
+    'tsspider': {},
+    'tgfx': {},
 }
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp3']['suite_name'] = 'N810 mozilla-central talos Tp3'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp3']['build_dir'] = 'n810-trunk-tp'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp3']['config_file'] = 'mobile.config'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp3']['active_tests'] = {
-    'tp':   90,
-}
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp3']['disable_jit'] = False
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp1']['suite_name'] = 'N810 mozilla-central talos non-Tp1'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp1']['build_dir'] = 'n810-trunk-non-tp1'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp1']['config_file'] = 'mobile.config'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp1']['active_tests'] = {
-    'ts':   60,
-#    'tpan':   60,
-#    'tzoom':   60,
-    'twinopen':   60,
-    'tdhtml':   60,
-}
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp1']['disable_jit'] = False
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp2']['suite_name'] = 'N810 mozilla-central talos non-Tp2'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp2']['build_dir'] = 'n810-trunk-non-tp2'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp2']['config_file'] = 'mobile.config'
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp2']['active_tests'] = {
-    'tsvg':   60,
-    'tsspider':   60,
-    'tgfx':   60,
-}
-BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['non_tp2']['disable_jit'] = False
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp4']['suite_name'] = 'N810 mozilla-central talos Tp4'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp4']['build_dir'] = 'n810-trunk-tp4'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp4']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tp4']['timeout'] = 90
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['ts']['suite_name'] = 'N810 mozilla-central talos Ts'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['ts']['build_dir'] = 'n810-trunk-ts'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['ts']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['ts']['timeout'] = 60
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['twinopen']['suite_name'] = 'N810 mozilla-central talos Twinopen'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['twinopen']['build_dir'] = 'n810-trunk-twinopen'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['twinopen']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['twinopen']['timeout'] = 60
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tdhtml']['suite_name'] = 'N810 mozilla-central talos Tdhtml'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tdhtml']['build_dir'] = 'n810-trunk-tdhtml'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tdhtml']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tdhtml']['timeout'] = 60
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tsvg']['suite_name'] = 'N810 mozilla-central talos Tsvg'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tsvg']['build_dir'] = 'n810-trunk-tsvg'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tsvg']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tsvg']['timeout'] = 60
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tsspider']['suite_name'] = 'N810 mozilla-central talos Tsspider'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tsspider']['build_dir'] = 'n810-trunk-tsspider'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tsspider']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tsspider']['timeout'] = 60
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tgfx']['suite_name'] = 'N810 mozilla-central talos Tgfx'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tgfx']['build_dir'] = 'n810-trunk-tgfx'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tgfx']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-central']['platforms']['n810']['talos_suites']['tgfx']['timeout'] = 60
 BRANCHES['mozilla-central']['platforms']['n810']['test_suites'] = {
     'mochitest1': {},
     'mochitest2': {},
@@ -97,7 +104,7 @@ BRANCHES['mozilla-central']['platforms']['n810']['test_suites']['xpcshell']['kno
 # {{{1 mozilla-1.9.2
 #
 BRANCHES['mozilla-1.9.2']['tinderbox_tree'] = "MozillaTest"
-BRANCHES['mozilla-1.9.2']['graph_server'] = "graphs-stage-old.mozilla.org"
+BRANCHES['mozilla-1.9.2']['graph_server'] = "graphs-stage.mozilla.org"
 BRANCHES['mozilla-1.9.2']['platforms'] = {
     'n810': {},
 }
@@ -115,38 +122,45 @@ BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_build_dirs'] = [
     'http://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mobile-1.9.2/',
 ]
 BRANCHES['mozilla-1.9.2']['platforms']['n810']['poller_string'] = 'fennec-.*\.en-US\.linux.*arm\.tar\.bz2'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_scripts'] = 'http://staging-mobile-master.mv.mozilla.com/maemo/talos.tar.bz2'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_pageloader'] = 'http://staging-mobile-master.mv.mozilla.com/maemo/pageloader.tar.bz2'
 BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites'] = {
-    'tp3': {},
-    'non_tp1': {},
-    'non_tp2': {},
+    'tp4': {},
+    'ts': {},
+    'twinopen': {},
+    'tdhtml': {},
+    'tsvg': {},
+    'tsspider': {},
+    'tgfx': {},
 }
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp3']['suite_name'] = 'N810 mozilla-1.9.2 talos Tp3'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp3']['build_dir'] = 'n810-1.9.2-tp'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp3']['config_file'] = 'mobile.config'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp3']['active_tests'] = {
-    'tp':   90,
-}
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp3']['disable_jit'] = False
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp1']['suite_name'] = 'N810 mozilla-1.9.2 talos non-Tp1'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp1']['build_dir'] = 'n810-1.9.2-non-tp1'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp1']['config_file'] = 'mobile.config'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp1']['active_tests'] = {
-    'ts':   60,
-#    'tpan':   60,
-#    'tzoom':   60,
-    'twinopen':   60,
-    'tdhtml':   60,
-}
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp1']['disable_jit'] = False
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp2']['suite_name'] = 'N810 mozilla-1.9.2 talos non-Tp2'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp2']['build_dir'] = 'n810-1.9.2-non-tp2'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp2']['config_file'] = 'mobile.config'
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp2']['active_tests'] = {
-    'tsvg':   60,
-    'tsspider':   60,
-    'tgfx':   60,
-}
-BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['non_tp2']['disable_jit'] = False
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp4']['suite_name'] = 'N810 mozilla-1.9.2 talos Tp4'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp4']['build_dir'] = 'n810-1.9.2-tp4'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp4']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tp4']['timeout'] = 90
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['ts']['suite_name'] = 'N810 mozilla-1.9.2 talos Ts'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['ts']['build_dir'] = 'n810-1.9.2-ts'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['ts']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['ts']['timeout'] = 60
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['twinopen']['suite_name'] = 'N810 mozilla-1.9.2 talos Twinopen'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['twinopen']['build_dir'] = 'n810-1.9.2-twinopen'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['twinopen']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['twinopen']['timeout'] = 60
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tdhtml']['suite_name'] = 'N810 mozilla-1.9.2 talos Tdhtml'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tdhtml']['build_dir'] = 'n810-1.9.2-tdhtml'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tdhtml']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tdhtml']['timeout'] = 60
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tsvg']['suite_name'] = 'N810 mozilla-1.9.2 talos Tsvg'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tsvg']['build_dir'] = 'n810-1.9.2-tsvg'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tsvg']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tsvg']['timeout'] = 60
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tsspider']['suite_name'] = 'N810 mozilla-1.9.2 talos Tsspider'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tsspider']['build_dir'] = 'n810-1.9.2-tsspider'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tsspider']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tsspider']['timeout'] = 60
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tgfx']['suite_name'] = 'N810 mozilla-1.9.2 talos Tgfx'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tgfx']['build_dir'] = 'n810-1.9.2-tgfx'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tgfx']['config_file'] = 'mobile.config'
+BRANCHES['mozilla-1.9.2']['platforms']['n810']['talos_suites']['tgfx']['timeout'] = 60
 BRANCHES['mozilla-1.9.2']['platforms']['n810']['test_suites'] = {
     'mochitest1': {},
     'mochitest2': {},
@@ -182,7 +196,7 @@ BRANCHES['mozilla-1.9.2']['platforms']['n810']['test_suites']['xpcshell']['known
 # {{{1 TraceMonkey
 #
 BRANCHES['tracemonkey']['tinderbox_tree'] = "MozillaTest"
-BRANCHES['tracemonkey']['graph_server'] = "graphs-stage-old.mozilla.org"
+BRANCHES['tracemonkey']['graph_server'] = "graphs-stage.mozilla.org"
 BRANCHES['tracemonkey']['platforms'] = {
     'n810': {},
 }
@@ -192,7 +206,6 @@ BRANCHES['tracemonkey']['platforms']['n810']['buildbot_branch'] = 'maemo-tm'
 BRANCHES['tracemonkey']['platforms']['n810']['talos_branch'] = 'mobile-tracemonkey'
 BRANCHES['tracemonkey']['platforms']['n810']['poll_interval'] = 5*60
 BRANCHES['tracemonkey']['platforms']['n810']['unit_build_dirs'] = [
-    'http://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mobile-tracemonkey/',
     'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-tracemonkey/',
 ]
 BRANCHES['tracemonkey']['platforms']['n810']['talos_build_dirs'] = [
@@ -200,38 +213,45 @@ BRANCHES['tracemonkey']['platforms']['n810']['talos_build_dirs'] = [
     'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-tracemonkey/',
 ]
 BRANCHES['tracemonkey']['platforms']['n810']['poller_string'] = 'fennec-.*\.en-US\.linux.*arm\.tar\.bz2'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_scripts'] = 'http://staging-mobile-master.mv.mozilla.com/maemo/talos.tar.bz2'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_pageloader'] = 'http://staging-mobile-master.mv.mozilla.com/maemo/pageloader.tar.bz2'
 BRANCHES['tracemonkey']['platforms']['n810']['talos_suites'] = {
-    'tp3': {},
-    'non_tp1': {},
-    'non_tp2': {},
+    'tp4': {},
+    'ts': {},
+    'twinopen': {},
+    'tdhtml': {},
+    'tsvg': {},
+    'tsspider': {},
+    'tgfx': {},
 }
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp3']['suite_name'] = 'N810 tracemonkey talos Tp3'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp3']['build_dir'] = 'n810-tm-tp'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp3']['config_file'] = 'mobile-tracemonkey.config'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp3']['active_tests'] = {
-    'tp':   90,
-}
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp3']['disable_jit'] = False
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp1']['suite_name'] = 'N810 tracemonkey talos non-Tp1'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp1']['build_dir'] = 'n810-tm-non-tp1'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp1']['config_file'] = 'mobile-tracemonkey.config'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp1']['active_tests'] = {
-    'ts':   60,
-#    'tpan':   60,
-#    'tzoom':   60,
-    'twinopen':   60,
-    'tdhtml':   60,
-}
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp1']['disable_jit'] = False
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp2']['suite_name'] = 'N810 tracemonkey talos non-Tp2'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp2']['build_dir'] = 'n810-tm-non-tp2'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp2']['config_file'] = 'mobile-tracemonkey.config'
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp2']['active_tests'] = {
-    'tsvg':   60,
-    'tsspider':   60,
-    'tgfx':   60,
-}
-BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['non_tp2']['disable_jit'] = False
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp4']['suite_name'] = 'N810 tracemonkey talos Tp4'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp4']['build_dir'] = 'n810-tm-tp4'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp4']['config_file'] = 'mobile.config'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tp4']['timeout'] = 90
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['ts']['suite_name'] = 'N810 tracemonkey talos Ts'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['ts']['build_dir'] = 'n810-tm-ts'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['ts']['config_file'] = 'mobile.config'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['ts']['timeout'] = 60
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['twinopen']['suite_name'] = 'N810 tracemonkey talos Twinopen'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['twinopen']['build_dir'] = 'n810-tm-twinopen'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['twinopen']['config_file'] = 'mobile.config'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['twinopen']['timeout'] = 60
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tdhtml']['suite_name'] = 'N810 tracemonkey talos Tdhtml'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tdhtml']['build_dir'] = 'n810-tm-tdhtml'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tdhtml']['config_file'] = 'mobile.config'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tdhtml']['timeout'] = 60
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tsvg']['suite_name'] = 'N810 tracemonkey talos Tsvg'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tsvg']['build_dir'] = 'n810-tm-tsvg'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tsvg']['config_file'] = 'mobile.config'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tsvg']['timeout'] = 60
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tsspider']['suite_name'] = 'N810 tracemonkey talos Tsspider'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tsspider']['build_dir'] = 'n810-tm-tsspider'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tsspider']['config_file'] = 'mobile.config'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tsspider']['timeout'] = 60
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tgfx']['suite_name'] = 'N810 tracemonkey talos Tgfx'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tgfx']['build_dir'] = 'n810-tm-tgfx'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tgfx']['config_file'] = 'mobile.config'
+BRANCHES['tracemonkey']['platforms']['n810']['talos_suites']['tgfx']['timeout'] = 60
 BRANCHES['tracemonkey']['platforms']['n810']['test_suites'] = {
     'mochitest1': {},
     'mochitest2': {},
