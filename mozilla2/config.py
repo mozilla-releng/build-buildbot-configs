3c9f1fe86ef668b51fc1dc1842b6ae7cd00d5be0
Lukas Blakk <lsblakk@mozilla.com>
2009-11-04 15:42:49 -0800

    Bug 526627 - Switch talos-staging reporting to talos-staging-master02 r=catlee

--- mozilla2/config.py
+++ mozilla2/config.py
@@ -56,7 +56,7 @@ BRANCH_LEVEL_VARS = {
     # and if a failure to notify the talos master should result in a warning
     'talos_masters': [
         ('talos-master.mozilla.org:9010', False),
-        ('talos-staging-master.mozilla.org:9010', False),
+        ('talos-staging-master02.mv.mozilla.com:9010', False),
     ],
     # List of unittest masters to notify of new builds to test,
     # and if a failure to notify the master should result in a warning
