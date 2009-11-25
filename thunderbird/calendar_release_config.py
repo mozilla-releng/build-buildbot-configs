2b60fd24c5687049ce82b3904fc6cb3bdeafb306
Philippe M. Chiasson <gozer@mozillamessaging.com>
2009-11-25 16:46:59 -0500

    update comm-1.9.1 revision for Sunbird 1.0b1

--- thunderbird/calendar_release_config.py
+++ thunderbird/calendar_release_config.py
@@ -3,7 +3,7 @@ hgSshKey                   = '~calbld/.ssh/calbld_dsa'
 relbranchPrefix            = 'COMM'
 sourceRepoName             = 'comm-1.9.1-sunbird' # buildbot branch name
 sourceRepoPath             = 'releases/comm-1.9.1'
-sourceRepoRevision         = '1d5c52e21b70'
+sourceRepoRevision         = '39add317ae93'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 relbranchOverride          = ''
 mozillaRepoPath            = 'releases/mozilla-1.9.1'
