711066027eea9c84794d938fa608e73638ad170b
Philippe M. Chiasson <gozer@mozillamessaging.com>
2009-11-25 14:46:27 -0500

    build configuration for Sunbird 1.0b1
    --HG--
    rename : thunderbird/thunderbird_release_config.py => thunderbird/calendar_release_config.py
    rename : thunderbird/l10n-changesets => thunderbird/l10n-thunderbird-changesets

--- thunderbird/calendar_release_config.py
+++ thunderbird/calendar_release_config.py
@@ -1,57 +1,57 @@
-hgUsername                 = 'tbirdbld'
-hgSshKey                   = '~cltbld/.ssh/tbirdbld_dsa'
+hgUsername                 = 'calbld'
+hgSshKey                   = '~calbld/.ssh/calbld_dsa'
 relbranchPrefix            = 'COMM'
-sourceRepoName             = 'comm-central' # buildbot branch name
+sourceRepoName             = 'comm-1.9.1-sunbird' # buildbot branch name
 sourceRepoPath             = 'releases/comm-1.9.1'
-sourceRepoRevision         = '3047465d50a0'
+sourceRepoRevision         = '1d5c52e21b70'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
-relbranchOverride          = 'COMM1915_20091112_RELBRANCH'
+relbranchOverride          = ''
 mozillaRepoPath            = 'releases/mozilla-1.9.1'
-mozillaRepoRevision        = '6dc036c10334'
+mozillaRepoRevision        = '37d0f274bafc'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 # You typically want to set this to the gecko relbranch if doing a release off
 # a specific gecko version.
-mozillaRelbranchOverride   = 'COMM1915_20091112_RELBRANCH' # put Gecko relbranch here that we base upon
+mozillaRelbranchOverride   = '' # put Gecko relbranch here that we base upon
 inspectorRepoPath          = 'dom-inspector' # leave empty if inspector is not to be tagged
-inspectorRepoRevision      = '18a1c983c8ee'
-inspectorRelbranchOverride = 'COMM1915_20091112_RELBRANCH'
+inspectorRepoRevision      = 'ebe59a415752'
+inspectorRelbranchOverride = ''
 venkmanRepoPath            = '' # leave empty if venkman is not to be tagged
 venkmanRepoRevision        = ''
 venkmanRelbranchOverride   = ''
 chatzillaCVSRoot           = ''
 chatzillaTimestamp         = '' # leave empty if chatzilla is not to be tagged
 l10nRepoPath               = 'releases/l10n-mozilla-1.9.1'
-l10nRevisionFile           = 'l10n-thunderbird-changesets'
+l10nRevisionFile           = 'l10n-calendar-changesets'
 toolsRepoPath              = 'build/tools'
-cvsroot                    = ':ext:cltbld@cvs.mozilla.org:/cvsroot' # for patcher, etc.
-productVersionFile         = 'mail/config/version-191.txt'
-productName                = 'thunderbird'
-brandName                  = 'Thunderbird'
-appName                    = 'mail'
+cvsroot                    = ':ext:calbld@cvs.mozilla.org:/cvsroot' # for patcher, etc.
+productVersionFile         = 'calendar/sunbird/config/version.txt'
+productName                = 'sunbird'
+brandName                  = 'Sunbird'
+appName                    = 'calendar'
 # Sometimes we need the application version to be different from what we "call"
 # the build, eg public release candidates for a major release (3.1 RC1).
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '3.0rc1'
-appVersion                 = '3.0'
+version                    = '1.0b1'
+appVersion                 = '1.0b1'
 #XXX: Not entirely certain if/where this is used.
 milestone                  = '1.9.1.5'
-buildNumber                = 3
-baseTag                    = 'THUNDERBIRD_3_0rc1'
+buildNumber                = 1
+baseTag                    = 'SUNBIRD_1_0b1'
 # The old version is the revision from which we should generate update snippets.
-oldVersion                 = '3.0b4'
+oldVersion                 = ''
 oldAppVersion              = oldVersion
-oldBuildNumber             = 4
-oldBaseTag                 = 'THUNDERBIRD_3_0b4'
+oldBuildNumber             = 1
+oldBaseTag                 = ''
 releasePlatforms           = ('linux', 'win32', 'macosx')
-patcherConfig              = 'moz19-thunderbird-branch-patcher2.cfg'
+patcherConfig              = 'moz19-calendar-branch-patcher2.cfg'
 patcherToolsTag            = 'UPDATE_PACKAGING_R9'
 ftpServer                  = 'ftp.mozilla.org'
 stagingServer              = 'stage-old.mozilla.org'
 bouncerServer              = 'download.mozilla.org'
-ausServerUrl               = 'https://aus2.mozillamessaging.com'
-useBetaChannel             = 0
-verifyConfigs              = {'linux':  'moz19-thunderbird-linux.cfg',
-                              'macosx': 'moz19-thunderbird-mac.cfg',
-                              'win32':  'moz19-thunderbird-win32.cfg'}
+ausServerUrl               = 'https://aus2-community.mozilla.org'
+useBetaChannel             = 1
+verifyConfigs              = {'linux':  'moz19-calendar-linux.cfg',
+                              'macosx': 'moz19-calendar-mac.cfg',
+                              'win32':  'moz19-calendar-win32.cfg'}
