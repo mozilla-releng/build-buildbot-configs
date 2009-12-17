0df95e2d48eb80ef354c6e0511df9de46ff30a16
Aki Sasaki <asasaki@mozilla.com>
2009-12-17 14:30:53 -0800

    release repos. bug 533997, r=catlee

--- debsign/config-production.py
+++ debsign/config-production.py
@@ -1,6 +1,31 @@
 SLAVES = {
     'linux': ['debsign1'],
 }
+NIGHTLY_ENV = {
+    'STAGE_USERNAME': 'ffxbld',
+    'STAGE_SERVER': 'stage.mozilla.org',
+    'BASE_STAGE_PATH': '/home/ftp/pub/mozilla.org/mobile/repos',
+    'BASE_STAGE_URL': 'http://ftp.mozilla.org/pub/mozilla.org/mobile/repos',
+    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
+}
+REAL_RELEASE_ENV = {
+    'STAGE_USERNAME': 'cltbld',
+    'STAGE_SERVER': 'stage.mozilla.org',
+    'BASE_STAGE_PATH': '/home/ftp/pub/mozilla.org/mobile/repos',
+    'BASE_STAGE_URL': 'http://ftp.mozilla.org/pub/mozilla.org/mobile/repos',
+    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
+}
+CANDIDATES_RELEASE_ENV = {
+    'STAGE_USERNAME': 'cltbld',
+    'STAGE_SERVER': 'stage.mozilla.org',
+    'BASE_STAGE_PATH': '/home/ftp/pub/mozilla.org/mobile/candidates/1.0rc1-candidates/repos',
+    'BASE_STAGE_URL': 'http://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/1.0rc1-candidates/repos',
+    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
+}
+ 
+
+# Change for real release
+RELEASE_ENV = CANDIDATES_RELEASE_ENV
 
 DEBSIGN_CONFIG = {}
 TRIGGER_CONFIG = {}
@@ -12,14 +37,13 @@ DEBSIGN_CONFIG['branches'] = {
     '1.9.2': {},
 }
 DEBSIGN_CONFIG['branches']['trunk']['nightly_base_xulrunner_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-trunk'
+DEBSIGN_CONFIG['branches']['trunk']['nightly_env'] = NIGHTLY_ENV
+DEBSIGN_CONFIG['branches']['trunk']['enable_release'] = False
 DEBSIGN_CONFIG['branches']['1.9.2']['nightly_base_xulrunner_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-1.9.2'
-DEBSIGN_CONFIG['env'] = {
-    'STAGE_USERNAME': 'ffxbld',
-    'STAGE_SERVER': 'stage.mozilla.org',
-    'BASE_STAGE_PATH': '/home/ftp/pub/mozilla.org/mobile/repos',
-    'BASE_STAGE_URL': 'http://ftp.mozilla.org/pub/mozilla.org/mobile/repos',
-    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
-}
+DEBSIGN_CONFIG['branches']['1.9.2']['nightly_env'] = NIGHTLY_ENV
+DEBSIGN_CONFIG['branches']['1.9.2']['enable_release'] = True
+DEBSIGN_CONFIG['branches']['1.9.2']['release_base_xulrunner_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/1.0rc1-candidates/build1/maemo/multi'
+DEBSIGN_CONFIG['branches']['1.9.2']['release_env'] = RELEASE_ENV
 
 TRIGGER_CONFIG['sendchange_master'] = "production-mobile-master.mv.mozilla.com:9011"
 TRIGGER_CONFIG['branches'] = {
@@ -30,3 +54,5 @@ TRIGGER_CONFIG['nightly_base_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/fir
 TRIGGER_CONFIG['mobile_repo'] = 'http://hg.mozilla.org/mobile-browser'
 TRIGGER_CONFIG['locales_file'] = 'mobile/locales/all-locales'
 TRIGGER_CONFIG['conf_file'] = 'mobile/confvars.sh'
+
+TRIGGER_CONFIG['release_base_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/1.0rc1-candidates/build1/maemo'
