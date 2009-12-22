14450a79ac1df33b4e4434d804a1536248823a49
Philippe M. Chiasson <gozer@mozillamessaging.com>
2009-12-22 15:52:21 -0500

    Switch comm-1.9.1 build onto the COMM1916_20091130_RELBRANCH branch

--- thunderbird/config.py
+++ thunderbird/config.py
@@ -81,7 +81,7 @@ BRANCHES = {
 
 BRANCHES['comm-1.9.1-unittest'] = {
     #Follow the 3.0rc1 release branch until we release 3.0
-    'client_py_args' :  DEFAULTS['client_py_args'] + ['--mozilla-rev=COMM1915_20091112_RELBRANCH']
+    'client_py_args' :  DEFAULTS['client_py_args'] + ['--mozilla-rev=COMM1916_20091130_RELBRANCH']
     'factory': 'CCUnittestBuildFactory',
     'builder_type': 'check',
     'nightly': False,
@@ -167,7 +167,7 @@ BRANCHES['comm-central']['platforms'] = {
 }
 BRANCHES['comm-central']['mozilla_central_branch'] = 'releases/mozilla-1.9.1'
 #Follow the 3.0rc1 release branch until we release 3.0
-BRANCHES['comm-central']['client_py_args'] = ['--skip-comm', '--skip-chatzilla', '--skip-venkman', '--hg-options=--verbose --time'] + ['--mozilla-rev=COMM1915_20091112_RELBRANCH']
+BRANCHES['comm-central']['client_py_args'] = ['--skip-comm', '--skip-chatzilla', '--skip-venkman', '--hg-options=--verbose --time'] + ['--mozilla-rev=COMM1916_20091130_RELBRANCH']
 BRANCHES['comm-central']['cvsroot'] = ':pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot'
 BRANCHES['comm-central']['mozconfig'] = 'nightly/mozconfig'
 BRANCHES['comm-central']['package'] = True
@@ -630,7 +630,7 @@ BRANCHES['comm-central-bloat']['mozilla_central_branch'] = 'releases/mozilla-1.9
 BRANCHES['comm-central-bloat']['branch_name'] = 'comm-1.9.1'
 BRANCHES['comm-central-bloat']['hg_branch'] = 'releases/comm-1.9.1'
 #Follow the 3.0rc1 release branch until we release 3.0
-BRANCHES['comm-central-bloat']['client_py_args'] = ['--skip-comm', '--skip-chatzilla', '--skip-venkman', '--hg-options=--verbose --time'] + ['--mozilla-rev=COMM1915_20091112_RELBRANCH']
+BRANCHES['comm-central-bloat']['client_py_args'] = ['--skip-comm', '--skip-chatzilla', '--skip-venkman', '--hg-options=--verbose --time'] + ['--mozilla-rev=COMM1916_20091130_RELBRANCH']
 BRANCHES['comm-central-bloat']['cvsroot'] = ':pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot' 
 BRANCHES['comm-central-bloat']['mozconfig'] = 'debug/mozconfig'
 BRANCHES['comm-central-bloat']['nightly'] = False
