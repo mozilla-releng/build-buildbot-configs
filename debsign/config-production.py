f3baa730771f0ee5e122905f53da1dc4e4dfbce0
Aki Sasaki <asasaki@mozilla.com>
2010-01-14 18:37:25 -0800

    update trigger hacks for deb version changes. bug 538455, r=nthomas

--- debsign/config-production.py
+++ debsign/config-production.py
@@ -4,22 +4,25 @@ SLAVES = {
 NIGHTLY_ENV = {
     'STAGE_USERNAME': 'ffxbld',
     'STAGE_SERVER': 'stage.mozilla.org',
-    'BASE_STAGE_PATH': '/home/ftp/pub/mozilla.org/mobile/repos',
-    'BASE_STAGE_URL': 'http://ftp.mozilla.org/pub/mozilla.org/mobile/repos',
+    'BASE_STAGE_PATH': '/home/ftp',
+    'BASE_STAGE_URL': 'http://ftp.mozilla.org',
+    'STAGE_PATH': '/pub/mozilla.org/mobile/repos',
     'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
 }
 REAL_RELEASE_ENV = {
     'STAGE_USERNAME': 'ffxbld',
     'STAGE_SERVER': 'stage.mozilla.org',
-    'BASE_STAGE_PATH': '/home/ftp/pub/mozilla.org/mobile/repos',
-    'BASE_STAGE_URL': 'http://ftp.mozilla.org/pub/mozilla.org/mobile/repos',
+    'BASE_STAGE_PATH': '/home/ftp',
+    'BASE_STAGE_URL': 'http://ftp.mozilla.org',
+    'STAGE_DIR': '/pub/mozilla.org/mobile/repos',
     'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
 }
 CANDIDATES_RELEASE_ENV = {
     'STAGE_USERNAME': 'ffxbld',
     'STAGE_SERVER': 'stage.mozilla.org',
-    'BASE_STAGE_PATH': '/home/ftp/pub/mozilla.org/mobile/candidates/1.0rc2-candidates/repos',
-    'BASE_STAGE_URL': 'http://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/1.0rc2-candidates/repos',
+    'BASE_STAGE_PATH': '/home/ftp',
+    'BASE_STAGE_URL': 'http://ftp.mozilla.org',
+    'STAGE_DIR': '/pub/mozilla.org/mobile/candidates/1.0rc2-candidates/repos',
     'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
 }
  
@@ -36,23 +39,33 @@ DEBSIGN_CONFIG['branches'] = {
     'trunk': {},
     '1.9.2': {},
 }
-DEBSIGN_CONFIG['branches']['trunk']['nightly_base_xulrunner_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-trunk'
+DEBSIGN_CONFIG['branches']['trunk']['nightly_base_stage_path'] = '/home/ftp'
+DEBSIGN_CONFIG['branches']['trunk']['nightly_base_stage_url'] = 'http://ftp.mozilla.org'
+DEBSIGN_CONFIG['branches']['trunk']['nightly_stage_dir'] = '/pub/mozilla.org/firefox/nightly/latest-mobile-trunk'
 DEBSIGN_CONFIG['branches']['trunk']['nightly_env'] = NIGHTLY_ENV
 DEBSIGN_CONFIG['branches']['trunk']['enable_release'] = False
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly_base_xulrunner_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-1.9.2'
+DEBSIGN_CONFIG['branches']['1.9.2']['nightly_base_stage_path'] = '/home/ftp'
+DEBSIGN_CONFIG['branches']['1.9.2']['nightly_base_stage_url'] = 'http://ftp.mozilla.org'
+DEBSIGN_CONFIG['branches']['1.9.2']['nightly_stage_dir'] = '/pub/mozilla.org/firefox/nightly/latest-mobile-1.9.2'
 DEBSIGN_CONFIG['branches']['1.9.2']['nightly_env'] = NIGHTLY_ENV
 DEBSIGN_CONFIG['branches']['1.9.2']['enable_release'] = True
-DEBSIGN_CONFIG['branches']['1.9.2']['release_base_xulrunner_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/1.0rc2-candidates/build3/maemo/multi'
+DEBSIGN_CONFIG['branches']['1.9.2']['release_base_stage_path'] = '/home/ftp'
+DEBSIGN_CONFIG['branches']['1.9.2']['release_base_stage_url'] = 'http://ftp.mozilla.org'
+DEBSIGN_CONFIG['branches']['1.9.2']['release_stage_dir'] = '/pub/mozilla.org/mobile/candidates/1.0rc2-candidates/build3/maemo'
 DEBSIGN_CONFIG['branches']['1.9.2']['release_env'] = RELEASE_ENV
+
+#
+# Trigger config
+#
 
 TRIGGER_CONFIG['sendchange_master'] = "production-mobile-master.build.mozilla.org:9011"
 TRIGGER_CONFIG['branches'] = {
     'trunk': {},
     '1.9.2': {},
 }
-TRIGGER_CONFIG['nightly_base_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-'
+TRIGGER_CONFIG['branches']['trunk']['env'] = NIGHTLY_ENV
+TRIGGER_CONFIG['branches']['1.9.2']['env'] = NIGHTLY_ENV
+TRIGGER_CONFIG['release_mobile_tag'] = 'FENNEC_1_0rc2_RELEASE'
 TRIGGER_CONFIG['mobile_repo'] = 'http://hg.mozilla.org/mobile-browser'
 TRIGGER_CONFIG['locales_file'] = 'mobile/locales/all-locales'
 TRIGGER_CONFIG['conf_file'] = 'mobile/confvars.sh'
-
-TRIGGER_CONFIG['release_base_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/1.0rc2-candidates/build3/maemo'
--- debsign/config-staging.py
+++ debsign/config-staging.py
@@ -1,28 +1,39 @@
 SLAVES = {
     'linux': ['debsign1', 'debsign2'],
 }
+
 NIGHTLY_ENV = {
     'STAGE_USERNAME': 'cltbld',
     'STAGE_SERVER': 'staging-mobile-master.build.mozilla.org',
-    'BASE_STAGE_PATH': '/var/www/html/debtest',
-    'BASE_STAGE_URL': 'http://staging-mobile-master.build.mozilla.org/debtest',
+    'BASE_STAGE_PATH': '/var/www/html',
+    'BASE_STAGE_URL': 'http://staging-mobile-master.build.mozilla.org',
+    'STAGE_DIR': '/debtest',
     'SSH_KEY': '/home/cltbld/.ssh/id_rsa',
     'SIGNDEBS_BASEDIR': 'sign-debs-staging',
+}
+
+# Needed in staging only
+NIGHTLY_TRIGGER_ENV = {
+    'STAGE_USERNAME': 'ffxbld',
+    'STAGE_SERVER': 'stage.mozilla.org',
+    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
 }
 
 REAL_RELEASE_ENV = {
     'STAGE_USERNAME': 'cltbld',
     'STAGE_SERVER': 'staging-mobile-master.build.mozilla.org',
-    'BASE_STAGE_PATH': '/var/www/html/debtest',
+    'BASE_STAGE_PATH': '/var/www/html',
     'BASE_STAGE_URL': 'http://staging-mobile-master.build.mozilla.org/debtest',
+    'STAGE_DIR': '/debtest',
     'SSH_KEY': '/home/cltbld/.ssh/id_rsa',
     'SIGNDEBS_BASEDIR': 'sign-debs-staging',
 }
 CANDIDATES_RELEASE_ENV = {
     'STAGE_USERNAME': 'cltbld',
     'STAGE_SERVER': 'staging-mobile-master.build.mozilla.org',
-    'BASE_STAGE_PATH': '/var/www/html/debtest/candidates',
-    'BASE_STAGE_URL': 'http://staging-mobile-master.build.mozilla.org/debtest/candidates',
+    'BASE_STAGE_PATH': '/var/www/html',
+    'BASE_STAGE_URL': 'http://staging-mobile-master.build.mozilla.org',
+    'STAGE_DIR': '/debtest/candidates',
     'SSH_KEY': '/home/cltbld/.ssh/id_rsa',
     'SIGNDEBS_BASEDIR': 'sign-debs-staging',
 }
@@ -35,28 +46,38 @@ DEBSIGN_CONFIG = {}
 TRIGGER_CONFIG = {}
 
 DEBSIGN_CONFIG['slaves'] = SLAVES['linux']
-DEBSIGN_CONFIG['tools_repo_path'] = "http://hg.mozilla.org/users/asasaki_mozilla.com/test-tools"
+DEBSIGN_CONFIG['tools_repo_path'] = "http://hg.mozilla.org/build/tools"
 DEBSIGN_CONFIG['branches'] = {
     'trunk': {},
     '1.9.2': {},
 }
-DEBSIGN_CONFIG['branches']['trunk']['nightly_base_xulrunner_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-trunk'
+DEBSIGN_CONFIG['branches']['trunk']['nightly_base_stage_path'] = '/home/ftp'
+DEBSIGN_CONFIG['branches']['trunk']['nightly_base_stage_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-trunk'
+DEBSIGN_CONFIG['branches']['trunk']['nightly_stage_dir'] = '/pub/mozilla.org/firefox/nightly/latest-mobile-trunk'
 DEBSIGN_CONFIG['branches']['trunk']['nightly_env'] = NIGHTLY_ENV
 DEBSIGN_CONFIG['branches']['trunk']['enable_release'] = False
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly_base_xulrunner_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-1.9.2'
+DEBSIGN_CONFIG['branches']['1.9.2']['nightly_base_stage_path'] = '/home/ftp'
+DEBSIGN_CONFIG['branches']['1.9.2']['nightly_base_stage_url'] = 'http://ftp.mozilla.org'
+DEBSIGN_CONFIG['branches']['1.9.2']['nightly_stage_dir'] = '/pub/mozilla.org/firefox/nightly/latest-mobile-1.9.2'
 DEBSIGN_CONFIG['branches']['1.9.2']['nightly_env'] = NIGHTLY_ENV
 DEBSIGN_CONFIG['branches']['1.9.2']['enable_release'] = True
-DEBSIGN_CONFIG['branches']['1.9.2']['release_base_xulrunner_url'] = 'http://staging-mobile-master.build.mozilla.org/candidates/1.0-candidates/build1/maemo/multi'
+DEBSIGN_CONFIG['branches']['trunk']['release_base_stage_path'] = '/var/www/html'
+DEBSIGN_CONFIG['branches']['1.9.2']['release_base_stage_url'] = 'http://staging-mobile-master.build.mozilla.org'
+DEBSIGN_CONFIG['branches']['1.9.2']['release_stage_dir'] = '/candidates/1.0rc2-candidates/build3/maemo'
 DEBSIGN_CONFIG['branches']['1.9.2']['release_env'] = RELEASE_ENV
+
+#
+# Trigger config
+#
 
 TRIGGER_CONFIG['sendchange_master'] = "staging-mobile-master.build.mozilla.org:9011"
 TRIGGER_CONFIG['branches'] = {
     'trunk': {},
     '1.9.2': {},
 }
-TRIGGER_CONFIG['nightly_base_url'] = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mobile-'
+TRIGGER_CONFIG['branches']['trunk']['env'] = NIGHTLY_TRIGGER_ENV
+TRIGGER_CONFIG['branches']['1.9.2']['env'] = NIGHTLY_TRIGGER_ENV
+TRIGGER_CONFIG['branches']['1.9.2']['release_mobile_tag'] = 'FENNEC_1_0rc2_RELEASE'
 TRIGGER_CONFIG['mobile_repo'] = 'http://hg.mozilla.org/mobile-browser'
 TRIGGER_CONFIG['locales_file'] = 'mobile/locales/all-locales'
 TRIGGER_CONFIG['conf_file'] = 'mobile/confvars.sh'
-
-TRIGGER_CONFIG['release_base_url'] = 'http://staging-mobile-master.build.mozilla.org/candidates/1.0-candidates/build1/maemo'
