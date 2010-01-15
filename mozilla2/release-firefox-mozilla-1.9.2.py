90dc4700e24c0c88869f668e8de40464240d0378
Chris AtLee <catlee@mozilla.com>
2010-01-15 15:19:07 -0500

    Bug540000: Automation bumps for build and release of Firefox 3.6rc2 r=coop

--- mozilla2/release-firefox-mozilla-1.9.2.py
+++ mozilla2/release-firefox-mozilla-1.9.2.py
@@ -2,8 +2,8 @@ hgUsername         = 'ffxbld'
 hgSshKey           = '~cltbld/.ssh/ffxbld_dsa'
 sourceRepoName     = 'mozilla-1.9.2'
 sourceRepoPath     = 'releases/mozilla-1.9.2'
-sourceRepoRevision = '0f70d1d535dc'
-relbranchOverride  = ''
+sourceRepoRevision = '448d0d2d310c'
+relbranchOverride  = 'GECKO192_20100105_RELBRANCH'
 l10nRepoPath       = 'releases/l10n-mozilla-1.9.2'
 l10nRevisionFile   = 'l10n-changesets_mozilla-1.9.2'
 cvsroot            = ':ext:cltbld@cvs.mozilla.org:/cvsroot'
@@ -14,15 +14,15 @@ appName            = 'browser'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version            = '3.6rc1'
+version            = '3.6rc2'
 appVersion         = '3.6'
 milestone          = '1.9.2'
 buildNumber        = 1
-baseTag            = 'FIREFOX_3_6rc1'
-oldVersion         = '3.6b5'
+baseTag            = 'FIREFOX_3_6rc2'
+oldVersion         = '3.6rc1'
 oldAppVersion      = oldVersion
 oldBuildNumber     = 1
-oldBaseTag         = 'FIREFOX_3_6b5'
+oldBaseTag         = 'FIREFOX_3_6rc1'
 enUSPlatforms      = ('linux', 'win32', 'macosx', 'wince')
 l10nPlatforms      = ('linux', 'win32', 'macosx')
 patcherConfig      = 'moz192-branch-patcher2.cfg'
