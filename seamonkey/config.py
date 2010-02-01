a8e9dbb7d49bd5361898c27f4e9c67520650e78c
Robert Kaiser <kairo@kairo.at>
2010-02-01 17:29:39 +0100

    set graph server back to None, this doesn't work yet, need more work done in bug 492406

--- seamonkey/config.py
+++ seamonkey/config.py
@@ -46,7 +46,7 @@ BRANCH_LEVEL_VARS = {
     'aus2_user': 'seabld',
     'aus2_host': 'aus2-community.mozilla.org',
     'download_base_url': 'http://ftp.mozilla.org/pub/mozilla.org/seamonkey',
-    'graph_server': 'graphs-stage.mozilla.org',
+    'graph_server': None, #'graphs-stage.mozilla.org',
     'graph_selector': '/server/collect.cgi',
     'build_tools_repo_path': 'build/tools',
     'compare_locales_repo_path': 'build/compare-locales',
@@ -447,6 +447,8 @@ BRANCHES['comm-1.9.2']['app_name'] = 'suite'
 BRANCHES['comm-1.9.2']['brand_name'] = 'SeaMonkey'
 BRANCHES['comm-1.9.2']['start_hour'] = [0]
 BRANCHES['comm-1.9.2']['start_minute'] = [15]
+# Force graphs off for now, this branch is still experimental
+BRANCHES['comm-1.9.2']['graph_server'] = None
 # All platforms being built for this branch MUST be listed here.
 BRANCHES['comm-1.9.2']['platforms'] = {
     'linux': {},
