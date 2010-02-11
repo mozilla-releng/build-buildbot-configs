f900d2bfb644a4ce9f9330d23542ac3db585bc4e
Ben Hearsum <bhearsum@mozilla.com>
2010-02-11 10:10:47 -0500

    bug 541953: bring new win32 ix machines up to date after they arrive - update buildbot-configs with new slave names. r=catlee.
    --HG--
    extra : rebase_source : 61ed7a83d96f8db52537de7c2a8d2a239041711c

--- mozilla2-staging/config.py
+++ mozilla2-staging/config.py
@@ -9,9 +9,9 @@ from buildbotcustom.env import MozillaEnvironments
 MAC_MINIS = ['moz2-darwin9-slave%02i' % x for x in range(1,38)]
 XSERVES   = ['bm-xserve%02i' % x for x in [7,9,11,12,16,17,18,19,21,22]]
 WIN32_VMS = ['win32-slave%02i' % x for x in range(1,60)]
-WIN32_IXS = ['w32-ix-slave%02i' % x for x in range(1,26)]
+WIN32_IXS = ['mw32-ix-slave%02i' % x for x in range(1,26)]
 LINUX_VMS = ['moz2-linux-slave%02i' % x for x in range(1,51)]
-LINUX_IXS = ['moz2-linux-ix-slave%02i' % x for x in range(1,26)]
+LINUX_IXS = ['mv-moz2-linux-ix-slave%02i' % x for x in range(1,26)]
 SLAVES = {
     'linux': LINUX_VMS + LINUX_IXS,
     'linux64': ['moz2-linux64-slave%02i' % x for x in range(1,13)],
