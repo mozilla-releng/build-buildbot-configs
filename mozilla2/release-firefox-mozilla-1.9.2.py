2e1afcd6fc7e9ffb32189ae6fb3dac3a6f52b5fb
Ben Hearsum <bhearsum@mozilla.com>
2010-04-13 16:49:28 -0400

    bug 558769: tracking bug for build and release of firefox 3.6.4 - 3.6.4 config bumps. r=catlee
    --HG--
    extra : rebase_source : 44835aa1d8a7a60bda42ad3d43d26ed43470a40b

--- mozilla2/release-firefox-mozilla-1.9.2.py
+++ mozilla2/release-firefox-mozilla-1.9.2.py
@@ -2,8 +2,8 @@ hgUsername         = 'ffxbld'
 hgSshKey           = '~cltbld/.ssh/ffxbld_dsa'
 sourceRepoName     = 'mozilla-1.9.2'
 sourceRepoPath     = 'releases/mozilla-1.9.2'
-sourceRepoRevision = '3093de37377d'
-relbranchOverride  = 'GECKO1922_20100315_RELBRANCH'
+sourceRepoRevision = '0121ed69d08e'
+relbranchOverride  = ''
 l10nRepoPath       = 'releases/l10n-mozilla-1.9.2'
 l10nRevisionFile   = 'l10n-changesets_mozilla-1.9.2'
 cvsroot            = ':ext:cltbld@cvs.mozilla.org:/cvsroot'
@@ -14,15 +14,15 @@ appName            = 'browser'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version            = '3.6.3'
+version            = '3.6.4'
 appVersion         = version 
-milestone          = '1.9.2.3'
+milestone          = '1.9.2.4'
 buildNumber        = 1
-baseTag            = 'FIREFOX_3_6_3'
-oldVersion         = '3.6.2'
-oldAppVersion      = '3.6.2'
-oldBuildNumber     = 3
-oldBaseTag         = 'FIREFOX_3_6_2'
+baseTag            = 'FIREFOX_3_6_4'
+oldVersion         = '3.6.3'
+oldAppVersion      = '3.6.3'
+oldBuildNumber     = 1
+oldBaseTag         = 'FIREFOX_3_6_3'
 enUSPlatforms      = ('linux', 'win32', 'macosx')
 l10nPlatforms      = ('linux', 'win32', 'macosx')
 talosTestPlatforms = ('linux', 'win32', 'macosx')
