489e3734cccba28c32e8b6bd2c8705e2ae8a34d8
Aki Sasaki <asasaki@mozilla.com>
2010-04-14 15:34:30 -0700

    1.1b1 changesets etc. bug 557905, r=catlee

--- mozilla2/release-fennec-1.1.py
+++ mozilla2/release-fennec-1.1.py
@@ -6,10 +6,10 @@ mozSourceRepoName      = 'mozilla-1.9.2'
 # because the repo_setup builder needs to know where to clone repositories from.
 # It is not used for anything else.
 mozSourceRepoPath      = 'releases/mozilla-1.9.2'
-mozSourceRepoRevision  = 'FILLMEIN'
+mozSourceRepoRevision  = 'c846b94321ee'
 mobileSourceRepoName      = 'mobile-browser'
 mobileSourceRepoPath      = 'mobile-browser'
-mobileSourceRepoRevision  = 'FILLMEIN'
+mobileSourceRepoRevision  = '351d825a6c6f'
 mozRelbranchOverride      = ''
 l10nRelbranchOverride     = ''
 mobileRelbranchOverride   = ''
@@ -17,15 +17,15 @@ l10nRepoPath        = 'releases/l10n-mozilla-1.9.2'
 l10nRevisionFile    = 'l10n-changesets_mobile-1.1.json'
 productName         = 'fennec'
 appName             = 'mobile'
-mergeLocales        = False
+mergeLocales        = True
 # Sometimes we need the application version to be different from what we "call"
 # the build, eg public release candidates for a major release (3.1 RC1).
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
 version             = '1.1b1'
-appVersion          = '1.1'
-milestone           = '1.9.2.3'
+appVersion          = '1.1b1'
+milestone           = '1.9.2.5pre'
 buildNumber         = 1
 baseTag             = 'FENNEC_1_1b1'
 enUSPlatforms       = ('maemo',)
@@ -35,6 +35,6 @@ ftpServer           = 'ftp.mozilla.org'
 stagingServer       = 'stage.mozilla.org'
 stageBasePath       = '/home/ftp/pub/mobile/candidates'
 base_enUS_binaryURL = 'http://%s/pub/mozilla.org/mobile/candidates/%s-candidates/build%d' % (ftpServer, version, buildNumber)
-doPartnerRepacks    = True
+doPartnerRepacks    = False
 partnersRepoPath    = 'build/partner-repacks'
 partnerRepackPlatforms = ('maemo',)
