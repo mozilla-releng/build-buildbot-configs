e12ced7ed001fdbc86bc2dea98ae1df857d06e88
Philippe M. Chiasson <gozer@mozillamessaging.com>
2010-04-22 15:19:06 -0400

    Ready build configuration for Lightning 1.0b2 builds

--- thunderbird/release_calendar.py
+++ thunderbird/release_calendar.py
@@ -1,26 +1,26 @@
 hgUsername                 = 'calbld'
 hgSshKey                   = '~calbld/.ssh/calbld_dsa'
 relbranchPrefix            = 'COMM'
-sourceRepoName             = 'comm-1.9.1-sunbird' # buildbot branch name
-sourceRepoPath             = 'releases/comm-1.9.1'
-sourceRepoRevision         = '92f94a04799e'
+sourceRepoName             = 'comm-1.9.2-lightning' # buildbot branch name
+sourceRepoPath             = 'comm-central'
+sourceRepoRevision         = '19d5618679fe'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
-relbranchOverride          = 'COMM1915_20091204_RELBRANCH'
-mozillaRepoPath            = 'releases/mozilla-1.9.1'
-mozillaRepoRevision        = '6dc036c10334'
+relbranchOverride          = ''
+mozillaRepoPath            = 'releases/mozilla-1.9.2'
+mozillaRepoRevision        = 'dcf8e50115bc'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 # You typically want to set this to the gecko relbranch if doing a release off
 # a specific gecko version.
-mozillaRelbranchOverride   = 'COMM1915_20091112_RELBRANCH' # put Gecko relbranch here that we base upon
+mozillaRelbranchOverride   = ''
 inspectorRepoPath          = 'dom-inspector' # leave empty if inspector is not to be tagged
-inspectorRepoRevision      = '18a1c983c8ee'
-inspectorRelbranchOverride = 'COMM1915_20091112_RELBRANCH'
+inspectorRepoRevision      = '27b0e9ef1715'
+inspectorRelbranchOverride = ''
 venkmanRepoPath            = '' # leave empty if venkman is not to be tagged
 venkmanRepoRevision        = ''
 venkmanRelbranchOverride   = ''
 chatzillaCVSRoot           = ''
 chatzillaTimestamp         = '' # leave empty if chatzilla is not to be tagged
-l10nRepoPath               = 'releases/l10n-mozilla-1.9.1'
+l10nRepoPath               = 'releases/l10n-mozilla-1.9.2'
 l10nRevisionFile           = 'l10n-calendar-changesets'
 toolsRepoPath              = 'build/tools'
 cvsroot                    = ':ext:calbld@cvs.mozilla.org:/cvsroot' # for patcher, etc.
@@ -34,12 +34,12 @@ projectName                = 'calendar'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '1.0b1'
-appVersion                 = '1.0b1'
+version                    = '1.0b2'
+appVersion                 = '1.0b2'
 #XXX: Not entirely certain if/where this is used.
-milestone                  = '1.9.1.5'
-buildNumber                = 6
-baseTag                    = 'SUNBIRD_1_0b1'
+milestone                  = '1.9.2.5'
+buildNumber                = 1
+baseTag                    = 'CALENDAR_1_0b2'
 # The old version is the revision from which we should generate update snippets.
 oldVersion                 = ''
 oldAppVersion              = oldVersion
