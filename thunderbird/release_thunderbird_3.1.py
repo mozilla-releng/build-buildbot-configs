b5d8920c86d81f97eeb96d99660cae31756bcb4a
Philippe M. Chiasson <gozer@mozillamessaging.com>
2010-04-27 14:25:03 -0400

    Build configuration for Thunderbird 3.1b2 (en-US only)

--- thunderbird/release_thunderbird_3.1.py
+++ thunderbird/release_thunderbird_3.1.py
@@ -2,18 +2,18 @@ hgUsername                 = 'tbirdbld'
 hgSshKey                   = '~cltbld/.ssh/tbirdbld_dsa'
 relbranchPrefix            = 'COMM'
 sourceRepoName             = 'comm-1.9.2' # buildbot branch name
-sourceRepoPath             = 'comm-central'
-sourceRepoRevision         = '8a57290de34e'
+sourceRepoPath             = 'releases/comm-1.9.2'
+sourceRepoRevision         = 'e54fc70745ba'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 relbranchOverride          = ''
 mozillaRepoPath            = 'releases/mozilla-1.9.2'
-mozillaRepoRevision        = 'f20ea707314c'
+mozillaRepoRevision        = 'afa51044ecf0'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 # You typically want to set this to the gecko relbranch if doing a release off
 # a specific gecko version.
-mozillaRelbranchOverride   = 'COMM192_20100302_RELBRANCH' # put Gecko relbranch here that we base upon
+mozillaRelbranchOverride   = '' # put Gecko relbranch here that we base upon
 inspectorRepoPath          = 'dom-inspector' # leave empty if inspector is not to be tagged
-inspectorRepoRevision      = '2bbdd20af50a'
+inspectorRepoRevision      = '27b0e9ef1715'
 inspectorRelbranchOverride = ''
 venkmanRepoPath            = '' # leave empty if venkman is not to be tagged
 venkmanRepoRevision        = ''
@@ -26,7 +26,7 @@ toolsRepoPath              = 'build/tools'
 cvsroot                    = ':ext:cltbld@cvs.mozilla.org:/cvsroot' # for patcher, etc.
 productVersionFile         = 'mail/config/version-192.txt'
 productName                = 'thunderbird'
-brandName                  = 'Lanikai'
+brandName                  = 'Thunderbird'
 appName                    = 'mail'
 ftpName                    = appName
 # Sometimes we need the application version to be different from what we "call"
@@ -34,17 +34,17 @@ ftpName                    = appName
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '3.1b1'
+version                    = '3.1b2'
 appVersion                 = version
 #XXX: Not entirely certain if/where this is used.
-milestone                  = '1.9.2.2'
+milestone                  = '1.9.2.5'
 buildNumber                = 1
-baseTag                    = 'THUNDERBIRD_3_1_b1'
+baseTag                    = 'THUNDERBIRD_3_1_b2'
 # The old version is the revision from which we should generate update snippets.
-oldVersion                 = '3.1a1'
+oldVersion                 = '3.1b1'
 oldAppVersion              = oldVersion
-oldBuildNumber             = 3
-oldBaseTag                 = 'THUNDERBIRD_3_1_a1'
+oldBuildNumber             = 1
+oldBaseTag                 = 'THUNDERBIRD_3_1_b1'
 releasePlatforms           = ('linux', 'win32', 'macosx')
 patcherConfig              = 'moz192-thunderbird-branch-patcher2.cfg'
 patcherToolsTag            = 'UPDATE_PACKAGING_R9'
