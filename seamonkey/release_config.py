206a0a5ebb456eabeab25d8ed9fa3ca70e178f1a
Robert Kaiser <kairo@kairo.at>
2010-05-04 20:12:47 +0200

    update release configs and l10n-changesets for 2.0.5 release

--- seamonkey/release_config.py
+++ seamonkey/release_config.py
@@ -3,11 +3,11 @@ hgSshKey                   = '~seabld/.ssh/seabld_dsa'
 relbranchPrefix            = 'COMM'
 sourceRepoName             = 'comm-1.9.1' # buildbot branch name
 sourceRepoPath             = 'releases/comm-1.9.1'
-sourceRepoRevision         = 'ec9aba945dab'
-relbranchOverride          = 'COMM1919_20100317_RELBRANCH'
+sourceRepoRevision         = 'd0c19c3b0144'
+relbranchOverride          = ''
 mozillaRepoPath            = 'releases/mozilla-1.9.1'
-mozillaRepoRevision        = 'FIREFOX_3_5_9_RELEASE'
-mozillaRelbranchOverride   = 'GECKO1919_20100315_RELBRANCH' # put Gecko relbranch here that we base upon
+mozillaRepoRevision        = 'FIREFOX_3_5_10_RELEASE'
+mozillaRelbranchOverride   = 'GECKO19110_20100504_RELBRANCH' # put Gecko relbranch here that we base upon
 inspectorRepoPath          = 'dom-inspector' # leave empty if inspector is not to be tagged
 inspectorRepoRevision      = 'f6c78804ebb4'
 inspectorRelbranchOverride = 'COMM_1_9_1_BRANCH'
@@ -29,15 +29,15 @@ appName                    = 'suite'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '2.0.4'
+version                    = '2.0.5'
 appVersion                 = version
-milestone                  = '1.9.1.9'
+milestone                  = '1.9.1.10'
 buildNumber                = 1
-baseTag                    = 'SEAMONKEY_2_0_4'
-oldVersion                 = '2.0.3'
+baseTag                    = 'SEAMONKEY_2_0_5'
+oldVersion                 = '2.0.4'
 oldAppVersion              = oldVersion
 oldBuildNumber             = 1
-oldBaseTag                 = 'SEAMONKEY_2_0_3'
+oldBaseTag                 = 'SEAMONKEY_2_0_4'
 enUSPlatforms              = ('linux', 'linux64', 'win32', 'macosx')
 l10nPlatforms              = ('linux', 'win32', 'macosx')
 patcherConfig              = 'moz191-seamonkey-branch-patcher2.cfg'
