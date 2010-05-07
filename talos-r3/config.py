72c764f8dfcce0e0805777d2881b37eb45e39ffd
John Ford <jhford@mozilla.com>
2010-05-07 15:01:03 -0700

    bug 557294 - add new talos-r3 minis to master configs r=catlee p=jhford

--- talos-r3/config.py
+++ talos-r3/config.py
@@ -24,12 +24,12 @@ SUITES = {
 }
 
 SLAVES = {
-    'fedora': ["talos-r3-fed-%03i" % x for x in range(1,41)],
-    'fedora64' : ["talos-r3-fed64-%03i" % x for x in range (1,21)],
-    'xp': ["talos-r3-xp-%03i" % x for x in range(1,41)],
-    'win7': ["talos-r3-w7-%03i" % x for x in range(1,41)],
-    'leopard': ["talos-r3-leopard-%03i" % x for x in range(1,41)],
-    'snowleopard': ["talos-r3-snow-%03i" % x for x in range(1,21)],
+    'fedora': ["talos-r3-fed-%03i" % x for x in range(1,51)],
+    'fedora64' : ["talos-r3-fed64-%03i" % x for x in range (1,51)],
+    'xp': ["talos-r3-xp-%03i" % x for x in range(1,51)],
+    'win7': ["talos-r3-w7-%03i" % x for x in range(1,51)],
+    'leopard': ["talos-r3-leopard-%03i" % x for x in range(1,51)],
+    'snowleopard': ["talos-r3-snow-%03i" % x for x in range(1,51)],
 }
 
 BRANCHES = {
