56309b917f79e7098e99a9a4e25f02b8ce32867e
Chris AtLee <catlee@mozilla.com>
2010-05-19 09:51:42 -0400

    talos-master02 shouldn't be in staging configs

--- mozilla2-staging/config.py
+++ mozilla2-staging/config.py
@@ -60,7 +60,6 @@ GLOBAL_VARS = {
     'talos_masters': [
         ('talos-staging-master02.build.mozilla.org:9010', False),
         ('talos-staging-master02.build.mozilla.org:9012', False),
-        ('talos-master02.build.mozilla.org:9010', False),
     ],
     # List of unittest masters to notify of new builds to test,
     # and if a failure to notify the master should result in a warning
