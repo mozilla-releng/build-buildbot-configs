24b9f1d030b1454e7d6a69bc541e05653687c6ee
Philipp Kewisch <mozilla@kewis.ch>
2010-05-25 18:59:19 +0200

    Prepare for Lightning 1.0b2 release

--- calendar/release_calendar.py
+++ calendar/release_calendar.py
@@ -3,7 +3,7 @@ hgSshKey                   = '~calbld/.ssh/calbld_dsa'
 relbranchPrefix            = 'COMM'
 sourceRepoName             = 'comm-1.9.2-lightning' # buildbot branch name
 sourceRepoPath             = 'releases/comm-1.9.2'
-sourceRepoRevision         = '36491163e98a'
+sourceRepoRevision         = '80b719ebe194'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 relbranchOverride          = ''
 mozillaRepoPath            = 'releases/mozilla-1.9.2'
