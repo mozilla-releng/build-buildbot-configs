fe1b72586090b6385078f5d9dfa905293bcb82f7
Mark Banner <bugzilla@standard8.plus.com>
2010-06-08 08:46:11 +0100

    Correct c-1.9.2 revision for TB 3.1 rc2

--- thunderbird/release_thunderbird_3.1.py
+++ thunderbird/release_thunderbird_3.1.py
@@ -3,7 +3,7 @@ hgSshKey                   = '~cltbld/.ssh/tbirdbld_dsa'
 relbranchPrefix            = 'COMM'
 sourceRepoName             = 'comm-1.9.2' # buildbot branch name
 sourceRepoPath             = 'releases/comm-1.9.2'
-sourceRepoRevision         = '8092632c4f66'
+sourceRepoRevision         = '04a804c3584c'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 relbranchOverride          = 'COMM1924_20100519_RELBRANCH'
 mozillaRepoPath            = 'releases/mozilla-1.9.2'
