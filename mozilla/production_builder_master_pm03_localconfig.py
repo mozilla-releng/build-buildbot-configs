0b1665800696599e4d0174d2c7bc07eaba19ba8c
Lukas Blakk <lsblakk@mozilla.com>
2010-06-18 15:42:03 -0700

    De-activating mozilla-1.9.3 branch for refactoring
    --HG--
    branch : buildbot-0.8.0

--- mozilla/production_builder_master_pm03_localconfig.py
+++ mozilla/production_builder_master_pm03_localconfig.py
@@ -13,7 +13,7 @@ c['manhole'] = manhole.PasswordManhole("tcp:1235:interface=127.0.0.1", "cltbld",
 
 from config import BRANCHES, SLAVES
 ACTIVE_BRANCHES = ['places', 'addonsmgr', 'electrolysis', 'tracemonkey',
-    'mozilla-1.9.1', 'mozilla-1.9.2', 'mozilla-1.9.3', 'mozilla-central',
+    'mozilla-1.9.1', 'mozilla-1.9.2', 'mozilla-central',
     'maple', 'cedar', 'birch']
 
 # Set up our fast slaves
