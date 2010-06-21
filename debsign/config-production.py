3b024142f59f662e50604de1f320e5a8f733fef5
Aki Sasaki <asasaki@mozilla.com>
2010-06-21 11:00:50 -0700

    multi deb repos!! bug 563081, r=jhford
    --HG--
    extra : rebase_source : 4a25bd8bb1f89ab06fffb6c18d061708955f0c42

--- debsign/config-production.py
+++ debsign/config-production.py
@@ -1,75 +1,19 @@
 SLAVES = {
-    'linux': ['debsign1'],
+    "linux": ["debsign1"],
 }
-NIGHTLY_ENV = {
-    'STAGE_USERNAME': 'ffxbld',
-    'STAGE_SERVER': 'stage.mozilla.org',
-    'BASE_STAGE_PATH': '/home/ftp',
-    'BASE_STAGE_URL': 'http://ftp.mozilla.org',
-    'STAGE_DIR': '/pub/mozilla.org/mobile/repos',
-    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
-}
-REAL_RELEASE_ENV = {
-    'STAGE_USERNAME': 'ffxbld',
-    'STAGE_SERVER': 'stage.mozilla.org',
-    'BASE_STAGE_PATH': '/home/ftp',
-    'BASE_STAGE_URL': 'http://ftp.mozilla.org',
-    'STAGE_DIR': '/pub/mozilla.org/mobile/repos',
-    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
-}
-CANDIDATES_RELEASE_ENV = {
-    'STAGE_USERNAME': 'ffxbld',
-    'STAGE_SERVER': 'stage.mozilla.org',
-    'BASE_STAGE_PATH': '/home/ftp',
-    'BASE_STAGE_URL': 'http://ftp.mozilla.org',
-    'STAGE_DIR': '/pub/mozilla.org/mobile/candidates/1.1rc1-candidates/repos',
-    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
-}
- 
 
-# Change for real release
-RELEASE_ENV = CANDIDATES_RELEASE_ENV
-
-DEBSIGN_CONFIG = {}
-TRIGGER_CONFIG = {}
-
-DEBSIGN_CONFIG['slaves'] = SLAVES['linux']
-DEBSIGN_CONFIG['tools_repo_path'] = "http://hg.mozilla.org/build/tools"
-DEBSIGN_CONFIG['branches'] = {
-    'trunk': {'nightly': {},
-              'release': {}},
-    '1.9.2': {'nightly': {},
-              'release': {}},
+BRANCHES = {
+    "mozilla-central": {},
+    "mozilla-1.9.2": {},
 }
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['base_stage_path'] = '/home/ftp'
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['base_stage_url'] = 'http://ftp.mozilla.org'
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['stage_dir'] = '/pub/mozilla.org/mobile/nightly/latest-mobile-trunk'
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['env'] = NIGHTLY_ENV
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['extra_debs_list'] = None
-DEBSIGN_CONFIG['branches']['trunk']['enable_release'] = False
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['base_stage_path'] = '/home/ftp'
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['base_stage_url'] = 'http://ftp.mozilla.org'
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['stage_dir'] = '/pub/mozilla.org/mobile/nightly/latest-mobile-1.9.2'
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['env'] = NIGHTLY_ENV
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['extra_debs_list'] = None
-DEBSIGN_CONFIG['branches']['1.9.2']['enable_release'] = True
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['base_stage_path'] = '/home/ftp'
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['base_stage_url'] = 'http://ftp.mozilla.org'
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['stage_dir'] = '/pub/mozilla.org/mobile/candidates/1.1rc1-candidates/build2/maemo4'
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['env'] = RELEASE_ENV
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['extra_debs_list'] = []
+BRANCHES["mozilla-central"]["script_repo_tag"] = 'PRODUCTION'
+BRANCHES["mozilla-central"]["nightly_config_file"] = "deb_repos/trunk_nightly.json"
+BRANCHES["mozilla-central"]["nightly_platforms"] = ["chinook", "fremantle"]
+BRANCHES["mozilla-central"]["enable_release"] = False
 
-#
-# Trigger config
-#
-
-TRIGGER_CONFIG['sendchange_master'] = "production-mobile-master.build.mozilla.org:9011"
-TRIGGER_CONFIG['branches'] = {
-    'trunk': {},
-    '1.9.2': {},
-}
-TRIGGER_CONFIG['branches']['trunk']['env'] = NIGHTLY_ENV
-TRIGGER_CONFIG['branches']['1.9.2']['env'] = NIGHTLY_ENV
-TRIGGER_CONFIG['branches']['1.9.2']['release_mobile_tag'] = 'FENNEC_1_1rc1_RELEASE'
-TRIGGER_CONFIG['mobile_repo'] = 'http://hg.mozilla.org/releases/mobile-1.1'
-TRIGGER_CONFIG['locales_file'] = 'mobile/locales/all-locales'
+BRANCHES["mozilla-1.9.2"]["script_repo_tag"] = 'PRODUCTION'
+BRANCHES["mozilla-1.9.2"]["nightly_config_file"] = "deb_repos/1.1_nightly.json"
+BRANCHES["mozilla-1.9.2"]["nightly_platforms"] = ["chinook", "fremantle"]
+BRANCHES["mozilla-1.9.2"]["enable_release"] = True
+BRANCHES["mozilla-1.9.2"]["release_config_file"] = "deb_repos/1.1_release.json"
+BRANCHES["mozilla-1.9.2"]["release_platforms"] = ["chinook", "fremantle"]
--- debsign/config-staging.py
+++ debsign/config-staging.py
@@ -1,87 +1,19 @@
 SLAVES = {
-    'linux': ['debsign1', 'debsign2'],
+    "linux": ["debsign1", "debsign2"],
 }
 
-NIGHTLY_ENV = {
-    'STAGE_USERNAME': 'cltbld',
-    'STAGE_SERVER': 'staging-mobile-master.build.mozilla.org',
-    'BASE_STAGE_PATH': '/var/www/html',
-    'BASE_STAGE_URL': 'http://staging-mobile-master.build.mozilla.org',
-    'STAGE_DIR': '/debtest',
-    'SSH_KEY': '/home/cltbld/.ssh/id_rsa',
-    'SIGNDEBS_BASEDIR': 'sign-debs-staging',
+BRANCHES = {
+    "mozilla-central": {},
+    "mozilla-1.9.2": {},
 }
+BRANCHES["mozilla-central"]["script_repo_tag"] = "default"
+BRANCHES["mozilla-central"]["nightly_config_file"] = "deb_repos/staging_trunk_nightly.json"
+BRANCHES["mozilla-central"]["nightly_platforms"] = ["chinook", "fremantle"]
+BRANCHES["mozilla-central"]["enable_release"] = False
 
-# Needed in staging only
-NIGHTLY_TRIGGER_ENV = {
-    'STAGE_USERNAME': 'ffxbld',
-    'STAGE_SERVER': 'stage.mozilla.org',
-    'SSH_KEY': '/home/cltbld/.ssh/ffxbld_dsa',
-}
-
-REAL_RELEASE_ENV = {
-    'STAGE_USERNAME': 'cltbld',
-    'STAGE_SERVER': 'staging-mobile-master.build.mozilla.org',
-    'BASE_STAGE_PATH': '/var/www/html',
-    'BASE_STAGE_URL': 'http://staging-mobile-master.build.mozilla.org/debtest',
-    'STAGE_DIR': '/debtest',
-    'SSH_KEY': '/home/cltbld/.ssh/id_rsa',
-    'SIGNDEBS_BASEDIR': 'sign-debs-staging',
-}
-CANDIDATES_RELEASE_ENV = {
-    'STAGE_USERNAME': 'cltbld',
-    'STAGE_SERVER': 'staging-mobile-master.build.mozilla.org',
-    'BASE_STAGE_PATH': '/var/www/html',
-    'BASE_STAGE_URL': 'http://staging-mobile-master.build.mozilla.org',
-    'STAGE_DIR': '/debtest/candidates',
-    'SSH_KEY': '/home/cltbld/.ssh/id_rsa',
-    'SIGNDEBS_BASEDIR': 'sign-debs-staging',
-}
-
-
-# Change for real release
-RELEASE_ENV = CANDIDATES_RELEASE_ENV
-
-DEBSIGN_CONFIG = {}
-TRIGGER_CONFIG = {}
-
-DEBSIGN_CONFIG['slaves'] = SLAVES['linux']
-DEBSIGN_CONFIG['tools_repo_path'] = "http://hg.mozilla.org/build/tools"
-DEBSIGN_CONFIG['branches'] = {
-    'trunk': {'nightly': {},
-              'release': {}},
-    '1.9.2': {'nightly': {},
-              'release': {}},
-}
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['base_stage_path'] = '/home/ftp'
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['base_stage_url'] = 'http://ftp.mozilla.org'
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['stage_dir'] = '/pub/mozilla.org/mobile/nightly/latest-mobile-trunk'
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['env'] = NIGHTLY_ENV
-DEBSIGN_CONFIG['branches']['trunk']['nightly']['extra_debs_list'] = None
-DEBSIGN_CONFIG['branches']['trunk']['enable_release'] = False
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['base_stage_path'] = '/home/ftp'
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['base_stage_url'] = 'http://ftp.mozilla.org'
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['stage_dir'] = '/pub/mozilla.org/mobile/nightly/latest-mobile-1.9.2'
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['env'] = NIGHTLY_ENV
-DEBSIGN_CONFIG['branches']['1.9.2']['nightly']['extra_debs_list'] = None
-DEBSIGN_CONFIG['branches']['1.9.2']['enable_release'] = True
-DEBSIGN_CONFIG['branches']['trunk']['release']['base_stage_path'] = '/var/www/html'
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['base_stage_url'] = 'http://staging-mobile-master.build.mozilla.org'
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['stage_dir'] = '/candidates/1.1rc1-candidates/build1/maemo4'
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['env'] = RELEASE_ENV
-DEBSIGN_CONFIG['branches']['1.9.2']['release']['extra_debs_list'] = None
-
-#
-# Trigger config
-#
-
-TRIGGER_CONFIG['sendchange_master'] = "staging-mobile-master.build.mozilla.org:9011"
-TRIGGER_CONFIG['branches'] = {
-    'trunk': {},
-    '1.9.2': {},
-}
-TRIGGER_CONFIG['branches']['trunk']['env'] = NIGHTLY_TRIGGER_ENV
-TRIGGER_CONFIG['branches']['1.9.2']['env'] = NIGHTLY_TRIGGER_ENV
-TRIGGER_CONFIG['branches']['1.9.2']['release_mobile_tag'] = 'default'
-TRIGGER_CONFIG['mobile_repo'] = 'http://hg.mozilla.org/releases/mobile-1.1'
-TRIGGER_CONFIG['locales_file'] = 'mobile/locales/all-locales'
+BRANCHES["mozilla-1.9.2"]["script_repo_tag"] = "default"
+BRANCHES["mozilla-1.9.2"]["nightly_config_file"] = "deb_repos/staging_1.1_nightly.json"
+BRANCHES["mozilla-1.9.2"]["nightly_platforms"] = ["chinook", "fremantle"]
+BRANCHES["mozilla-1.9.2"]["enable_release"] = True
+BRANCHES["mozilla-1.9.2"]["release_config_file"] = "deb_repos/staging_1.1_release.json"
+BRANCHES["mozilla-1.9.2"]["release_platforms"] = ["chinook", "fremantle"]
