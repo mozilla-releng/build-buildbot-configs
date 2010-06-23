47445c4dd649f7fce176625f0f51edc78b434c5d
Robert Kaiser <kairo@kairo.at>
2010-06-23 16:45:34 +0200

    update configs for SeaMonkey 2.1a2 release run

--- seamonkey/release-comm-central-trunk.py
+++ seamonkey/release-comm-central-trunk.py
@@ -3,13 +3,13 @@ hgSshKey                   = '~seabld/.ssh/seabld_dsa'
 relbranchPrefix            = 'COMM'
 sourceRepoName             = 'comm-central-trunk' # buildbot branch name
 sourceRepoPath             = 'comm-central'
-sourceRepoRevision         = '519b9b0655ac'
+sourceRepoRevision         = '73d458b52ccb'
 relbranchOverride          = ''
 mozillaRepoPath            = 'mozilla-central'
-mozillaRepoRevision        = '3a26cbe20cd2'
-mozillaRelbranchOverride   = '' # put Gecko relbranch here that we base upon
+mozillaRepoRevision        = 'f6cf82daa5bd'
+mozillaRelbranchOverride   = 'COMM193a5_20100623_RELBRANCH' # put Gecko relbranch here that we base upon
 inspectorRepoPath          = 'dom-inspector' # leave empty if inspector is not to be tagged
-inspectorRepoRevision      = '43c6839edab5'
+inspectorRepoRevision      = '03cf7e93828f'
 inspectorRelbranchOverride = ''
 venkmanRepoPath            = 'venkman' # leave empty if venkman is not to be tagged
 venkmanRepoRevision        = '05ab405b963d'
@@ -29,19 +29,18 @@ appName                    = 'suite'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '2.1a1'
+version                    = '2.1a2'
 appVersion                 = version
-milestone                  = '1.9.3a4'
+milestone                  = '1.9.3a5'
 buildNumber                = 1
-baseTag                    = 'SEAMONKEY_2_1a1'
-oldVersion                 = ''
+baseTag                    = 'SEAMONKEY_2_1a2'
+oldVersion                 = '2.1a2'
 oldAppVersion              = oldVersion
 oldBuildNumber             = 1
-oldBaseTag                 = ''
+oldBaseTag                 = 'SEAMONKEY_2_1a1'
 enUSPlatforms              = ('linux', 'linux64', 'win32', 'macosx')
 l10nPlatforms              = ()
-# TODO: create this file before 2.1a2
-patcherConfig              = 'moz193-seamonkey-branch-patcher2.cfg'
+patcherConfig              = 'moz20-seamonkey-branch-patcher2.cfg'
 patcherToolsTag            = 'UPDATE_PACKAGING_R11'
 ftpServer                  = 'ftp.mozilla.org'
 stagingServer              = 'stage-old.mozilla.org'
@@ -51,8 +50,7 @@ bouncerServer              = 'download.mozilla.org'
 ausServerUrl               = 'https://aus2-community.mozilla.org'
 releaseNotesUrl            = None
 useBetaChannel             = 1
-# TODO: create these files before first 2.1 requiring updates
-verifyConfigs              = {'linux':  'moz193-seamonkey-linux.cfg',
-                              'macosx': 'moz193-seamonkey-mac.cfg',
-                              'win32':  'moz193-seamonkey-win32.cfg'}
+verifyConfigs              = {'linux':  'moz20-seamonkey-linux.cfg',
+                              'macosx': 'moz20-seamonkey-mac.cfg',
+                              'win32':  'moz20-seamonkey-win32.cfg'}
 majorUpdateRepoPath        = None
