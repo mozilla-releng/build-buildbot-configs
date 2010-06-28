75cc08644db70ef165ee5edf83423ecad5df3fdf
Philippe M. Chiasson <gozer@mozillamessaging.com>
2010-06-28 15:51:56 -0400

    Update release config for Thunderbird 3.1 Final

--- thunderbird/release_thunderbird_3.1.py
+++ thunderbird/release_thunderbird_3.1.py
@@ -27,6 +27,7 @@ chatzillaTimestamp         = '' # leave empty if chatzilla is not to be tagged
 l10nRepoPath               = 'releases/l10n-mozilla-1.9.2'
 l10nRevisionFile           = 'l10n-thunderbird-changesets-3.1'
 toolsRepoPath              = 'build/tools'
+buildToolsRepoPath	   = ''
 cvsroot                    = ':ext:cltbld@cvs.mozilla.org:/cvsroot' # for patcher, etc.
 productVersionFile         = 'mail/config/version-192.txt'
 productName                = 'thunderbird'
@@ -38,12 +39,12 @@ ftpName                    = appName
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '3.1rc2'
+version                    = '3.1'
 appVersion                 = '3.1'
 #XXX: Not entirely certain if/where this is used.
 milestone                  = '1.9.2.4'
 buildNumber                = 1
-baseTag                    = 'THUNDERBIRD_3_1_rc2'
+baseTag                    = 'THUNDERBIRD_3_1'
 # The old version is the revision from which we should generate update snippets.
 oldVersion                 = '3.1rc1'
 oldAppVersion              = '3.1'
@@ -57,6 +58,7 @@ stagingServer              = 'stage-old.mozilla.org'
 bouncerServer              = 'download.mozilla.org'
 ausServerUrl               = 'https://aus2.mozillamessaging.com'
 useBetaChannel             = 1
+l10nPlatforms              = ['linux', 'macosx', 'win32' ]
 verifyConfigs              = {'linux':  'moz192-thunderbird-linux.cfg',
                               'macosx': 'moz192-thunderbird-mac.cfg',
                               'win32':  'moz192-thunderbird-win32.cfg'}
