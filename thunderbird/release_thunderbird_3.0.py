34270038feacbf5ebe039dbd67004153a4a97f2e
John Hopkins <john.hopkins@mozillamessaging.com>
2010-07-01 16:56:09 -0700

    Thunderbird 3.0.6. r=gozer

--- thunderbird/release_thunderbird_3.0.py
+++ thunderbird/release_thunderbird_3.0.py
@@ -3,15 +3,15 @@ hgSshKey                   = '~cltbld/.ssh/tbirdbld_dsa'
 relbranchPrefix            = 'COMM'
 sourceRepoName             = 'comm-1.9.1' # buildbot branch name
 sourceRepoPath             = 'releases/comm-1.9.1'
-sourceRepoRevision         = 'c4b1d9c9fb5f'
+sourceRepoRevision         = 'dcfa7f4510b1'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
-relbranchOverride          = 'COMM19110_20100510_RELBRANCH'
+relbranchOverride          = 'COMM19111_20100701_RELBRANCH'
 mozillaRepoPath            = 'releases/mozilla-1.9.1'
-mozillaRepoRevision        = '8440965a6347'
+mozillaRepoRevision        = '68b47271b5fe'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 # You typically want to set this to the gecko relbranch if doing a release off
 # a specific gecko version.
-mozillaRelbranchOverride   = 'COMM19110_20100506_RELBRANCH' # put Gecko relbranch here that we base upon
+mozillaRelbranchOverride   = 'COMM19111_20100701_RELBRANCH' # put Gecko relbranch here that we base upon
 inspectorRepoPath          = 'dom-inspector' # leave empty if inspector is not to be tagged
 inspectorRepoRevision      = '18a1c983c8ee'
 inspectorRelbranchOverride = 'COMM19110_20100510_RELBRANCH'
@@ -37,17 +37,18 @@ ftpName                    = appName
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '3.0.5'
+version                    = '3.0.6'
 appVersion                 = version
 #XXX: Not entirely certain if/where this is used.
-milestone                  = '1.9.1.10'
-buildNumber                = 2
-baseTag                    = 'THUNDERBIRD_3_0_5'
+# Look in release-firefox-mozilla-1.9.1.py for the milestone value
+milestone                  = '1.9.1.11'
+buildNumber                = 1
+baseTag                    = 'THUNDERBIRD_3_0_6'
 # The old version is the revision from which we should generate update snippets.
-oldVersion                 = '3.0.4'
+oldVersion                 = '3.0.5'
 oldAppVersion              = oldVersion
-oldBuildNumber             = 1
-oldBaseTag                 = 'THUNDERBIRD_3_0_4'
+oldBuildNumber             = 2
+oldBaseTag                 = 'THUNDERBIRD_3_0_5'
 releasePlatforms           = ('linux', 'win32', 'macosx')
 patcherConfig              = 'moz19-thunderbird-branch-patcher2.cfg'
 patcherToolsTag            = 'UPDATE_PACKAGING_R11'
