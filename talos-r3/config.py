3bd2b329ca1c845eb66888b979d5578b13cea1be
Lukas Blakk <lsblakk@mozilla.com>
2010-07-16 17:41:11 -0700

    Bug 571121 & Bug 572575 -- adds new talos-r3 slaves to talos/config.py p=lsblakk r=alice

--- talos-r3/config.py
+++ talos-r3/config.py
@@ -29,13 +29,13 @@ SUITES = {
 }
 
 SLAVES = {
-    'fedora': ["talos-r3-fed-%03i" % x for x in range(1,51)],
-    'fedora64' : ["talos-r3-fed64-%03i" % x for x in range (1,51)],
-    'xp': ["talos-r3-xp-%03i" % x for x in range(1,51)],
-    'win7': ["talos-r3-w7-%03i" % x for x in range(1,51)],
+    'fedora': ["talos-r3-fed-%03i" % x for x in range(1,54)],
+    'fedora64' : ["talos-r3-fed64-%03i" % x for x in range (1,56)],
+    'xp': ["talos-r3-xp-%03i" % x for x in range(1,54)],
+    'win7': ["talos-r3-w7-%03i" % x for x in range(1,54)],
     'w764': ["t-r3-w764-%03i" % x for x in range(1,51)],
-    'leopard': ["talos-r3-leopard-%03i" % x for x in range(1,51)],
-    'snowleopard': ["talos-r3-snow-%03i" % x for x in range(1,51)],
+    'leopard': ["talos-r3-leopard-%03i" % x for x in range(1,54)],
+    'snowleopard': ["talos-r3-snow-%03i" % x for x in range(1,56)],
 }
 
 BRANCHES = {
