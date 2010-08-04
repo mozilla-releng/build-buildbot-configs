6f010cd9f21346e3279583592ca78ebc86057c9a
Aki Sasaki <asasaki@mozilla.com>
2010-08-03 18:14:00 -0700

    stagger debsigning nightly times. bug 584081, r=jhford

--- debsign/config-production.py
+++ debsign/config-production.py
@@ -10,6 +10,7 @@ BRANCHES["mozilla-central"]["script_repo_tag"] = 'PRODUCTION'
 BRANCHES["mozilla-central"]["nightly_config_file"] = "deb_repos/trunk_nightly.json"
 BRANCHES["mozilla-central"]["nightly_platforms"] = ["chinook", "fremantle", "fremantle-qt"]
 BRANCHES["mozilla-central"]["enable_release"] = False
+BRANCHES["mozilla-central"]["nightly_hour"] = 4
 
 BRANCHES["mozilla-1.9.2"]["script_repo_tag"] = 'PRODUCTION'
 BRANCHES["mozilla-1.9.2"]["nightly_config_file"] = "deb_repos/1.1_nightly.json"
@@ -17,3 +18,4 @@ BRANCHES["mozilla-1.9.2"]["nightly_platforms"] = ["chinook", "fremantle"]
 BRANCHES["mozilla-1.9.2"]["enable_release"] = True
 BRANCHES["mozilla-1.9.2"]["release_config_file"] = "deb_repos/1.1_release.json"
 BRANCHES["mozilla-1.9.2"]["release_platforms"] = ["chinook", "fremantle"]
+BRANCHES["mozilla-1.9.2"]["nightly_hour"] = 4
--- debsign/config-staging.py
+++ debsign/config-staging.py
@@ -10,6 +10,7 @@ BRANCHES["mozilla-central"]["script_repo_tag"] = "default"
 BRANCHES["mozilla-central"]["nightly_config_file"] = "deb_repos/staging_trunk_nightly.json"
 BRANCHES["mozilla-central"]["nightly_platforms"] = ["chinook", "fremantle", "fremantle-qt"]
 BRANCHES["mozilla-central"]["enable_release"] = False
+BRANCHES["mozilla-central"]["nightly_hour"] = 9
 
 BRANCHES["mozilla-1.9.2"]["script_repo_tag"] = "default"
 BRANCHES["mozilla-1.9.2"]["nightly_config_file"] = "deb_repos/staging_1.1_nightly.json"
@@ -17,3 +18,4 @@ BRANCHES["mozilla-1.9.2"]["nightly_platforms"] = ["chinook", "fremantle"]
 BRANCHES["mozilla-1.9.2"]["enable_release"] = True
 BRANCHES["mozilla-1.9.2"]["release_config_file"] = "deb_repos/staging_1.1_release.json"
 BRANCHES["mozilla-1.9.2"]["release_platforms"] = ["chinook", "fremantle"]
+BRANCHES["mozilla-1.9.2"]["nightly_hour"] = 9
