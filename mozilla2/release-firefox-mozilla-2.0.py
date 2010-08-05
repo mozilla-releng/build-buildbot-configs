2ac645db0f433a857ba555bc8d7fbb06c3366e2d
Aki Sasaki <asasaki@mozilla.com>
2010-08-04 17:47:53 -0700

    4.0b3 build2 configs. bug 583967, r=nthomas

--- mozilla2/release-firefox-mozilla-2.0.py
+++ mozilla2/release-firefox-mozilla-2.0.py
@@ -2,8 +2,8 @@ hgUsername         = 'ffxbld'
 hgSshKey           = '~cltbld/.ssh/ffxbld_dsa'
 sourceRepoName     = 'mozilla-central'
 sourceRepoPath     = sourceRepoName
-sourceRepoRevision = '2f187db8f5f6'
-relbranchOverride  = ''
+sourceRepoRevision = '69f2d0457750'
+relbranchOverride  = 'GECKO20b3_20100804_RELBRANCH'
 l10nRepoPath       = 'l10n-central'
 l10nRevisionFile   = 'l10n-changesets_mozilla-2.0'
 # mergeLocales allows missing localized strings to be filled in by their en-US
@@ -23,7 +23,7 @@ version            = '4.0b3'
 appVersion         = version
 # NB: this will become 2.0.x not 2.0.0.x, bug 577875
 milestone          = '2.0b3'
-buildNumber        = 1
+buildNumber        = 2
 baseTag            = 'FIREFOX_4_0b3'
 oldVersion         = '4.0b2'
 oldAppVersion      = oldVersion
