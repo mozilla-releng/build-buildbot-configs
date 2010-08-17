1937a2a26a7a14ce5f51e2c8befd409ca72354a1
Lukas Blakk <lsblakk@mozilla.com>
2010-08-17 13:13:00 -0700

    Bug 588045 - configs for Firefox 4.0b4 build2 p=lsblakk r=bhearsum

--- mozilla2/release-firefox-mozilla-2.0.py
+++ mozilla2/release-firefox-mozilla-2.0.py
@@ -2,8 +2,8 @@ hgUsername         = 'ffxbld'
 hgSshKey           = '~cltbld/.ssh/ffxbld_dsa'
 sourceRepoName     = 'mozilla-central'
 sourceRepoPath     = sourceRepoName
-sourceRepoRevision = 'b51b190b9fcc'
-relbranchOverride  = ''
+sourceRepoRevision = '9aa39b619a19'
+relbranchOverride  = 'GECKO20b4_20100817_RELBRANCH'
 l10nRepoPath       = 'l10n-central'
 l10nRevisionFile   = 'l10n-changesets_mozilla-2.0'
 # mergeLocales allows missing localized strings to be filled in by their en-US
@@ -23,7 +23,7 @@ version            = '4.0b4'
 appVersion         = version
 # NB: this will become 2.0.x not 2.0.0.x, bug 577875
 milestone          = '2.0b4'
-buildNumber        = 1
+buildNumber        = 2
 baseTag            = 'FIREFOX_4_0b4'
 oldVersion         = '4.0b3'
 oldAppVersion      = oldVersion
