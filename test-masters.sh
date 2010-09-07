ff2a02c1c8e494d0e912091769bb3ff0be864da7
Rail Aliev <rail@mozilla.com>
2010-09-07 09:16:51 +0300

    Bug 593797: test-masters.sh should exit non zero on errors. r=nthomas

--- test-masters.sh
+++ test-masters.sh
@@ -4,11 +4,16 @@ master_dir=master_dir
 if [ "$1" = "-8" ]; then
     extra_args=-8
 fi
+
+exit_code=0
+
 for master_name in $(python setup-master.py $extra_args -l); do
     rm -rf $master_dir
     mkdir $master_dir
     echo -n "${master_name}... "
     python setup-master.py $extra_args $master_dir $master_name
-    (cd $master_dir; buildbot checkconfig > /dev/null && echo OK || (echo "Broken pieces are in $master_dir"; false))
+    (cd $master_dir; buildbot checkconfig > /dev/null && echo OK) || { echo "Broken pieces are in $master_dir"; exit_code=1; }
     rm -rf $master_dir
 done
+
+exit $exit_code
