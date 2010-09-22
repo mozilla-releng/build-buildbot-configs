714ba174a40aac1112e039cd9ac07b550c5aefcc
Philippe M. Chiasson <gozer@mozillamessaging.com>
2010-09-22 15:52:10 -0400

    increase build space (libxul)

--- thunderbird/config.py
+++ thunderbird/config.py
@@ -414,7 +414,7 @@ DEFAULTS = {
     'product_name':           'Thunderbird',
     'brand_name':             'Shredder',
     'app_name':			'thunderbird',
-    'build_space':            8,
+    'build_space':             9,
     'l10n_nightly_updates':    False,
 
     'create_partial':          False,
