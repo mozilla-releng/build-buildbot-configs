abc6567c542d6887ebdb024a3b8431be5755b476
Nick Thomas <nthomas@mozilla.com>
2010-09-27 15:45:09 +1300

    Bug 571367, turn production x86_64 builds into i386/x86_64 universal binaries (run macosx64 platform where leopard slaves are), r=bhearsum

--- mozilla-tests/production_tests_master_tm02_localconfig.py
+++ mozilla-tests/production_tests_master_tm02_localconfig.py
@@ -14,4 +14,6 @@ c['manhole'] = manhole.PasswordManhole("tcp:1235:interface=127.0.0.1", "cltbld",
 from config import BRANCHES
 # Do everything!
 ACTIVE_BRANCHES = BRANCHES.keys()
-ACTIVE_PLATFORMS = {'macosx': None, 'linux': None}
+# macosx64 is here so that the leopard slaves test the i386+x86_64 universal
+# binary, opt unit and talos
+ACTIVE_PLATFORMS = {'macosx': None, 'linux': None, 'macosx64': None}
