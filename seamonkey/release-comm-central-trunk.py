bf350099a0c6bc9b612c966393dafda217a4078a
Robert Kaiser <kairo@kairo.at>
2010-10-07 20:41:01 +0200

    version and config changes for spinning SeaMonkey 2.1 Beta 1

--- seamonkey/release-comm-central-trunk.py
+++ seamonkey/release-comm-central-trunk.py
@@ -3,29 +3,29 @@ hgSshKey                   = '~seabld/.ssh/seabld_dsa'
 relbranchPrefix            = 'COMM'
 sourceRepoName             = 'comm-central-trunk' # buildbot branch name
 sourceRepoPath             = 'comm-central'
-sourceRepoRevision         = 'd24027998497'
-relbranchOverride          = 'COMM20b4_20100815_RELBRANCH'
+sourceRepoRevision         = '8d9a2e529de2'
+relbranchOverride          = ''
 mozillaRepoPath            = 'mozilla-central'
-mozillaRepoRevision        = '5e9c05dbe8d6'
-mozillaRelbranchOverride   = 'COMM20b4_20100815_RELBRANCH' # put Gecko relbranch here that we base upon
+mozillaRepoRevision        = '329afdc371fb'
+mozillaRelbranchOverride   = 'GECKO20b7pre_20101006_RELBRANCH' # put Gecko relbranch here that we base upon
 inspectorRepoPath          = 'dom-inspector' # leave empty if inspector is not to be tagged
-inspectorRepoRevision      = 'eef568e4ee05'
-inspectorRelbranchOverride = 'COMM20b4_20100815_RELBRANCH'
+inspectorRepoRevision      = '38a67e4ecf17'
+inspectorRelbranchOverride = ''
 venkmanRepoPath            = 'venkman' # leave empty if venkman is not to be tagged
-venkmanRepoRevision        = '5d2f0995baff'
-venkmanRelbranchOverride   = 'COMM20b4_20100815_RELBRANCH'
+venkmanRepoRevision        = '662801d486aa'
+venkmanRelbranchOverride   = ''
 chatzillaRepoPath          = 'chatzilla' # leave empty if chatzilla is not to be tagged
 chatzillaRepoRevision      = '2c5f8e558d1d'
-chatzillaRelbranchOverride = 'COMM20b4_20100815_RELBRANCH'
+chatzillaRelbranchOverride = ''
 l10nRepoPath               = 'l10n-central'
-l10nRevisionFile           = 'l10n-changesets'
+l10nRevisionFile           = 'l10n-changesets_comm_central_trunk'
 cvsroot                    = ':ext:seabld@cvs.mozilla.org:/cvsroot' # for patcher, etc.
 productVersionFile         = 'suite/config/version.txt'
 # mergeLocales allows missing localized strings to be filled in by their en-US
 # equivalent string. This is on (True) by default for nightly builds, but
 # should be False for releases *EXCEPT* alphas and early betas. If in doubt,
 # ask release-drivers.
-mergeLocales               = False
+mergeLocales               = True
 productName                = 'seamonkey'
 brandName                  = 'SeaMonkey'
 appName                    = 'suite'
@@ -34,15 +34,15 @@ appName                    = 'suite'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '2.1a3'
+version                    = '2.1b1'
 appVersion                 = version
-milestone                  = '2.0b4'
-buildNumber                = 2
-baseTag                    = 'SEAMONKEY_2_1a3'
-oldVersion                 = '2.1a2'
+milestone                  = '2.0b7'
+buildNumber                = 1
+baseTag                    = 'SEAMONKEY_2_1b1'
+oldVersion                 = '2.1a3'
 oldAppVersion              = oldVersion
 oldBuildNumber             = 2
-oldBaseTag                 = 'SEAMONKEY_2_1a2'
+oldBaseTag                 = 'SEAMONKEY_2_1a3'
 enUSPlatforms              = ('linux', 'linux64', 'win32', 'macosx')
 l10nPlatforms              = ()
 patcherConfig              = 'moz20-seamonkey-branch-patcher2.cfg'
