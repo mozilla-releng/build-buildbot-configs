8894c239c19c1c7e9003efbe0518f650960534b3
Aki Sasaki <asasaki@mozilla.com>
2010-11-08 15:33:31 -0800

    set c['changeHorizon'] = None. bug 610025, p=dustin, r=catlee

--- mozilla/master_common.py
+++ mozilla/master_common.py
@@ -2,7 +2,7 @@ c = BuildmasterConfig = {}
 c['projectName'] = "Firefox"
 c['projectURL'] = "http://wiki.mozilla.org/Firefox"
 c['logMaxSize'] = 50 * 1024 * 1024
-c['changeHorizon'] = 100
+c['changeHorizon'] = None
 c['logCompressionMethod'] = 'gz'
 c['eventHorizon'] = 20
 c['buildCacheSize'] = 10
