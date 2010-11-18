612db9fb02ea99f46d6449d3c747dd87789742ad
Aki Sasaki <asasaki@mozilla.com>
2010-11-17 18:48:40 -0800

    move mozharness from my user repo to build/mozharness for debsign masters. bug 574473, r=lsblakk

--- debsign/config-production.py
+++ debsign/config-production.py
@@ -6,7 +6,7 @@ BRANCHES = {
     "mozilla-central": {},
     "mozilla-1.9.2": {},
 }
-BRANCHES["mozilla-central"]["script_repo_tag"] = 'PRODUCTION'
+BRANCHES["mozilla-central"]["script_repo_tag"] = 'default'
 BRANCHES["mozilla-central"]["nightly_config_file"] = "deb_repos/trunk_nightly.json"
 BRANCHES["mozilla-central"]["nightly_platforms"] = ["fremantle", "fremantle-qt"]
 BRANCHES["mozilla-central"]["enable_release"] = True
@@ -14,7 +14,7 @@ BRANCHES["mozilla-central"]["release_config_file"] = "deb_repos/4.0_release.json
 BRANCHES["mozilla-central"]["release_platforms"] = ["fremantle"]
 BRANCHES["mozilla-central"]["nightly_hour"] = 4
 
-BRANCHES["mozilla-1.9.2"]["script_repo_tag"] = 'PRODUCTION'
+BRANCHES["mozilla-1.9.2"]["script_repo_tag"] = 'default'
 BRANCHES["mozilla-1.9.2"]["nightly_config_file"] = "deb_repos/1.1_nightly.json"
 BRANCHES["mozilla-1.9.2"]["nightly_platforms"] = ["chinook", "fremantle"]
 BRANCHES["mozilla-1.9.2"]["enable_release"] = True
