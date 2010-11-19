85997e65ba3f0918d7a4c5ec7e9e9a7ab01d91e7
Robert Kaiser <kairo@kairo.at>
2010-11-19 14:08:40 +0100

    make machine names match reality

--- seamonkey/config.py
+++ seamonkey/config.py
@@ -13,8 +13,7 @@ SLAVES = {
              ['cn-sea-qm-win2k3-%02i' % x for x in [1]] +
              ['cb-sea-win32-tbox'],
     'macosx': ['cb-sea-miniosx%02i' % x for x in [1,2]],
-    'macosx64': ['cb-sea-miniosx%02i' % x for x in [3,4,5]] +
-                ['cb-sea-miniosx64-%02i' % x for x in [1,2,3]],
+    'macosx64': ['cb-sea-miniosx64-%02i' % x for x in [1,2,3]],
 }
 
 
