f8c3dbeef141af59c11e951a4cb7726bdbb908bc
Aki Sasaki <asasaki@mozilla.com>
2010-12-14 16:09:13 -0800

    4.0b3 configs. bug 615696, r=lsblakk

--- mozilla2/release-fennec-2.0.py
+++ mozilla2/release-fennec-2.0.py
@@ -6,13 +6,13 @@ mozSourceRepoName      = 'mozilla-central'
 # because the repo_setup builder needs to know where to clone repositories from.
 # It is not used for anything else.
 mozSourceRepoPath      = 'mozilla-central'
-mozSourceRepoRevision  = 'ec7521865660'
+mozSourceRepoRevision  = 'abe884259481'
 mobileSourceRepoName      = 'mobile-browser'
 mobileSourceRepoPath      = 'mobile-browser'
-mobileSourceRepoRevision  = 'a73da861034a'
-mozRelbranchOverride      = 'GECKO20b7pre_20101029_RELBRANCH'
-l10nRelbranchOverride     = 'GECKO20b7pre_20101029_RELBRANCH'
-mobileRelbranchOverride   = 'GECKO20b7pre_20101029_RELBRANCH'
+mobileSourceRepoRevision  = 'dabce289f397'
+mozRelbranchOverride      = 'GECKO20b8_20101214_RELBRANCH'
+l10nRelbranchOverride     = 'GECKO20b8_20101214_RELBRANCH'
+mobileRelbranchOverride   = ''
 l10nRepoPath        = 'l10n-central'
 l10nRevisionFile    = 'l10n-changesets_mobile-2.0.json'
 productName         = 'fennec'
@@ -24,15 +24,15 @@ enableMultiLocale   = True
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version             = '4.0b2'
+version             = '4.0b3'
 appVersion          = version
-milestone           = '2.0b7pre'
-buildNumber         = 4
-baseTag             = 'FENNEC_4_0b2'
-oldVersion          = '4.0b1'
+milestone           = '2.0b8'
+buildNumber         = 1
+baseTag             = 'FENNEC_4_0b3'
+oldVersion          = '4.0b2'
 oldAppVersion       = oldVersion
-oldBuildNumber      = 3
-oldBaseTag          = 'FENNEC_4_0b1'
+oldBuildNumber      = 4
+oldBaseTag          = 'FENNEC_4_0b2'
 enUSPlatforms       = ('maemo5-gtk', 'android-r7')
 l10nPlatforms       = ('maemo5-gtk',)
 enUSDesktopPlatforms = ('linux-i686', 'macosx-i686', 'win32-i686')
