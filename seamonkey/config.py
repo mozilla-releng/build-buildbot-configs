56f71d875d967a7d19a26512e1b1dd1d65c8cf6c
Justin Wood <Callek@gmail.com>
2011-02-04 08:21:29 -0800

    Bug 631386, Drop cb-sea-macosx01 while the machine is down; it was causing reconfig to never complete.

--- seamonkey/config.py
+++ seamonkey/config.py
@@ -8,7 +8,7 @@ SLAVES = {
     'win32': ['cb-seamonkey-win32-%02i' % x for x in [1,2,3]] +
              ['cn-sea-qm-win2k3-%02i' % x for x in [1]] +
              ['cb-sea-win32-tbox'],
-    'macosx': ['cb-sea-miniosx%02i' % x for x in [1,2]],
+    'macosx': ['cb-sea-miniosx%02i' % x for x in [2,]],
     'macosx64': ['cb-sea-miniosx64-%02i' % x for x in [1,2,3]],
 }
 
