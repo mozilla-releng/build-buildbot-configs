8980253f2ab0465bdc13854e046236a291c38aea
Nick Thomas <nthomas@mozilla.com>
2011-02-23 16:15:09 +1300

    Bug 631731, release config for Firefox 4.0b12 build1, r=rail + final changesets

--- mozilla/staging_release-firefox-mozilla-2.0.py
+++ mozilla/staging_release-firefox-mozilla-2.0.py
@@ -5,8 +5,8 @@ releaseConfig['hgSshKey']            = '~cltbld/.ssh/ffxbld_dsa'
 releaseConfig['sourceRepoName']      = 'mozilla-central'
 releaseConfig['sourceRepoClonePath'] = releaseConfig['sourceRepoName']
 releaseConfig['sourceRepoPath']      = 'users/stage-ffxbld/mozilla-central'
-releaseConfig['sourceRepoRevision']  = '633e895d5e84'
-releaseConfig['relbranchOverride']   = 'GECKO20b5_20100831_RELBRANCH'
+releaseConfig['sourceRepoRevision']  = '51702867d932'
+releaseConfig['relbranchOverride']   = ''
 releaseConfig['l10nRepoClonePath']   = 'l10n-central'
 releaseConfig['l10nRepoPath']        = 'users/stage-ffxbld'
 releaseConfig['l10nRevisionFile']    = 'l10n-changesets_mozilla-2.0'
@@ -24,22 +24,22 @@ releaseConfig['appName']             = 'browser'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-releaseConfig['version']             = '4.0b6'
+releaseConfig['version']             = '4.0b12'
 releaseConfig['appVersion']          = releaseConfig['version']
-releaseConfig['milestone']           = '2.0b6'
-releaseConfig['buildNumber']         = 2
-releaseConfig['baseTag']             = 'FIREFOX_4_0b6'
-releaseConfig['oldVersion']          = '4.0b5'
+releaseConfig['milestone']           = '2.0b12'
+releaseConfig['buildNumber']         = 1
+releaseConfig['baseTag']             = 'FIREFOX_4_0b12'
+releaseConfig['oldVersion']          = '4.0b11'
 releaseConfig['oldAppVersion']       = releaseConfig['oldVersion']
-releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'FIREFOX_4_0b5'
+releaseConfig['oldBuildNumber']      = 3
+releaseConfig['oldBaseTag']          = 'FIREFOX_4_0b11'
 releaseConfig['enUSPlatforms']       = ('linux', 'linux64', 'win32', 'macosx64')
 releaseConfig['l10nPlatforms']       = releaseConfig['enUSPlatforms']
 releaseConfig['talosTestPlatforms']  = releaseConfig['enUSPlatforms']
 releaseConfig['unittestPlatforms']   = releaseConfig['enUSPlatforms']
-releaseConfig['xulrunnerPlatforms']  = ()
+releaseConfig['xulrunnerPlatforms']  = releaseConfig['enUSPlatforms']
 releaseConfig['patcherConfig']       = 'moz20-branch-patcher2.cfg'
-releaseConfig['patcherToolsTag']     = 'UPDATE_PACKAGING_R12'
+releaseConfig['patcherToolsTag']     = 'UPDATE_PACKAGING_R13'
 releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 releaseConfig['ftpServer']           = 'ftp.mozilla.org'
@@ -48,7 +48,7 @@ releaseConfig['bouncerServer']       = 'download.mozilla.org'
 releaseConfig['ausServerUrl']        = 'http://staging-stage.build.mozilla.org'
 releaseConfig['ausUser']             = 'cltbld'
 releaseConfig['ausSshKey']           = 'cltbld_dsa'
-releaseConfig['releaseNotesUrl']     = 'http://www.mozilla.com/%locale%/firefox/4.0b6/releasenotes/'
+releaseConfig['releaseNotesUrl']     = None
 releaseConfig['testOlderPartials']   = False
 releaseConfig['useBetaChannel']      = 0
 releaseConfig['verifyConfigs']       = {
@@ -58,7 +58,7 @@ releaseConfig['verifyConfigs']       = {
     'win32':  'moz20-firefox-win32.cfg'
 }
 releaseConfig['doPartnerRepacks']    = False
-releaseConfig['partnersRepoPath']    = 'build/partner-repacks'
+releaseConfig['partnersRepoPath']    = 'users/stage-ffxbld/partner-repacks'
 releaseConfig['majorUpdateRepoPath'] = None
 # Tuxedo/Bouncer related
 releaseConfig['tuxedoConfig']        = 'firefox-tuxedo.ini'
@@ -67,4 +67,3 @@ releaseConfig['tuxedoServerUrl']     = 'https://tuxedo.stage.mozilla.com/api/'
 releaseConfig['AllRecipients']       = ['release@mozilla.com',]
 releaseConfig['PassRecipients']      = ['release@mozilla.com',]
 releaseConfig['releaseTemplates']    = 'release_templates'
-
