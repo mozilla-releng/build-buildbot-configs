da651e89b837a9e6158d260779fb4d8d601f3697
Ben Hearsum <bhearsum@mozilla.com>
2011-03-01 12:24:38 -0500

    bug 605176: change factories and buildbot-config to support unittests on Android - switch to only 2 reftest chunks. r=aki

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -333,26 +333,14 @@ PLATFORM_UNITTEST_VARS = {
                     )),
                     ('reftest-1', (
                         {'suite': 'reftest-sanity',
-                         'totalChunks': 4,
+                         'totalChunks': 2,
                          'thisChunk': 1,
                         },
                     )),
                     ('reftest-2', (
                         {'suite': 'reftest-sanity',
-                         'totalChunks': 4,
+                         'totalChunks': 2,
                          'thisChunk': 2,
-                        },
-                    )),
-                    ('reftest-3', (
-                        {'suite': 'reftest-sanity',
-                         'totalChunks': 4,
-                         'thisChunk': 3,
-                        },
-                    )),
-                    ('reftest-4', (
-                        {'suite': 'reftest-sanity',
-                         'totalChunks': 4,
-                         'thisChunk': 4,
                         },
                     )),
                     ('crashtest', (
