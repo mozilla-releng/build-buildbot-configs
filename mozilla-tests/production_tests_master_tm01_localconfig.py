a69ced486ba04c1a744f57a9c6e5f458e828a6f2
Lukas Blakk <lsblakk@mozilla.com>
2011-03-09 14:20:43 -0800

    Bug 572064 creates a new project in mozilla-tests for polling jetpack repo & running jetpack test on checkin p=lsblakk r=catlee

--- mozilla-tests/production_tests_master_tm01_localconfig.py
+++ mozilla-tests/production_tests_master_tm01_localconfig.py
@@ -11,7 +11,8 @@ c['buildbotURL'] = 'http://test-master01.build.mozilla.org:8012/'
 from buildbot import manhole
 c['manhole'] = manhole.PasswordManhole("tcp:1235:interface=127.0.0.1", "cltbld", "password")
 
-from config import BRANCHES
+from config import BRANCHES, PROJECTS
 # Do everything!
 ACTIVE_BRANCHES = BRANCHES.keys()
 ACTIVE_PLATFORMS = {'linux64': None, 'win64': None, 'macosx64': None, 'android': None}
+ACTIVE_PROJECTS = PROJECTS.keys()
