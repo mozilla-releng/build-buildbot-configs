d86d5efcbe94980c277dcdaa92b091f41eb284ab
Ben Hearsum <bhearsum@mozilla.com>
2011-03-17 09:53:05 -0400

    bug 607179: create new buildbot-master{4,5,6}.build.m.o - add configs files for new masters, rename tests_master3 config. r=catlee
    --HG--
    extra : rebase_source : d704b42493f44af62f45964d97ba9905a32bb703

--- mozilla-tests/production_tests_master_bm05_localconfig.py
+++ mozilla-tests/production_tests_master_bm05_localconfig.py
@@ -6,13 +6,12 @@ c['status'] = [
         WebStatus(http_port=8011, allowForce=True)
 ]
 
-c['buildbotURL'] = 'http://buildbot-master1.build.scl1.mozilla.com:8011/'
+c['buildbotURL'] = 'http://buildbot-master05.build.mozilla.org:8011/'
 
 from buildbot import manhole
 c['manhole'] = manhole.PasswordManhole("tcp:1236:interface=127.0.0.1", "cltbld", "password")
 
-from config import BRANCHES, PLATFORMS, PROJECTS
+from config import BRANCHES, PLATFORMS
 # Do everything!
 ACTIVE_BRANCHES = BRANCHES.keys()
 ACTIVE_PLATFORMS = dict((k,None) for k in PLATFORMS.keys())
-ACTIVE_PROJECTS = PROJECTS.keys()
---
+++ mozilla/production_try_builder_master_bm06_localconfig.py
@@ -0,0 +1,32 @@
+c = BuildmasterConfig = {}
+c['slavePortnum'] = 9012
+
+from buildbot.status.html import WebStatus
+c['status'] = [
+        WebStatus(http_port=8012, allowForce=True)
+]
+
+c['buildbotURL'] = 'http://buildbot-master06.build.mozilla.org:8012/'
+
+from buildbot import manhole
+c['manhole'] = manhole.PasswordManhole("tcp:1238:interface=127.0.0.1", "cltbld", "password")
+
+from config import BRANCHES, TRY_SLAVES, PROJECTS
+ACTIVE_BRANCHES = ['tryserver']
+ACTIVE_RELEASE_BRANCHES = []
+# Override with TRY_SLAVES
+SLAVES = TRY_SLAVES
+
+# Don't do any projects
+ACTIVE_PROJECTS = []
+
+# Set up our fast slaves
+# No need to reload, this is reloaded by builder_master.cfg
+import buildbotcustom.misc
+buildbotcustom.misc.fastRegexes.extend([
+    '-ix-',
+    'xserve',
+    ])
+ENABLE_RELEASES = False
+STAGING = False
+RESERVED_SLAVES = None
