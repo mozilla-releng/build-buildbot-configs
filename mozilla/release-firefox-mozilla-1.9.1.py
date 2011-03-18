a51d2ec0d1b465ec2c774b368543a192acda7c65
John Ford <jhford@mozilla.com>
2011-03-18 11:56:55 -0700

    bug 642234 - MU configs for 3.5/3.6 to 4.0 r=bhearsum

--- mozilla/release-firefox-mozilla-1.9.1.py
+++ mozilla/release-firefox-mozilla-1.9.1.py
@@ -99,10 +99,10 @@ releaseConfig['partnersRepoPath']    = 'build/partner-repacks'
 
 # Major update configuration
 releaseConfig['majorUpdateRepoPath'] = 'releases/mozilla-2.0'
-releaseConfig['majorUpdateToVersion']   = '4.0rc1'
+releaseConfig['majorUpdateToVersion']   = '4.0rc2'
 releaseConfig['majorUpdateAppVersion']  = '4.0'
-releaseConfig['majorUpdateBuildNumber'] = 1
-releaseConfig['majorUpdateBaseTag']     = 'FIREFOX_4_0rc1'
+releaseConfig['majorUpdateBuildNumber'] = 3
+releaseConfig['majorUpdateBaseTag']     = 'FIREFOX_4_0rc2'
 releaseConfig['majorUpdateReleaseNotesUrl']  = 'http://www.mozilla.com/%locale%/firefox/4.0/details/'
 releaseConfig['majorUpdatePatcherConfig']    = 'moz191-branch-major-update-patcher2.cfg'
 releaseConfig['majorPatcherToolsTag']        = 'UPDATE_PACKAGING_R11_1_MU'
