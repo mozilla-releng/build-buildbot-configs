f51bb0150a160ba986c7fa031786299afb27aa4e
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2011-04-15 14:50:15 -0400

    Bug 649516. Fix nextAppVersion for 3.6 releases. r=catlee (default)

--- mozilla/release-firefox-mozilla-1.9.2.py
+++ mozilla/release-firefox-mozilla-1.9.2.py
@@ -25,8 +25,8 @@ releaseConfig['oldAppVersion']       = releaseConfig['oldVersion']
 releaseConfig['oldBuildNumber']      = 1
 releaseConfig['oldBaseTag']          = 'FIREFOX_3_6_16'
 #  Next (nightly) version info
-releaseConfig['nextAppVersion']      = '3.6.17pre'
-releaseConfig['nextMilestone']       = '1.9.2.17pre'
+releaseConfig['nextAppVersion']      = '3.6.18pre'
+releaseConfig['nextMilestone']       = '1.9.2.18pre'
 #  Repository configuration, for tagging
 releaseConfig['sourceRepositories']  = {
     'mozilla': {
