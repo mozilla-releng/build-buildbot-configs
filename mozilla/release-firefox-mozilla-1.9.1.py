1ff18afe7651fddff93aad01163579ce87cb6458
Rail Aliev <rail@mozilla.com>
2011-04-20 13:07:21 +0300

    Merge from production

--- mozilla/release-firefox-mozilla-1.9.1.py
+++ mozilla/release-firefox-mozilla-1.9.1.py
@@ -14,26 +14,26 @@ releaseConfig['appName']             = 'browser'
 releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 #  Current version info
-releaseConfig['version']             = '3.5.18'
+releaseConfig['version']             = '3.5.19'
 releaseConfig['appVersion']          = releaseConfig['version']
-releaseConfig['milestone']           = '1.9.1.18'
+releaseConfig['milestone']           = '1.9.1.19'
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_3_5_18'
+releaseConfig['baseTag']             = 'FIREFOX_3_5_19'
 #  Old version info
-releaseConfig['oldVersion']          = '3.5.17'
+releaseConfig['oldVersion']          = '3.5.18'
 releaseConfig['oldAppVersion']       = releaseConfig['oldVersion']
 releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'FIREFOX_3_5_17'
+releaseConfig['oldBaseTag']          = 'FIREFOX_3_5_18'
 #  Next (nightly) version info
-releaseConfig['nextAppVersion']      = '3.5.19pre'
-releaseConfig['nextMilestone']       = '1.9.1.19pre'
+releaseConfig['nextAppVersion']      = '3.5.20pre'
+releaseConfig['nextMilestone']       = '1.9.1.20pre'
 #  Repository configuration, for tagging
 releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-1.9.1',
         'path': 'releases/mozilla-1.9.1',
-        'revision': '8da078c0caf8',
-        'relbranch': 'GECKO19117_2011012114_RELBRANCH',
+        'revision': '1c37949defbe',
+        'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
                 'version': releaseConfig['appVersion'],
@@ -102,10 +102,10 @@ releaseConfig['partnersRepoPath']    = 'build/partner-repacks'
 
 # Major update configuration
 releaseConfig['majorUpdateRepoPath'] = 'releases/mozilla-2.0'
-releaseConfig['majorUpdateToVersion']   = '4.0rc2'
-releaseConfig['majorUpdateAppVersion']  = '4.0'
-releaseConfig['majorUpdateBuildNumber'] = 3
-releaseConfig['majorUpdateBaseTag']     = 'FIREFOX_4_0rc2'
+releaseConfig['majorUpdateToVersion']   = '4.0.1'
+releaseConfig['majorUpdateAppVersion']  = releaseConfig['majorUpdateToVersion']
+releaseConfig['majorUpdateBuildNumber'] = 1
+releaseConfig['majorUpdateBaseTag']     = 'FIREFOX_4_0_1'
 releaseConfig['majorUpdateReleaseNotesUrl']  = 'https://www.mozilla.com/%locale%/firefox/4.0/details/'
 releaseConfig['majorUpdatePatcherConfig']    = 'moz191-branch-major-update-patcher2.cfg'
 releaseConfig['majorPatcherToolsTag']        = 'UPDATE_PACKAGING_R11_1_MU'
