3be880e0b04983d98587c2fe6d032eb3e2530c93
Lukas Blakk <lsblakk@mozilla.com>
2011-04-27 12:56:47 -0700

    Bug 652641 5.0b1 configs p=lsblakk r=catlee
    --HG--
    rename : mozilla/l10n-changesets_mozilla-central => mozilla/l10n-changesets_mozilla-beta
    rename : mozilla/release-firefox-mozilla-central.py => mozilla/release-firefox-mozilla-beta.py

--- mozilla/production_builder_master_pm03_localconfig.py
+++ mozilla/production_builder_master_pm03_localconfig.py
@@ -16,7 +16,7 @@ ACTIVE_BRANCHES = ['shadow-central', 'mozilla-1.9.1', 'mozilla-1.9.2', 'mozilla-
         'mozilla-2.0', 'mozilla-2.1', 'mozilla-beta', 'mozilla-aurora',
 ] + ACTIVE_PROJECT_BRANCHES
 ACTIVE_PROJECTS = PROJECTS.keys()
-ACTIVE_RELEASE_BRANCHES = ['mozilla-1.9.2', ]
+ACTIVE_RELEASE_BRANCHES = ['mozilla-1.9.2', 'mozilla-beta']
 
 # Set up our fast slaves
 # No need to reload, this is reloaded by builder_master.cfg
