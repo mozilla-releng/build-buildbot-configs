82dcb7a31e48b4c8cf44974c04d43abb7cb6d5dd
Mark Banner <bugzilla@standard8.plus.com>
2011-05-18 01:00:13 +0100

    Switch Thunderbird's Miramar builders to pull from the l10n-miramar repos rather than the aurora ones.

--- thunderbird/config.py
+++ thunderbird/config.py
@@ -294,7 +294,7 @@ def makeBuildConfig(builderType, branchName, mozillaRepo, mozillaCentralBranch):
             bc['l10n_repo'] = 'releases/l10n-mozilla-1.9.2'
         else:
             bc['l10n'] = True
-            bc['l10n_repo'] = 'releases/l10n/mozilla-aurora'
+            bc['l10n_repo'] = 'releases/l10n-miramar'
         bc['l10n_nightly_updates'] = True
         if branchName == 'comm-1.9.2':
             bc['l10n_tree'] = 'tb31x'
