f45b4434bd9307fbbb3d26bfa9a89c50a9ff8854
John Hopkins <john.hopkins@mozillamessaging.com>
2011-06-13 09:19:33 -0700

    Thunderbird 3.1.11 build 1

--- thunderbird/release_master.py
+++ thunderbird/release_master.py
@@ -38,15 +38,15 @@ gloConfig = {
         'relbranchPrefix'            : 'COMM',
         'sourceRepoName'             : 'comm-1.9.2', # buildbot branch name
         'sourceRepoPath'             : 'releases/comm-1.9.2',
-        'sourceRepoRevision'         : '8f80fb4514a0',
+        'sourceRepoRevision'         : 'cc2222a35d1b',
         # 'If' blank, automation will create its own branch based on COMM_<date>_RELBRANCH
         'relbranchOverride'          : '',
         'mozillaRepoPath'            : 'releases/mozilla-1.9.2',
-        'mozillaRepoRevision'        : 'dd0bc929f34c',
+        'mozillaRepoRevision'        : 'd020c6aeb86c',
         # 'If' blank, automation will create its own branch based on COMM_<date>_RELBRANCH
         # 'You' typically want to set this to the gecko relbranch if doing a release off
         # 'a' specific gecko version.
-        'mozillaRelbranchOverride'   : 'GECKO19217_2011041406_RELBRANCH', # put Gecko relbranch here that we base upon
+        'mozillaRelbranchOverride'   : 'GECKO19218_2011061302_RELBRANCH', # put Gecko relbranch here that we base upon
         'inspectorRepoPath'          : 'dom-inspector', # leave empty if inspector is not to be tagged
         'inspectorRepoRevision'      : 'c1b38e365772',
         'inspectorRelbranchOverride' : 'COMM1929_20100910_RELBRANCH',
@@ -75,18 +75,18 @@ gloConfig = {
         # 'appVersion' and oldAppVersion are optional definitions used in places that
         # 'don''t care about what we call it. Eg, when version bumping we will bump to
         # 'appVersion', not version.
-        'version'                    : '3.1.10',
+        'version'                    : '3.1.11',
         #'appVersion'                 : version,
         #XXX: 'Not' entirely certain if/where this is used.
         # 'Derived' from mozillaRelbranchOverride. eg: COMM19211_20101004_RELBRANCH == 1.9.2.11
         'milestone'                  : '1.9.2.17',
         'buildNumber'                : 1,
-        'baseTag'                    : 'THUNDERBIRD_3_1_10',
+        'baseTag'                    : 'THUNDERBIRD_3_1_11',
         # 'The' old version is the revision from which we should generate update snippets.
-        'oldVersion'                 : '3.1.9',
+        'oldVersion'                 : '3.1.10',
         #'oldAppVersion'              : oldVersion,
         'oldBuildNumber'             : 1,
-        'oldBaseTag'                 : 'THUNDERBIRD_3_1_9',
+        'oldBaseTag'                 : 'THUNDERBIRD_3_1_10',
         'oldBinaryName'              : 'thunderbird',
         'enable_weekly_bundle'       : False,
         'enUSPlatforms'              : ('linux', 'win32', 'macosx'),
