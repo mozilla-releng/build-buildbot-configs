9462a508be54e516da7e6f30c23d4ceb07424e15
John Hopkins <john.hopkins@mozillamessaging.com>
2011-06-14 00:12:51 -0700

    Set patcherToolsTag to UPDATE_PACKAGING_R11_1 for TB 3.1.11.

--- thunderbird/release_master.py
+++ thunderbird/release_master.py
@@ -93,7 +93,7 @@ gloConfig = {
         'unittestPlatforms'          : (),
         'xulrunnerPlatforms'         : (),
         'patcherConfig'              : 'moz192-thunderbird-branch-patcher2.cfg',
-        'patcherToolsTag'            : 'UPDATE_PACKAGING_R13',
+        'patcherToolsTag'            : 'UPDATE_PACKAGING_R11_1',
         'ftpServer'                  : 'ftp.mozilla.org',
         'stagingServer'              : 'stage-old.mozilla.org',
         'bouncerServer'              : 'download.mozilla.org',
