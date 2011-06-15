4758a715a723fa72350f1a5bb8654fab797a40e7
Aki Sasaki <asasaki@mozilla.com>
2011-06-15 09:22:08 -0700

    bug 664048 - fennec 6.0b1 prep. r=lsblakk
    --HG--
    rename : mozilla2-staging/l10n-changesets_mobile-5.0.json => mozilla2-staging/l10n-changesets_mobile-6.0.json
    rename : mozilla2-staging/release-fennec-5.0.py => mozilla2-staging/release-fennec-6.0.py
    rename : mozilla2/l10n-changesets_mobile-5.0.json => mozilla2/l10n-changesets_mobile-6.0.json
    rename : mozilla2/release-fennec-5.0.py => mozilla2/release-fennec-6.0.py

--- debsign/config-staging.py
+++ debsign/config-staging.py
@@ -6,6 +6,7 @@ BRANCHES = {
     "mozilla-central": {},
     "mozilla-2.1": {},
     "mozilla-mobile-5.0": {},
+    "6.0": {},
 }
 BRANCHES["mozilla-central"]["script_repo_tag"] = "default"
 BRANCHES["mozilla-central"]["nightly_config_file"] = "deb_repos/staging_trunk_nightly.json"
@@ -29,3 +30,12 @@ BRANCHES["mozilla-mobile-5.0"]["enable_release"] = True
 BRANCHES["mozilla-mobile-5.0"]["release_config_file"] = "deb_repos/staging_5.0_release.json"
 BRANCHES["mozilla-mobile-5.0"]["release_platforms"] = ["fremantle"]
 BRANCHES["mozilla-mobile-5.0"]["nightly_hour"] = 9
+
+BRANCHES["6.0"]["script_repo_tag"] = "default"
+BRANCHES["6.0"]["enable_nightly"] = False
+BRANCHES["6.0"]["nightly_config_file"] = "deb_repos/staging_mozilla-beta_nightly.json"
+BRANCHES["6.0"]["nightly_platforms"] = ["fremantle", "fremantle-qt"]
+BRANCHES["6.0"]["enable_release"] = True
+BRANCHES["6.0"]["release_config_file"] = "deb_repos/staging_6.0_release.json"
+BRANCHES["6.0"]["release_platforms"] = ["fremantle"]
+BRANCHES["6.0"]["nightly_hour"] = 9
