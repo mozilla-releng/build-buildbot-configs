2e7d1d643cf083b2d62f9c87f90b02b8faa3d2e7
Lukas Blakk <lsblakk@mozilla.com>
2011-06-15 12:05:24 +0200

    Bug 664259 - run all jobs on birch p=lsblakk r=rail

--- mozilla/project_branches.py
+++ mozilla/project_branches.py
@@ -176,45 +176,7 @@ PROJECT_BRANCHES = {
     },
     #####  TWIGS aka RENTABLE BRANCHES
     'alder': {},
-    'birch': {
-        'platforms': {
-            'macosx': {
-                'enable_opt_unittests': False,
-                'enable_debug_unittests': False,
-            },
-            'macosx-debug': {
-                'dont_build': True,
-            },
-            'macosx64': {
-                'enable_opt_unittests': False,
-                'enable_debug_unittests': False,
-            },
-            'macosx64-debug': {
-                'dont_build': True,
-            },
-            'linux': {
-                'enable_opt_unittests': False,
-                'enable_debug_unittests': False,
-            },
-            'linux-debug': {
-                'dont_build': True,
-            },
-            'linux64': {
-                'enable_opt_unittests': False,
-                'enable_debug_unittests': False,
-            },
-            'linux64-debug': {
-                'dont_build': True,
-            },
-            'win32': {
-                'enable_opt_unittests': False,
-                'enable_debug_unittests': False,
-            },
-            'win32-debug': {
-                'dont_build': True,
-            },
-        },
-    },
+    'birch': {},
     'cedar': {},
     'holly': {},
     'larch': {},
