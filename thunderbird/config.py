ffe9948b37f7aec79c0d1391482d544f2831300b
John Hopkins <john.hopkins@mozillamessaging.com>
2011-06-28 13:54:42 -0700

    Bug 635662 - Automatically update blocklist.xml in cvs/hg from blocklist service. r=gozer

--- thunderbird/config.py
+++ thunderbird/config.py
@@ -276,6 +276,14 @@ def makeBuildConfig(builderType=None, branchName=None, hgBranch=None,
     bc['tinderbox_tree'] = tinderboxTree
     if builderType == 'nightly':
         bc['branch_name'] = branchName
+        # Blocklist settings
+        bc['repo_path'] = bc['hg_branch'] # alias
+        bc['product_name'] = 'thunderbird'
+        bc['enable_blocklist_update'] = True
+        bc['blocklist_update_on_closed_tree'] = False
+        bc['hg_ssh_key'] = '/home/cltbld/.ssh/tbirdbld_dsa'
+        bc['hg_username'] = 'tbirdbld'
+        # end Blocklist settings
         bc['client_py_extra_args'] = \
             ['--skip-comm',
              '--hg-options=--verbose --time',
