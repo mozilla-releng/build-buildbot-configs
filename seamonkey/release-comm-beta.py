5579fc501d15aa11051ca0aeb1d5889745ab3809
Justin Wood <Callek@gmail.com>
2011-07-02 03:55:40 -0400

    backout linux64 update stuffs, failed because no linux64 repacks
    --HG--
    branch : seamonkey-production

--- seamonkey/release-comm-beta.py
+++ seamonkey/release-comm-beta.py
@@ -63,7 +63,6 @@ testOlderPartials          = False
 releaseNotesUrl            = None
 useBetaChannel             = 0
 verifyConfigs              = {'linux':  'mozBeta-seamonkey-linux.cfg',
-                              'linux64': 'mozBeta-seamonkey-linux64.cfg',
                               'macosx64': 'mozBeta-seamonkey-mac64.cfg',
                               'win32':  'mozBeta-seamonkey-win32.cfg'}
 majorUpdateRepoPath        = None
