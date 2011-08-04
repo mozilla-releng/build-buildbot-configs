036f46c5f69570be7935f7cf8487a6f7caae1f66
Nick Thomas <nthomas@mozilla.com>
2011-08-04 20:28:06 +1200

    Bug 675732 - release configs for Firefox & Fennec 6.0b5, r=aki
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -14,16 +14,16 @@ releaseConfig['appName']             = 'browser'
 releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 #  Current version info
-releaseConfig['version']             = '6.0b4'
+releaseConfig['version']             = '6.0b5'
 releaseConfig['appVersion']          = '6.0'
 releaseConfig['milestone']           = '6.0'
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_6_0b4'
+releaseConfig['baseTag']             = 'FIREFOX_6_0b5'
 #  Old version info
-releaseConfig['oldVersion']          = '6.0b3'
+releaseConfig['oldVersion']          = '6.0b4'
 releaseConfig['oldAppVersion']       = '6.0'
-releaseConfig['oldBuildNumber']      = 2
-releaseConfig['oldBaseTag']          = 'FIREFOX_6_0b3'
+releaseConfig['oldBuildNumber']      = 1
+releaseConfig['oldBaseTag']          = 'FIREFOX_6_0b4'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = '6.0'
 releaseConfig['nextMilestone']       = '6.0'
@@ -32,7 +32,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': 'd66a5a76f6ba',
+        'revision': 'a1e3e6a625ce',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
