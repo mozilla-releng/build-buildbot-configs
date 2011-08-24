03355345a66de121cbff6f6325b47e9173ab1ee9
Aki Sasaki <asasaki@mozilla.com>
2011-08-24 16:56:06 -0700

    bug 681448 - 7.0b2 configs. p=coop r=me
    --HG--
    branch : production

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -14,16 +14,16 @@ releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '7.0b1'
+releaseConfig['version']             = '7.0b2'
 releaseConfig['appVersion']          = '7.0'
 releaseConfig['milestone']           = '7.0'
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_7_0b1'
+releaseConfig['baseTag']             = 'FENNEC_7_0b2'
 #  Old version info
-releaseConfig['oldVersion']          = '6.0b6'
-releaseConfig['oldAppVersion']       = '6.0'
+releaseConfig['oldVersion']          = '7.0b1'
+releaseConfig['oldAppVersion']       = '7.0'
 releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'FENNEC_6_0b6'
+releaseConfig['oldBaseTag']          = 'FENNEC_7_0b1'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = '7.0'
 releaseConfig['nextMilestone']       = '7.0'
@@ -32,7 +32,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '8786e3e49240',
+        'revision': '7e3966aece57',
         'relbranch': None,
         'bumpFiles': {
             'mobile/confvars.sh': {
