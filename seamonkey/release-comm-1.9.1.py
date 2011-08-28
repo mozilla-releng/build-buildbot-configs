881ea36ae839548f1cf4a6578be15c3dde7675fa
Justin Wood <Callek@gmail.com>
2011-08-28 18:02:59 -0400

    Bug 682739, SeaMonkey 2.0.14 -> SeaMonkey 2.3.1 Major Update
    --HG--
    branch : seamonkey-production

--- seamonkey/release-comm-1.9.1.py
+++ seamonkey/release-comm-1.9.1.py
@@ -71,11 +71,11 @@ majorUpdatePatcherConfig  = 'moz191-seamonkey-branch-major-patcher2.cfg'
 majorUpdateVerifyConfigs   = {'linux':  'moz191-seamonkey-linux-major.cfg',
                               'macosx': 'moz191-seamonkey-mac-major.cfg',
                               'win32':  'moz191-seamonkey-win32-major.cfg'}
-majorUpdateToVersion       = '2.2'
-majorUpdateAppVersion      = '2.2'
-majorUpdateBaseTag         = 'SEAMONKEY_2_2'
-majorUpdateBuildNumber     = 2
-majorUpdateReleaseNotesUrl = 'https://www.mozilla.org/start/2.2/%locale%/index.html'
+majorUpdateToVersion       = '2.3.1'
+majorUpdateAppVersion      = '2.3.1'
+majorUpdateBaseTag         = 'SEAMONKEY_2_3_1'
+majorUpdateBuildNumber     = 1
+majorUpdateReleaseNotesUrl = 'https://www.mozilla.org/start/unsupported/%locale%/index.html'
 
 # Tuxedo/Bouncer related - XXX: atm not allowed for SeaMonkey
 #tuxedoConfig        = 'seamonkey-tuxedo.ini'
