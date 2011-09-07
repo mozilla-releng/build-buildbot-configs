9ca77226b89e4d375eefb1a8f9c07ef609270bf0
Mark Banner <bugzilla@standard8.plus.com>
2011-09-07 14:19:43 +0100

    Calendar changes: Part of bug 682540 - reduce the length of paths on the builders so that we don't get issues with long path names, and bug 685133 specify initial Lightning 1.0b6 configuration for a comm-beta based release

--- calendar/release_calendar.py
+++ calendar/release_calendar.py
@@ -1,17 +1,17 @@
 hgUsername                 = 'calbld'
 hgSshKey                   = '~calbld/.ssh/calbld_dsa'
 relbranchPrefix            = 'COMM'
-sourceRepoName             = 'comm-aurora-lightning' # buildbot branch name
-sourceRepoPath             = 'releases/comm-aurora'
-sourceRepoRevision         = '653d6c77512a'
+sourceRepoName             = 'comm-beta' # buildbot branch name
+sourceRepoPath             = 'releases/comm-beta'
+sourceRepoRevision         = '900579ee49e0'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
-relbranchOverride          = 'COMM50_20110620_RELBRANCH'
-mozillaRepoPath            = 'releases/mozilla-aurora'
-mozillaRepoRevision        = '9dcd813b2fc8'
+relbranchOverride          = 'COMM70_20110831_RELBRANCH'
+mozillaRepoPath            = 'releases/mozilla-beta'
+mozillaRepoRevision        = 'ff20a21364bb'
 # If blank, automation will create its own branch based on COMM_<date>_RELBRANCH
 # You typically want to set this to the gecko relbranch if doing a release off
 # a specific gecko version.
-mozillaRelbranchOverride   = 'COMM50_20110620_RELBRANCH'
+mozillaRelbranchOverride   = 'GECKO70_2011083111_RELBRANCH'
 inspectorRepoPath          = '' # leave empty if inspector is not to be tagged
 inspectorRepoRevision      = ''
 inspectorRelbranchOverride = ''
@@ -23,7 +23,7 @@ venkmanRepoRevision        = ''
 venkmanRelbranchOverride   = ''
 chatzillaCVSRoot           = ''
 chatzillaTimestamp         = '' # leave empty if chatzilla is not to be tagged
-l10nRepoPath               = 'releases/l10n/mozilla-aurora'
+l10nRepoPath               = 'releases/l10n/mozilla-beta'
 l10nRevisionFile           = 'l10n-calendar-changesets'
 toolsRepoPath              = 'build/tools'
 cvsroot                    = ':ext:calbld@cvs.mozilla.org:/cvsroot' # for patcher, etc.
@@ -38,26 +38,27 @@ projectName                = 'sunbird'
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '1.0b4'
-appVersion                 = '1.0b4'
+version                    = '1.0b6'
+appVersion                 = '1.0b6'
 #XXX: Not entirely certain if/where this is used.
-milestone                  = '5.0'
+milestone                  = '7.0'
 buildNumber                = 1
-baseTag                    = 'CALENDAR_1_0b4'
+baseTag                    = 'CALENDAR_1_0b6'
 # The old version is the revision from which we should generate update snippets.
-oldVersion                 = '1.0b2'
+oldVersion                 = '1.0b5'
 oldAppVersion              = oldVersion
-oldBuildNumber             = 2
+oldBuildNumber             = 1
 oldBaseTag                 = ''
 releasePlatforms           = ()
 patcherConfig              = 'moz19-calendar-branch-patcher2.cfg'
-patcherToolsTag            = 'UPDATE_PACKAGING_R11'
+patcherToolsTag            = 'UPDATE_PACKAGING_R14'
 ftpServer                  = 'ftp.mozilla.org'
 stagingServer              = 'stage-old.mozilla.org'
 bouncerServer              = 'download.mozilla.org'
 ausServerUrl               = 'https://aus2-community.mozilla.org'
 useBetaChannel             = 1
-l10nPlatforms              = ('linux', 'win32', 'macosx')
+#l10nPlatforms              = ('linux', 'win32', 'macosx')
+l10nPlatforms              = ()
 verifyConfigs              = {'linux':  'moz19-calendar-linux.cfg',
                               'macosx': 'moz19-calendar-mac.cfg',
                               'win32':  'moz19-calendar-win32.cfg'}
