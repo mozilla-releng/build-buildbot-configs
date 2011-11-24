8fd1aed635edff1e0b663a56bfee1f55f202a964
Ben Hearsum <bhearsum@mozilla.com>
2011-11-24 10:24:51 -0500

    bug 700860: Put mw32-ix-slave26 into the production pool. r=armenzg

--- mozilla/preproduction_config.py
+++ mozilla/preproduction_config.py
@@ -1,7 +1,7 @@
 MAC_SNOW_MINIS = ['moz2-darwin10-slave%02i' % x for x in range(1,30) + range(40,57)]
 MAC_MINIS      = ['moz2-darwin9-slave%02i' % x for x in range(1,73) if x not in (4,5,20,40,59)]
 XSERVES        = ['bm-xserve%02i' % x for x in range(6,13) + range(15,25)]
-WIN32_IXS      = ['mw32-ix-slave%02i' % x for x in range(1,26)] + ['w32-ix-slave%02i' % x for x in range(1,45)]
+WIN32_IXS      = ['mw32-ix-slave%02i' % x for x in range(1,27)] + ['w32-ix-slave%02i' % x for x in range(1,45)]
 WIN64_IXS      = ['mw64-ix-slave01'] + ['w64-ix-slave%02i' % x for x in range(1,43)]
 LINUX_VMS      = ['moz2-linux-slave%02i' % x for x in range(1,61)]
 LINUX_IXS      = ['mv-moz2-linux-ix-slave%02i' % x for x in range(1,24)] + ['linux-ix-slave%02i' % x for x in range(1,43)]
