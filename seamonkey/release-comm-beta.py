217c71d3f23709a3d0c90af3c76052bbb6a2fea4
Edmund Wong <ewong@pw-wspx.org>
2011-12-08 11:42:53 +0800

    Bug 708513 - Update configs for SeaMonkey 2.6 Beta 3.
    --HG--
    branch : seamonkey-production

--- seamonkey/release-comm-beta.py
+++ seamonkey/release-comm-beta.py
@@ -3,11 +3,11 @@ hgSshKey                   = '~seabld/.ssh/seabld_dsa'
 relbranchPrefix            = 'SEA_COMM'
 sourceRepoName             = 'comm-beta' # buildbot branch name
 sourceRepoPath             = 'releases/comm-beta'
-sourceRepoRevision         = 'a03506c2ab9f'
+sourceRepoRevision         = 'e65027f51cf3'
 relbranchOverride          = ''
 mozillaRepoPath            = 'releases/mozilla-beta'
-mozillaRepoRevision        = 'FIREFOX_9_0b4_BUILD1'
-mozillaRelbranchOverride   = 'GECKO90_2011113005_RELBRANCH' # put Gecko relbranch here that we base upon
+mozillaRepoRevision        = 'FIREFOX_9_0b5_BUILD1'
+mozillaRelbranchOverride   = 'GECKO90_2011120622_RELBRANCH' # put Gecko relbranch here that we base upon
 inspectorRepoPath          = 'dom-inspector' # leave empty if inspector is not to be tagged
 inspectorRepoRevision      = 'b075d299d443'
 inspectorRelbranchOverride = 'DOMI_2_0_10'
@@ -37,16 +37,16 @@ skip_tag                   =  False
 # appVersion and oldAppVersion are optional definitions used in places that
 # don't care about what we call it. Eg, when version bumping we will bump to
 # appVersion, not version.
-version                    = '2.6b2'
+version                    = '2.6b3'
 usePrettyLongVer           = False
 appVersion                 = '2.6'
 milestone                  = '9.0'
 buildNumber                = 1
-baseTag                    = 'SEAMONKEY_2_6b2'
-oldVersion                 = '2.6b1'
+baseTag                    = 'SEAMONKEY_2_6b3'
+oldVersion                 = '2.6b2'
 oldAppVersion              = '2.6'
 oldBuildNumber             = 1
-oldBaseTag                 = 'SEAMONKEY_2_6b1'
+oldBaseTag                 = 'SEAMONKEY_2_6b2'
 oldRepoPath                = 'releases/comm-beta'
 enUSPlatforms              = ('linux', 'linux64', 'win32', 'macosx64')
 l10nPlatforms              = ('linux', 'win32', 'macosx64')
