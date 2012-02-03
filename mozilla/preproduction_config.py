1d84f918d28f35b21a6c81baaf282d5ccc148142
Chris Cooper <ccooper@deadsquid.com>
2012-02-03 11:09:48 -0500

    Bug 705403 - Add buildbotve to PATH for try - r=armenzg

--- mozilla/preproduction_config.py
+++ mozilla/preproduction_config.py
@@ -94,7 +94,7 @@ BRANCHES = {
                     # Source server support, bug 506702
                     'PDBSTR_PATH': '/c/Program Files/Debugging Tools for Windows/srcsrv/pdbstr.exe',
                     'HG_SHARE_BASE_DIR': 'e:/builds/hg-shared',
-
+                    'PATH': "${MOZILLABUILD}buildbotve\\scripts;${PATH}",
                 },
             },
         }
