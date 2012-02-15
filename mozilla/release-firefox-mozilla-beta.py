560ba16b4389e494d92256949e7f57f5ea775bb8
Aki Sasaki <asasaki@mozilla.com>
2012-02-14 19:14:56 -0800

    bug 718900 - 11.0b3 configs. r=nthomas

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -15,16 +15,16 @@ releaseConfig['appName']             = 'browser'
 releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 #  Current version info
-releaseConfig['version']             = '11.0b2'
+releaseConfig['version']             = '11.0b3'
 releaseConfig['appVersion']          = '11.0'
 releaseConfig['milestone']           = '11.0'
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_11_0b2'
+releaseConfig['baseTag']             = 'FIREFOX_11_0b3'
 #  Old version info
-releaseConfig['oldVersion']          = '11.0b1'
+releaseConfig['oldVersion']          = '11.0b2'
 releaseConfig['oldAppVersion']       = '11.0'
 releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'FIREFOX_11_0b1'
+releaseConfig['oldBaseTag']          = 'FIREFOX_11_0b2'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -33,7 +33,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': 'c0feb8e404a0',
+        'revision': '805226ae6764',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
