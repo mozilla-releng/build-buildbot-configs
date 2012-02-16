851fc8370441589bfc57c3862a97f689908a5d01
Aki Sasaki <asasaki@mozilla.com>
2012-02-15 21:51:02 -0800

    bug 727629 - 10.0.2 configs. p=nthomas r=aki
    --HG--
    branch : production

--- mozilla/release-fennec-mozilla-release.py
+++ mozilla/release-fennec-mozilla-release.py
@@ -15,16 +15,16 @@ releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '10.0.1'
+releaseConfig['version']             = '10.0.2'
 releaseConfig['appVersion']          = releaseConfig['version']
 releaseConfig['milestone']           = releaseConfig['version']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_10_0_1'
+releaseConfig['baseTag']             = 'FENNEC_10_0_2'
 #  Old version info
-releaseConfig['oldVersion']          = '10.0'
+releaseConfig['oldVersion']          = '10.0.1'
 releaseConfig['oldAppVersion']       = releaseConfig['oldVersion']
 releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'FENNEC_10_0'
+releaseConfig['oldBaseTag']          = 'FENNEC_10_0_1'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['version']
 releaseConfig['nextMilestone']       = releaseConfig['version']
@@ -33,7 +33,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-release',
         'path': 'releases/mozilla-release',
-        'revision': '18ce5e304e97',
+        'revision': 'bd611a3115b0',
         'relbranch': None,
         'bumpFiles': {
             'mobile/confvars.sh': {
