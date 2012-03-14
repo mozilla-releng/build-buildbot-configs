5d243dbad4b6374547c1e0cae3e7a9bcf9a24582
Rail Aliiev <rail@mozilla.com>
2012-03-14 11:52:19 -0400

    Bug 735681 - Build Thunderbird 12 beta 1. r=mbanner

--- thunderbird/release_master.py
+++ thunderbird/release_master.py
@@ -134,15 +134,15 @@ gloConfig = {
         'sourceRepoName'             : 'comm-beta', # buildbot branch name
         'sourceRepoPath'             : 'releases/comm-beta',
         'oldRepoPath'                : 'releases/comm-beta',
-        'sourceRepoRevision'         : 'e2eba41d5d80',
+        'sourceRepoRevision'         : 'd080a8ebf16a',
         # 'If' blank, automation will create its own branch based on COMM_<date>_RELBRANCH
         'relbranchOverride'          : '',
         'mozillaRepoPath'            : 'releases/mozilla-beta',
-        'mozillaRepoRevision'        : '7c1525a92228',
+        'mozillaRepoRevision'        : 'ef24dacc62ef',
         # 'If' blank, automation will create its own branch based on COMM_<date>_RELBRANCH
         # 'You' typically want to set this to the gecko relbranch if doing a release off
         # 'a' specific gecko version.
-        'mozillaRelbranchOverride'   : 'COMM110_2012030905_RELBRANCH', # put Gecko relbranch here that we base upon
+        'mozillaRelbranchOverride'   : '', # put Gecko relbranch here that we base upon
         'inspectorRepoPath'          : '', #'dom-inspector', # leave empty if inspector is not to be tagged
         'inspectorRepoRevision'      : '',
         'inspectorRelbranchOverride' : '',
@@ -173,17 +173,17 @@ gloConfig = {
         # 'appVersion' and oldAppVersion are optional definitions used in places that
         # 'don''t care about what we call it. Eg, when version bumping we will bump to
         # 'appVersion', not version.
-        'version'                    : '11.0b6',
+        'version'                    : '12.0b1',
         'oldVersion'                 : '11.0b5',
-        'appVersion'                 : '11.0', # no 'b1' suffix for betas
+        'appVersion'                 : '12.0', # no 'b1' suffix for betas
         'oldAppVersion'              : '11.0',
         'buildNumber'                : 1,
         'oldBuildNumber'             : 2,
-        'baseTag'                    : 'THUNDERBIRD_11_0b6',
+        'baseTag'                    : 'THUNDERBIRD_12_0b1',
         'oldBaseTag'                 : 'THUNDERBIRD_11_0b5',
         #XXX: 'Not' entirely certain if/where this is used.
         # 'Derived' from mozillaRelbranchOverride. eg: COMM19211_20101004_RELBRANCH == 1.9.2.11
-        'milestone'                  : '11.0',
+        'milestone'                  : '12.0',
         # 'The' old version is the revision from which we should generate update snippets.
         'enable_weekly_bundle'       : True,
         'enUSPlatforms'              : ('linux', 'linux64', 'win32', 'macosx64'),
