c1dd76e16380182177e0490e9e643e82a26aa65a
Hal Wine <hwine@mozilla.com>
2012-04-03 17:48:46 -0700

    bug 735388 - 12.0b4 config files; r=nthomas
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -15,16 +15,16 @@ releaseConfig['appName']             = 'browser'
 releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 #  Current version info
-releaseConfig['version']             = '12.0b3'
+releaseConfig['version']             = '12.0b4'
 releaseConfig['appVersion']          = '12.0'
 releaseConfig['milestone']           = '12.0'
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_12_0b3'
+releaseConfig['baseTag']             = 'FIREFOX_12_0b4'
 #  Old version info
-releaseConfig['oldVersion']          = '12.0b2'
+releaseConfig['oldVersion']          = '12.0b3'
 releaseConfig['oldAppVersion']       = '12.0'
 releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'FIREFOX_12_0b2'
+releaseConfig['oldBaseTag']          = 'FIREFOX_12_0b3'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -33,7 +33,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '5f8213f4be4e',
+        'revision': 'e4ed83ba6eb9',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
