c7f8af7cc4161d6202fa280d1adab01d8e306e21
Edmund Wong <ewong@pw-wspx.org>
2012-04-19 09:06:48 +0800

    Bug 746831 - Update configs for SeaMonkey 2.9b4. r=Callek
    --HG--
    branch : seamonkey-production

--- seamonkey/release-comm-beta.py
+++ seamonkey/release-comm-beta.py
@@ -8,16 +8,16 @@ releaseConfig['productName']                = 'seamonkey'
 releaseConfig['brandName']                  = 'SeaMonkey'
 releaseConfig['appName']                    = 'suite'
 #  Current version info
-releaseConfig['version']                    = '2.9b3'
+releaseConfig['version']                    = '2.9b4'
 releaseConfig['appVersion']                 = '2.9'
 releaseConfig['milestone']                  = '12.0'
-releaseConfig['buildNumber']                = 2
-releaseConfig['baseTag']                    = 'SEAMONKEY_2_9b3'
+releaseConfig['buildNumber']                = 1
+releaseConfig['baseTag']                    = 'SEAMONKEY_2_9b4'
 #  Old version info
-releaseConfig['oldVersion']                 = '2.9b2'
+releaseConfig['oldVersion']                 = '2.9b3'
 releaseConfig['oldAppVersion']              = '2.9'
-releaseConfig['oldBuildNumber']             = 1
-releaseConfig['oldBaseTag']                 = 'SEAMONKEY_2_9b2'
+releaseConfig['oldBuildNumber']             = 2
+releaseConfig['oldBaseTag']                 = 'SEAMONKEY_2_9b3'
 releaseConfig['oldRepoPath']                = 'releases/comm-beta'
 #  Next (nightly) version info
 #     not yet available
@@ -26,14 +26,14 @@ releaseConfig['skip_tag']                   = False
 releaseConfig['relbranchPrefix']            = 'SEA_COMM'
 releaseConfig['sourceRepoName']             = 'comm-beta' # buildbot branch name
 releaseConfig['sourceRepoPath']             = 'releases/comm-beta'
-releaseConfig['sourceRepoRevision']         = 'cc5c2d528370'
-releaseConfig['relbranchOverride']          = 'SEA_COMM120_20120411_RELBRANCH'
+releaseConfig['sourceRepoRevision']         = '33ee8cf6630f'
+releaseConfig['relbranchOverride']          = ''
 #releaseConfig['productVersionFile']        = 'suite/config/version-20.txt'
 releaseConfig['productVersionFile']         = ''
 #   Mozilla
 releaseConfig['mozillaRepoPath']            = 'releases/mozilla-beta'
-releaseConfig['mozillaRepoRevision']        = 'FIREFOX_12_0b5_BUILD1'
-releaseConfig['mozillaRelbranchOverride']   = 'GECKO120_2012041106_RELBRANCH' # put Gecko relbranch here that we base upon
+releaseConfig['mozillaRepoRevision']        = 'FIREFOX_12_0b6_BUILD1'
+releaseConfig['mozillaRelbranchOverride']   = 'GECKO120_2012041716_RELBRANCH' # put Gecko relbranch here that we base upon
 #   Inspector
 releaseConfig['inspectorRepoPath']          = 'dom-inspector' # leave empty if inspector is not to be tagged
 releaseConfig['inspectorRepoRevision']      = 'b72c3dc211a0'
