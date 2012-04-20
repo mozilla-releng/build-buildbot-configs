0c05e1bf9613a7f3c8b626967bd4a5648dfeccb2
Ben Hearsum <bhearsum@mozilla.com>
2012-04-20 16:27:12 -0400

    bug 735392: Tracking bug for build & release of Firefox 12.0. r=rail

--- mozilla/release-firefox-mozilla-release.py
+++ mozilla/release-firefox-mozilla-release.py
@@ -15,16 +15,16 @@ releaseConfig['appName']             = 'browser'
 releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 #  Current version info
-releaseConfig['version']             = '11.0'
+releaseConfig['version']             = '12.0'
 releaseConfig['appVersion']          = releaseConfig['version']
 releaseConfig['milestone']           = releaseConfig['version']
-releaseConfig['buildNumber']         = 2
-releaseConfig['baseTag']             = 'FIREFOX_11_0'
+releaseConfig['buildNumber']         = 1
+releaseConfig['baseTag']             = 'FIREFOX_12_0'
 #  Old version info
-releaseConfig['oldVersion']          = '10.0.2'
+releaseConfig['oldVersion']          = '11.0'
 releaseConfig['oldAppVersion']       = releaseConfig['oldVersion']
-releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'FIREFOX_10_0_2'
+releaseConfig['oldBuildNumber']      = 2
+releaseConfig['oldBaseTag']          = 'FIREFOX_11_0'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -33,7 +33,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-release',
         'path': 'releases/mozilla-release',
-        'revision': '398407cca90c',
+        'revision': '5bcfa0da3be9',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
