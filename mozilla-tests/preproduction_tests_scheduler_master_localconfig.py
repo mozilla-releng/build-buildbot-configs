aa5d2d27f8ec0a7aa4bbf6462227ffbba7697f67
Rail Aliiev <rail@mozilla.com>
2012-04-20 17:19:28 -0400

    Bug 747424 - Support Thunderbird in preproduction. r=aki

--- mozilla-tests/preproduction_tests_scheduler_master_localconfig.py
+++ mozilla-tests/preproduction_tests_scheduler_master_localconfig.py
@@ -5,9 +5,12 @@ from buildbot import manhole
 c['manhole'] = manhole.PasswordManhole("tcp:1233:interface=127.0.0.1", "cltbld", "password")
 
 from config import BRANCHES, PLATFORMS, PROJECTS
+import thunderbird_config
 # Do everything!
 ACTIVE_BRANCHES = BRANCHES.keys()
+ACTIVE_THUNDERBIRD_BRANCHES = thunderbird_config.BRANCHES.keys()
 ACTIVE_PLATFORMS = dict((platform, None) for platform in PLATFORMS.keys())
+ACTIVE_THUNDERBIRD_PLATFORMS = dict((platform, None) for platform in thunderbird_config.PLATFORMS.keys())
 ACTIVE_PROJECTS = PROJECTS.keys()
 
 QUEUEDIR = "/dev/shm/queue"
