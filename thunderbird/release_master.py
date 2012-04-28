28998fc96dab4203ac2db99a28cb7a0a33d2b0bf
Mark Banner <bugzilla@standard8.plus.com>
2012-04-28 18:52:22 +0100

    Build config for Thunderbird 12.0.1 build 1

--- thunderbird/release_master.py
+++ thunderbird/release_master.py
@@ -237,7 +237,7 @@ gloConfig = {
         'sourceRepoName'             : 'comm-release', # buildbot branch name
         'sourceRepoPath'             : 'releases/comm-release',
         'oldRepoPath'                : 'releases/comm-release',
-        'sourceRepoRevision'         : '37620e34f3a4',
+        'sourceRepoRevision'         : 'b5ed052dcc54',
         # 'If' blank, automation will create its own branch based on COMM_<date>_RELBRANCH
         'relbranchOverride'          : '',
         'mozillaRepoPath'            : 'releases/mozilla-release',
@@ -245,7 +245,7 @@ gloConfig = {
         # 'If' blank, automation will create its own branch based on COMM_<date>_RELBRANCH
         # 'You' typically want to set this to the gecko relbranch if doing a release off
         # 'a' specific gecko version.
-        'mozillaRelbranchOverride'   : '', # put Gecko relbranch here that we base upon
+        'mozillaRelbranchOverride'   : 'TB_COMM120_20120420_RELBRANCH', # put Gecko relbranch here that we base upon
         'inspectorRepoPath'          : '', #'dom-inspector', # leave empty if inspector is not to be tagged
         'inspectorRepoRevision'      : '',
         'inspectorRelbranchOverride' : '',
@@ -276,17 +276,17 @@ gloConfig = {
         # 'appVersion' and oldAppVersion are optional definitions used in places that
         # 'don''t care about what we call it. Eg, when version bumping we will bump to
         # 'appVersion', not version.
-        'version'                    : '12.0',
-        'appVersion'                 : '12.0', # no 'b1' suffix for betas
+        'version'                    : '12.0.1',
+        'appVersion'                 : '12.0.1', # no 'b1' suffix for betas
         #XXX: 'Not' entirely certain if/where this is used.
         # 'Derived' from mozillaRelbranchOverride. eg: COMM19211_20101004_RELBRANCH == 1.9.2.11
         'milestone'                  : '12.0',
         'buildNumber'                : 1,
-        'baseTag'                    : 'THUNDERBIRD_12_0',
-        'oldVersion'                 : '11.0.1',
-        'oldAppVersion'              : '11.0.1',
+        'baseTag'                    : 'THUNDERBIRD_12_0_1',
+        'oldVersion'                 : '12.0',
+        'oldAppVersion'              : '12.0',
         'oldBuildNumber'             : 1,
-        'oldBaseTag'                 : 'THUNDERBIRD_11_0_1',
+        'oldBaseTag'                 : 'THUNDERBIRD_12_0',
         'enable_weekly_bundle'       : False,
         'enUSPlatforms'              : ('linux', 'linux64', 'win32', 'macosx64'),
         #'l10nPlatforms'              : (),
