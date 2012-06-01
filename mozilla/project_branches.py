84b1445e9b28ee90dd87520a6f6eee84f2d18976
Ed Morley <emorley@mozilla.com>
2012-06-01 16:22:23 +0100

    Bug 759080 - Disable talos on the profiling branch & remove redundant entry for build-system; r=armenzg

--- mozilla/project_branches.py
+++ mozilla/project_branches.py
@@ -25,7 +25,6 @@ PROJECT_BRANCHES = {
         ]
     },
     'build-system': {
-        'enable_talos': True,
         'pgo_strategy': 'per-checkin',
         'platforms': {
             'win32': {
@@ -134,7 +133,7 @@ PROJECT_BRANCHES = {
 #        },
 #    },
     'profiling': {
-        'enable_talos': True,
+        'enable_talos': False,
         'enable_nightly': True,
         'create_snippet': True,
         'create_partial': True,
