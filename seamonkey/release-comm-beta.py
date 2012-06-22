2a47c6c2a45edd5c2f1ff9ba5c977076afbc0674
Edmund Wong <ewong@pw-wspx.org>
2012-06-22 10:58:22 +0800

    Bug 766839 - Update configs for SeaMonkey 2.11b3 build1 - fix.
    --HG--
    branch : seamonkey-production

--- seamonkey/release-comm-beta.py
+++ seamonkey/release-comm-beta.py
@@ -16,7 +16,7 @@ releaseConfig['baseTag']                    = 'SEAMONKEY_2_11b3'
 #  Old version info
 releaseConfig['oldVersion']                 = '2.11b2'
 releaseConfig['oldAppVersion']              = '2.11'
-releaseConfig['oldBuildNumber']             = 2
+releaseConfig['oldBuildNumber']             = 1
 releaseConfig['oldBaseTag']                 = 'SEAMONKEY_2_11b2'
 releaseConfig['oldRepoPath']                = 'releases/comm-beta'
 #  Next (nightly) version info
