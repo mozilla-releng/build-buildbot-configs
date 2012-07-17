861138f53355433e6728e04dbb4d1cbb4351c18f
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2012-07-17 13:42:57 -0400

    Bug 772810. Thunderbird 15.0b1 changes. r=hwine
    --HG--
    branch : production
    extra : transplant_source : 9%E4%ACaQ%A9ABrY%BC%A6%06%05%08I%7D%E0U%D9

--- mozilla/release-thunderbird-comm-beta.py
+++ mozilla/release-thunderbird-comm-beta.py
@@ -20,16 +20,16 @@ releaseConfig['mozilla_dir']         = 'mozilla'
 releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 #  Current version info
-releaseConfig['version']             = '14.0b5'
-releaseConfig['appVersion']          = '14.0'
+releaseConfig['version']             = '15.0b1'
+releaseConfig['appVersion']          = '15.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'THUNDERBIRD_14_0b5'
+releaseConfig['baseTag']             = 'THUNDERBIRD_15_0b1'
 #  Old version info
-releaseConfig['oldVersion']          = '14.0b4'
+releaseConfig['oldVersion']          = '14.0b5'
 releaseConfig['oldAppVersion']       = '14.0'
 releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'THUNDERBIRD_14_0b4'
+releaseConfig['oldBaseTag']          = 'THUNDERBIRD_14_0b5'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -38,7 +38,7 @@ releaseConfig['sourceRepositories']  = {
     'comm': {
         'name': 'comm-beta',
         'path': 'releases/comm-beta',
-        'revision': '89070f84365e',
+        'revision': 'c3489d5b7b65',
         'relbranch': None,
         'bumpFiles': {
             'mail/config/version.txt': {
@@ -50,7 +50,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '58fc5e165c31',
+        'revision': 'eb421f1dd8ef',
         'relbranch': None,
         'bumpFiles': {},
     }
