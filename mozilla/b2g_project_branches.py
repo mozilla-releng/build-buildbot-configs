67e88099a6b9a9fb96930710e57deb6f43186f44
Ed Morley <emorley@mozilla.com>
2012-08-08 15:59:37 +0100

    Bug 775149 - Followup: Switch off B2G builds on the birch repo; r=catlee

--- mozilla/b2g_project_branches.py
+++ mozilla/b2g_project_branches.py
@@ -40,7 +40,8 @@ PROJECT_BRANCHES = {
     #####  TWIGS aka RENTABLE BRANCHES
     'alder': {},
     'ash': {},
-    'birch': {},
+    # Only need to build on OS X (testing gcc OS X builds still work)
+    #'birch': {},
     'cedar': {},
     # Customizations for windows update service changes (bug 481815)
     #'elm': {},
