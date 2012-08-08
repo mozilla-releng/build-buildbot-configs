a51b7bbbc1f943fff4dbd6b7d1505f93ed070a04
Kim Moir <kmoir@mozilla.com>
2012-08-08 13:39:11 -0400

    Bug 772030 - tracking bug for build and release of Firefox/Fennec 15.0b4 r=rail
    --HG--
    branch : production

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -16,16 +16,16 @@ releaseConfig['binaryName']          = releaseConfig['productName'].capitalize()
 releaseConfig['oldBinaryName']       = releaseConfig['binaryName']
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '15.0b3'
+releaseConfig['version']             = '15.0b4'
 releaseConfig['appVersion']          = '15.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_15_0b3'
+releaseConfig['baseTag']             = 'FENNEC_15_0b4'
 #  Old version info
-releaseConfig['oldVersion']          = '15.0b2'
+releaseConfig['oldVersion']          = '15.0b3'
 releaseConfig['oldAppVersion']       = '15.0'
 releaseConfig['oldBuildNumber']      = 1
-releaseConfig['oldBaseTag']          = 'FENNEC_15_0b2'
+releaseConfig['oldBaseTag']          = 'FENNEC_15_0b3'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -34,7 +34,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '1ac588e89dd5',
+        'revision': '611896e7d593',
         'relbranch': None,
         'bumpFiles': {
             'mobile/android/confvars.sh': {
