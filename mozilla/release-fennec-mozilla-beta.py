7ca3213a8d83c532fa2ea5b7c977051911403f15
John Hopkins <jhopkins@mozilla.com>
2012-10-09 13:05:02 -0400

    Bug 796959 - tracking bug for build and release of Firefox 17.0b1. r=bhearsum

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -14,11 +14,11 @@ releaseConfig['productName']         = 'fennec'
 releaseConfig['appName']             = 'mobile'
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '16.0b6'
-releaseConfig['appVersion']          = '16.0'
+releaseConfig['version']             = '17.0b1'
+releaseConfig['appVersion']          = '17.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_16_0b6'
+releaseConfig['baseTag']             = 'FENNEC_17_0b1'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -27,7 +27,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '8a8a932fbef4',
+        'revision': 'defe56708537',
         'relbranch': None,
         'bumpFiles': {
             'mobile/android/confvars.sh': {
