eff2874f2ed9e12fd12a5d831e36914ef0128efc
Justin Wood <Callek@gmail.com>
2012-10-15 19:43:53 -0400

    No Bug -- Remove my personal e-mail from extra recipients now that I'm an employee and won't need it there in the forseeable future. r=bhearsum over IRC
    --HG--
    extra : rebase_source : a19ba0c5b52dd9b33f853484b367e9745d01a0ba

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -3,7 +3,7 @@ releaseConfig['disable_tinderbox_mail'] = True
 releaseConfig['base_clobber_url'] = 'http://clobberer.pvt.build.mozilla.org/always_clobber.php'
 
 # Release Notification
-releaseConfig['AllRecipients']       = ['<release@mozilla.com>','<release-mgmt@mozilla.com>','<Callek@gmail.com>']
+releaseConfig['AllRecipients']       = ['<release@mozilla.com>','<release-mgmt@mozilla.com>']
 releaseConfig['ImportantRecipients'] = ['<release-drivers@mozilla.org>',]
 releaseConfig['releaseTemplates']    = 'release_templates'
 releaseConfig['messagePrefix']       = '[release] '
