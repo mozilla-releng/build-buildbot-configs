779439b41f3e14c115b872d3cd9152c63cc5edfb
Philipp Kewisch <mozilla@kewis.ch>
2012-11-14 21:32:28 +0200

    Config for Lightning 2.0b1

--- calendar/release_calendar.py
+++ calendar/release_calendar.py
@@ -1,16 +1,16 @@
 ### This needs to be filled in ###
 
-branchSuffix               = 'beta'
+branchSuffix               = 'aurora'
 
-sourceRepoRevision         = '4d57e83014b3'
-mozillaRepoRevision        = 'THUNDERBIRD_17_0b3_RELEASE'
+sourceRepoRevision         = '030433ea8826'
+mozillaRepoRevision        = 'default'
 
-milestone                  = '17.0'
+milestone                  = '18.0'
 
-version                    = '1.9'
+version                    = '2.0b1'
 buildNumber                = 1
 
-oldVersion                 = '1.9b2'
+oldVersion                 = '1.9'
 oldBuildNumber             = 1
 
 relbranchOverride          = ''
