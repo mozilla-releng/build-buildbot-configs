9168cb16807b998203beb7c5cf3f7ae6357b763c
bhearsum@mozilla.com <bhearsum@mozilla.com>
2012-11-16 08:11:37 -0500

    Update release config for Fennec-17.0b7-build1
    --HG--
    branch : production

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -14,11 +14,11 @@ releaseConfig['productName']         = 'fennec'
 releaseConfig['appName']             = 'mobile'
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '17.0b6'
+releaseConfig['version']             = '17.0b7'
 releaseConfig['appVersion']          = '17.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 2
-releaseConfig['baseTag']             = 'FENNEC_17_0b6'
+releaseConfig['buildNumber']         = 1
+releaseConfig['baseTag']             = 'FENNEC_17_0b7'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -27,8 +27,8 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': 'eba2b989092a',
-        'relbranch': 'MOBILE170_2012111306_RELBRANCH',
+        'revision': '000e362a815a',
+        'relbranch': None,
         'bumpFiles': {
             'mobile/android/confvars.sh': {
                 'version': releaseConfig['appVersion'],
