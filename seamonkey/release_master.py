8a84345a051a07b06ab3946653c710212676275f
Justin Wood <Callek@gmail.com>
2012-11-25 03:09:22 -0500

    Bug 813820 - Add tooltool directives to release factories for SeaMonkey 2.15 Beta 1
    --HG--
    branch : seamonkey-production

--- seamonkey/release_master.py
+++ seamonkey/release_master.py
@@ -317,6 +317,8 @@ for platform in releaseConfig['enUSPlatforms']:
         unittestMasters=unittestMasters,
         unittestBranch=unittestBranch,
         clobberURL=branchConfig['base_clobber_url'],
+        tooltool_manifest_src= pf.get('tooltool_manifest_src', None),
+        tooltool_url_list= branchConfig.get('tooltool_url_list', []),
     )
 
     builders.append({
@@ -359,6 +361,8 @@ for platform in releaseConfig['enUSPlatforms']:
             buildNumber=releaseConfig['buildNumber'],
             tree='release',
             clobberURL=branchConfig['base_clobber_url'],
+            tooltool_manifest_src= pf.get('tooltool_manifest_src', None),
+            tooltool_url_list= branchConfig.get('tooltool_url_list', []),
         )
 
         builders.append({
