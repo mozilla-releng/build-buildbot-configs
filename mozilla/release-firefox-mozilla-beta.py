f53be8f256de29ce1b109fed2b26fe07af6a5136
ffxbld <none@none>
2012-12-12 10:04:25 -0500

    Update release config for Firefox-18.0b4-build1

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -20,17 +20,17 @@ releaseConfig['messagePrefix']       = '[release] '
 releaseConfig['productName']         = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '18.0b3'
+releaseConfig['version']             = '18.0b4'
 releaseConfig['appVersion']          = '18.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_18_0b3'
+releaseConfig['baseTag']             = 'FIREFOX_18_0b4'
 releaseConfig['partialUpdates']      = {
 
-    '18.0b1': {
+    '18.0b3': {
         'appVersion': '18.0',
         'buildNumber': 1,
-        'baseTag': 'FIREFOX_18_0b1',
+        'baseTag': 'FIREFOX_18_0b3',
     },
 
     '18.0b2': {
@@ -48,7 +48,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '68cb786721c7',
+        'revision': 'e904ee016207',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
