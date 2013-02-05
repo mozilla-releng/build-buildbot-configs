51fd1226ead36def58f6654eca174fe670095d8c
Chris AtLee <catlee@mozilla.com>
2013-02-05 17:09:10 -0500

    Bug 838140: Use releng-otoro.py config for otoro builds. r=aki

--- mozilla/b2g_config.py
+++ mozilla/b2g_config.py
@@ -721,7 +721,7 @@ PLATFORM_VARS = {
                 'script_name': 'scripts/b2g_build.py',
                 # b2g_build.py will checkout gecko from hg and look up a tooltool manifest given by the
                 # --target name below
-                'extra_args': ['--target', 'otoro', '--config', 'b2g/releng.py',
+                'extra_args': ['--target', 'otoro', '--config', 'b2g/releng-otoro.py',
                                '--gaia-languages-file', 'locales/languages_basecamp.json',
                                '--gecko-languages-file', 'gecko/b2g/locales/all-locales'],
                 'reboot_command': ['bash', '-c', 'sudo reboot; sleep 600'],
