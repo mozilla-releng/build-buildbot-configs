faaa899785c5d12822fcbbdaa1ad70e1182dcfff
Philipp Kewisch <mozilla@kewis.ch>
2013-02-10 14:59:00 +0100

    Lightning 1.9.1: add configuration for comm-esr17

--- calendar/config.py
+++ calendar/config.py
@@ -95,7 +95,139 @@ BRANCHES = {
     'comm-release': {},
     'comm-beta': {},
     'comm-aurora': {},
+    'comm-esr17': {}
 #    'comm-central': {},
+}
+
+######## lightning-esr17
+# All platforms being built for this branch MUST be listed here.
+BRANCHES['comm-esr17']['platforms'] = {
+    'linux': {},
+    'linux64': {},
+    'win32': {},
+    'macosx64': {},
+}
+
+BRANCHES['comm-esr17']['mozilla_central_branch'] = 'releases/mozilla-esr17'
+BRANCHES['comm-esr17']['download_base_url'] = DOWNLOAD_BASE_URL + 'sunbird'
+BRANCHES['comm-esr17']['branch_name'] = 'comm-esr17'
+BRANCHES['comm-esr17']['hg_branch'] = 'releases/comm-esr17'
+BRANCHES['comm-esr17']['client_py_args'] = ['--skip-comm', '--skip-chatzilla', '--skip-venkman'] + ['--mozilla-repo=http://hg.mozilla.org/releases/mozilla-esr17']
+BRANCHES['comm-esr17']['mozconfig'] = 'mozconfig-lightning'
+BRANCHES['comm-esr17']['period'] = 60 * 60 * 10
+BRANCHES['comm-esr17']['nightly_hour'] = [0]
+BRANCHES['comm-esr17']['nightly'] = False
+# Lightning doesn't need to package Thunderbird
+BRANCHES['comm-esr17']['package'] = False
+BRANCHES['comm-esr17']['upload_stage'] = True
+BRANCHES['comm-esr17']['upload_complete_mar'] = False
+#Might be better off per-platform instead of per-branch here.
+BRANCHES['comm-esr17']['upload_glob'] = "mozilla/dist/xpi-stage/{lightning,gdata-provider}.xpi"
+BRANCHES['comm-esr17']['stage_username'] = 'calbld'
+BRANCHES['comm-esr17']['stage_base_path'] = '/home/ftp/pub/mozilla.org/calendar/lightning'
+BRANCHES['comm-esr17']['stage_group'] = 'calendar'
+BRANCHES['comm-esr17']['stage_ssh_key'] = 'calbld_dsa'
+BRANCHES['comm-esr17']['l10n'] = False
+BRANCHES['comm-esr17']['l10n_tree'] = 'calendar10x'
+BRANCHES['comm-esr17']['l10n_repo'] = 'releases/l10n/mozilla-esr17'
+BRANCHES['comm-esr17']['irc_nick'] = 'calbuild'
+BRANCHES['comm-esr17']['irc_channels'] = [ 'maildev', 'calendar' ]
+BRANCHES['comm-esr17']['extensions'] = {
+    'lightning': {
+        'subdir': "calendar/lightning",
+        'download_base_url': DOWNLOAD_BASE_URL + 'lightning',
+        'l10n': True
+    }
+}
+
+BRANCHES['comm-esr17']['platforms']['linux']['base_name'] = 'Linux comm-esr17 lightning'
+BRANCHES['comm-esr17']['platforms']['linux64']['base_name'] = 'Linux x86_64 comm-esr17 lightning'
+BRANCHES['comm-esr17']['platforms']['win32']['base_name'] = 'WINNT 5.2 comm-esr17 lightning'
+BRANCHES['comm-esr17']['platforms']['macosx64']['base_name'] = 'MacOSX 10.6 comm-esr17 lightning'
+BRANCHES['comm-esr17']['platforms']['linux']['profiled_build'] = False
+BRANCHES['comm-esr17']['platforms']['linux64']['profiled_build'] = False
+BRANCHES['comm-esr17']['platforms']['win32']['profiled_build'] = False
+BRANCHES['comm-esr17']['platforms']['macosx64']['profiled_build'] = False
+BRANCHES['comm-esr17']['platforms']['linux']['milestone'] = "comm-esr17/linux-xpi"
+BRANCHES['comm-esr17']['platforms']['linux64']['milestone'] = "comm-esr17/linux64-xpi"
+BRANCHES['comm-esr17']['platforms']['win32']['milestone'] = "comm-esr17/win32-xpi"
+BRANCHES['comm-esr17']['platforms']['macosx64']['milestone'] = "comm-esr17/macosx-xpi"
+BRANCHES['comm-esr17']['platforms']['macosx64']['upload_glob'] = "mozilla/dist/universal/xpi-stage/{lightning,gdata-provider}.xpi"
+# If True, a complete update snippet for this branch will be generated and
+# uploaded to. Any platforms with 'debug' in them will not have snippets
+# generated.
+BRANCHES['comm-esr17']['create_snippet'] = False
+BRANCHES['comm-esr17']['platforms']['linux']['update_platform'] = 'Linux_x86-gcc3'
+BRANCHES['comm-esr17']['platforms']['win32']['update_platform'] = 'WINNT_x86-msvc'
+BRANCHES['comm-esr17']['platforms']['linux64']['update_platform'] = 'Linux_x86_64-gcc3'
+BRANCHES['comm-esr17']['platforms']['macosx64']['update_platform'] = 'Darwin_Universal-gcc3'
+# If True, 'make buildsymbols' and 'make uploadsymbols' will be run
+# SYMBOL_SERVER_* variables are setup in the environment section below
+BRANCHES['comm-esr17']['platforms']['linux']['upload_symbols'] = True
+BRANCHES['comm-esr17']['platforms']['linux64']['upload_symbols'] = True
+BRANCHES['comm-esr17']['platforms']['win32']['upload_symbols'] = True
+BRANCHES['comm-esr17']['platforms']['macosx64']['upload_symbols'] = True
+BRANCHES['comm-esr17']['tinderbox_tree'] = 'Calendar-Beta'
+BRANCHES['comm-esr17']['platforms']['linux']['slaves'] = BUILDERS['linux']['community']
+BRANCHES['comm-esr17']['platforms']['linux64']['slaves'] = BUILDERS['linux64']['momo']
+BRANCHES['comm-esr17']['platforms']['win32']['slaves'] = BUILDERS['win32']['community-new']
+BRANCHES['comm-esr17']['platforms']['macosx64']['slaves'] = BUILDERS['macosx']['10.7']['community']
+
+# This is used in a bunch of places where something needs to be run from
+# the objdir. This is necessary because of universal builds on Mac
+# creating subdirectories inside of the objdir.
+BRANCHES['comm-esr17']['platforms']['linux']['platform_objdir'] = OBJDIR
+BRANCHES['comm-esr17']['platforms']['linux64']['platform_objdir'] = OBJDIR
+BRANCHES['comm-esr17']['platforms']['win32']['platform_objdir'] = OBJDIR
+BRANCHES['comm-esr17']['platforms']['macosx64']['platform_objdir'] = '%s/i386' % OBJDIR
+BRANCHES['comm-esr17']['platforms']['linux']['env'] = {'CVS_RSH': 'ssh',
+    'MOZ_OBJDIR': OBJDIR,
+    'TINDERBOX_OUTPUT': '1',
+    'MOZ_CRASHREPORTER_NO_REPORT': '1',
+    'SYMBOL_SERVER_HOST': SYMBOL_SERVER_HOST,
+    'SYMBOL_SERVER_USER': SYMBOL_SERVER_USER,
+    'SYMBOL_SERVER_PATH': SYMBOL_SERVER_PATH,
+    'POST_SYMBOL_UPLOAD_CMD': SYMBOL_SERVER_POST_UPLOAD_CMD,
+    'SYMBOL_SERVER_SSH_KEY': '/home/cltbld/.ssh/calbld_dsa',
+
+    # Bug 779192
+    'LD_LIBRARY_PATH': '/tools/gcc-4.5/lib',
+}
+BRANCHES['comm-esr17']['platforms']['linux64']['env'] = {'CVS_RSH': 'ssh',
+    'MOZ_OBJDIR': OBJDIR,
+    'TINDERBOX_OUTPUT': '1',
+    'MOZ_CRASHREPORTER_NO_REPORT': '1',
+    'SYMBOL_SERVER_HOST': SYMBOL_SERVER_HOST,
+    'SYMBOL_SERVER_USER': SYMBOL_SERVER_USER,
+    'SYMBOL_SERVER_PATH': SYMBOL_SERVER_PATH,
+    'SYMBOL_SERVER_SSH_KEY': '/home/cltbld/.ssh/calbld_dsa',
+
+    # Bug 779192
+    'LD_LIBRARY_PATH': '/tools/gcc-4.5/lib64',
+}
+BRANCHES['comm-esr17']['platforms']['win32']['env'] = {'CVS_RSH': 'ssh',
+    'MOZ_OBJDIR': OBJDIR,
+    'TINDERBOX_OUTPUT': '1',
+    'MOZ_CRASHREPORTER_NO_REPORT': '1',
+    'SYMBOL_SERVER_HOST': SYMBOL_SERVER_HOST,
+    'SYMBOL_SERVER_USER': SYMBOL_SERVER_USER,
+    'SYMBOL_SERVER_PATH': SYMBOL_SERVER_PATH,
+    'POST_SYMBOL_UPLOAD_CMD': SYMBOL_SERVER_POST_UPLOAD_CMD,
+    'SYMBOL_SERVER_SSH_KEY': '/c/Documents and Settings/cltlbld/.ssh/calbld_dsa',
+}
+BRANCHES['comm-esr17']['platforms']['macosx64']['env'] = {'CVS_RSH': 'ssh',
+    'MOZ_OBJDIR': OBJDIR,
+    'TINDERBOX_OUTPUT': '1',
+    'MOZ_CRASHREPORTER_NO_REPORT': '1',
+    'DISABLE_LIGHTNING_INSTALL': '1',
+    'SYMBOL_SERVER_HOST': SYMBOL_SERVER_HOST,
+    'SYMBOL_SERVER_USER': SYMBOL_SERVER_USER,
+    'SYMBOL_SERVER_PATH': SYMBOL_SERVER_PATH,
+    'POST_SYMBOL_UPLOAD_CMD': SYMBOL_SERVER_POST_UPLOAD_CMD,
+    'SYMBOL_SERVER_SSH_KEY': '/Users/calbld/.ssh/calbld_dsa',
+
+    # See bug 758694
+    'PATH': '/tools/python/bin:/tools/buildbot/bin:/opt/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin',
 }
 
 ######## lightning-release
