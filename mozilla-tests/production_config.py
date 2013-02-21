fd6b4ded88b475ff2cd33d14fc92ee238b20314f
kmoir@mozilla.com <kmoir@mozilla.com>
2013-02-21 07:45:09 -0500

    [Bug 841468] add panda-0033 and panda-0045 to devices.json and use them as b2g devices r=armenzg

--- mozilla-tests/production_config.py
+++ mozilla-tests/production_config.py
@@ -15,7 +15,7 @@ SLAVES = {
     'panda_android': dict(
         [('panda-%04i' % x, {'http_port': '30%03i' % x, 'ssl_port': '31%03i' % x}) for x in range(22,33) + range(34,45) + range(46,82) + range(522,874) + range(885,887)]
     ),
-    'b2g_panda': dict([("panda-%04i" % x, {}) for x in range(82,522)]),
+    'b2g_panda': dict([("panda-%04i" % x, {}) for x in range(82,522) + range(33,34) + range(45,46)]),
     'ubuntu32': dict([("tst-linux32-ec2-%03i" % x, {}) for x in range(1, 150) + range(300, 450)]),
     'ubuntu64': dict([("tst-linux64-ec2-%03i" % x, {}) for x in range(1, 150) + range(300, 450)]),
 }
