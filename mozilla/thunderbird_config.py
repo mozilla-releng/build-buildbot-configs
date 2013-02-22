8f35bd7bdc9ae414e30e196d3ef7c1a71ae2e04d
Ben Hearsum <bhearsum@mozilla.com>
2013-02-22 11:44:59 -0500

    bug 825959/825630: Build Thunderbird 17.0.4 esr with mock - use correct gcc for debug builds. r=catlee

--- mozilla/thunderbird_config.py
+++ mozilla/thunderbird_config.py
@@ -728,6 +728,10 @@ BRANCHES['comm-release']['platforms']['linux']['mock_packages'].remove('gcc45_0m
 BRANCHES['comm-release']['platforms']['linux']['mock_packages'].append('gcc45_0moz4')
 BRANCHES['comm-release']['platforms']['linux64']['mock_packages'].remove('gcc45_0moz3')
 BRANCHES['comm-release']['platforms']['linux64']['mock_packages'].append('gcc45_0moz4')
+BRANCHES['comm-release']['platforms']['linux-debug']['mock_packages'].remove('gcc45_0moz3')
+BRANCHES['comm-release']['platforms']['linux-debug']['mock_packages'].append('gcc45_0moz4')
+BRANCHES['comm-release']['platforms']['linux64-debug']['mock_packages'].remove('gcc45_0moz3')
+BRANCHES['comm-release']['platforms']['linux64-debug']['mock_packages'].append('gcc45_0moz4')
 
 ######## comm-esr17
 BRANCHES['comm-esr17']['moz_repo_path'] = 'releases/mozilla-esr17'
@@ -772,6 +776,10 @@ BRANCHES['comm-esr17']['platforms']['linux']['mock_packages'].remove('gcc45_0moz
 BRANCHES['comm-esr17']['platforms']['linux']['mock_packages'].append('gcc45_0moz4')
 BRANCHES['comm-esr17']['platforms']['linux64']['mock_packages'].remove('gcc45_0moz3')
 BRANCHES['comm-esr17']['platforms']['linux64']['mock_packages'].append('gcc45_0moz4')
+BRANCHES['comm-esr17']['platforms']['linux-debug']['mock_packages'].remove('gcc45_0moz3')
+BRANCHES['comm-esr17']['platforms']['linux-debug']['mock_packages'].append('gcc45_0moz4')
+BRANCHES['comm-esr17']['platforms']['linux64-debug']['mock_packages'].remove('gcc45_0moz3')
+BRANCHES['comm-esr17']['platforms']['linux64-debug']['mock_packages'].append('gcc45_0moz4')
 
 ######## comm-beta
 BRANCHES['comm-beta']['moz_repo_path'] = 'releases/mozilla-beta'
