ad21605248004e70ea5100194ca314ae6b214b2f
ffxbld <none@none>
2013-03-07 21:29:56 -0800

    Update release config for Thunderbird-17.0.4-build1

--- mozilla/release-thunderbird-comm-release.py
+++ mozilla/release-thunderbird-comm-release.py
@@ -21,17 +21,17 @@ releaseConfig['productName']         = 'thunderbird'
 releaseConfig['appName']             = 'mail'
 releaseConfig['mozilla_dir']         = 'mozilla'
 #  Current version info
-releaseConfig['version']             = '17.0.2'
-releaseConfig['appVersion']          = '17.0.2'
+releaseConfig['version']             = '17.0.4'
+releaseConfig['appVersion']          = '17.0.4'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 2
-releaseConfig['baseTag']             = 'THUNDERBIRD_17_0_2'
+releaseConfig['buildNumber']         = 1
+releaseConfig['baseTag']             = 'THUNDERBIRD_17_0_4'
 releaseConfig['partialUpdates']      = {
 
-    '17.0': {
-        'appVersion': '17.0',
-        'buildNumber': 2,
-        'baseTag': 'THUNDERBIRD_17_0',
+    '17.0.3': {
+        'appVersion': '17.0.3',
+        'buildNumber': 1,
+        'baseTag': 'THUNDERBIRD_17_0_3',
     },
 
 }
@@ -43,8 +43,8 @@ releaseConfig['sourceRepositories']  = {
     'comm': {
         'name': 'comm-release',
         'path': 'releases/comm-esr17',
-        'revision': 'b88ae0d86bec',
-        'relbranch': 'GECKO1701_2013010313_RELBRANCH',
+        'revision': '66f7060807f0',
+        'relbranch': None,
         'bumpFiles': {
             'mail/config/version.txt': {
                 'version': releaseConfig['appVersion'],
@@ -55,8 +55,8 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-release',
         'path': 'releases/mozilla-esr17',
-        'revision': '0cdeaa290942',
-        'relbranch': 'GECKO1701_2013010313_RELBRANCH',
+        'revision': '2c3766cfba66',
+        'relbranch': 'GECKO1703_2013021513_RELBRANCH',
         'bumpFiles': {
             'config/milestone.txt': {
                 'version': releaseConfig['milestone'],
@@ -110,6 +110,7 @@ releaseConfig['ausUser']             = 'tbirdbld'
 releaseConfig['ausSshKey']           = 'auspush'
 releaseConfig['releaseNotesUrl']     = 'http://live.mozillamessaging.com/thunderbird/releasenotes?locale=%locale%&platform=%platform%&version=%version%'
 releaseConfig['testOlderPartials']   = False
+releaseConfig['promptWaitTime']      = None
 releaseConfig['updateVerifyChunks']  = 4
 releaseConfig['verifyConfigs']       = {
     'linux':  'mozRelease-thunderbird-linux.cfg',
