de69b7ab3e4b3684f31e4069b5b58ba1dedda450
Justin Wood <Callek@gmail.com>
2013-03-08 20:16:59 -0500

    Backed out changeset e228096a8ceb for theory on fixing SM2.16.1
    --HG--
    branch : seamonkey-production

--- seamonkey/release_master.py
+++ seamonkey/release_master.py
@@ -357,6 +357,8 @@ for platform in releaseConfig['enUSPlatforms']:
             buildNumber=releaseConfig['buildNumber'],
             tree='release',
             clobberURL=branchConfig['base_clobber_url'],
+            tooltool_manifest_src= pf.get('tooltool_manifest_src', None),
+            tooltool_url_list= branchConfig.get('tooltool_url_list', []),
         )
 
         builders.append({
