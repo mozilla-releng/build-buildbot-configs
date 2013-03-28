01fe76f4bf37076d5a8c8814aed159d50af9a8a7
Chris Cooper <ccooper@deadsquid.com>
2013-03-28 12:31:14 -0400

    Bug 810736 - set URL to fuzzing bundle - r=armenzg

--- mozilla/staging_config.py
+++ mozilla/staging_config.py
@@ -129,6 +129,7 @@ PROJECTS = {
     'fuzzing': {
         'disable_tinderbox_mail': True,
         'scripts_repo': 'http://hg.mozilla.org/build/tools',
+        'fuzzing_bundle': 'http://pvtbuilds.pvt.build.mozilla.org/bundles/fuzzing.hg',
         'fuzzing_repo': 'ssh://stage-ffxbld@hg.mozilla.org/private/fuzzing',
         'fuzzing_remote_host': 'stage-ffxbld@pvtbuilds2.dmz.scl3.mozilla.com',
         # Path needs extra leading slash due to optparse expansion on Win32
