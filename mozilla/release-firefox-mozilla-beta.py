9a8c237f7441a5b3f1561272a681a417aeb636bb
Rail Aliiev <rail@mozilla.com>
2013-04-08 18:56:57 -0400

    backout 60a4bd4c5da0
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -20,23 +20,23 @@ releaseConfig['messagePrefix']       = '[release] '
 releaseConfig['productName']         = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '21.0b2'
+releaseConfig['version']             = '21.0b1'
 releaseConfig['appVersion']          = '21.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_21_0b2'
+releaseConfig['baseTag']             = 'FIREFOX_21_0b1'
 releaseConfig['partialUpdates']      = {
+
+    '20.0b6': {
+        'appVersion': '20.0',
+        'buildNumber': 1,
+        'baseTag': 'FIREFOX_20_0b6',
+    },
 
     '20.0b7': {
         'appVersion': '20.0',
         'buildNumber': 1,
         'baseTag': 'FIREFOX_20_0b7',
-    },
-
-    '21.0b1': {
-        'appVersion': '21.0',
-        'buildNumber': 1,
-        'baseTag': 'FIREFOX_21_0b1',
     },
 
 }
@@ -48,7 +48,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '88f47f3fe367',
+        'revision': '7712be144d91',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
