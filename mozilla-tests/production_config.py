f1e2f5ef5e59b0891d56b4c03e40da0b8a6280c2
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2013-05-03 09:31:15 -0400

    Bug 770578. Add Win7-ix Cedar. r=coop

--- mozilla-tests/production_config.py
+++ mozilla-tests/production_config.py
@@ -6,6 +6,7 @@ SLAVES = {
         if x not in [45, 58, 59]]), # bug 661377, bug 780515, bug 753357
     'win7': dict([("talos-r3-w7-%03i" % x, {}) for x in range(4,135) \
         if x not in [10, 17]]), # bug 861326 & bug 747734
+    'win7-ix': dict([("t-w7-ix-%03i" % x, {}) for x in range(1,101)]),
     'win8': dict([("t-w864-ix-%03i" % x, {}) for x in range(1,101)]),
     'snowleopard': dict([("talos-r4-snow-%03i" % x, {}) for x in range(1,85) \
         if x not in [46, 81, 83]]), # bug 824754, bug 729090
