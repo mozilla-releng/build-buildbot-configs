2399fc2118754a6ab3ec200edc4b340898849ee0
Justin Wood <Callek@gmail.com>
2013-05-20 02:23:47 -0700

    Bug 873989 - Switch 32-bits linux builds to x86-64 mock environments for Thunderbird as well. r=bhearsum

--- mozilla/thunderbird_config.py
+++ mozilla/thunderbird_config.py
@@ -140,22 +140,44 @@ PLATFORM_VARS = {
             'dep_signing_servers': 'dep-signing',
             'tooltool_manifest_src': 'mail/config/tooltool-manifests/linux32/releng.manifest',
             'use_mock': True,
-            'mock_target': 'mozilla-centos6-i386',
+            'mock_target': 'mozilla-centos6-x86_64',
             'mock_packages': \
                        ['autoconf213', 'python', 'zip', 'mozilla-python27-mercurial', 'git', 'ccache',
-                        'glibc-static', 'libstdc++-static', 'perl-Test-Simple', 'perl-Config-General',
-                        'gtk2-devel', 'libnotify-devel', 'yasm',
-                        'alsa-lib-devel', 'libcurl-devel',
-                        'wireless-tools-devel', 'libX11-devel',
-                        'libXt-devel', 'mesa-libGL-devel',
-                        'gnome-vfs2-devel', 'wget',
-                        'mpfr', # required for system compiler
-                        'xorg-x11-font*', # fonts required for PGO
-                        'imake', # required for makedepend!?!
-                        'gcc45_0moz3', 'yasm', 'ccache', # <-- from releng repo
+                        'glibc-static.i686', 'libstdc++-static.i686', 'perl-Test-Simple', 'perl-Config-General',
+                        'gtk2-devel.i686', 'libnotify-devel.i686', 'yasm',
+                        'alsa-lib-devel.i686', 'libcurl-devel.i686',
+                        'wireless-tools-devel.i686', 'libX11-devel.i686',
+                        'libXt-devel.i686', 'mesa-libGL-devel.i686',
+                        'gnome-vfs2-devel.i686', 'GConf2-devel.i686', 'wget',
+                        'mpfr',  # required for system compiler
+                        'xorg-x11-font*',  # fonts required for PGO
+                        'imake',  # required for makedepend!?!
+                        'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'yasm', 'ccache',  # <-- from releng repo
                         'valgrind',
-                        'pulseaudio-libs-devel',
-                        'freetype-2.3.11-6.el6_2.9', 'freetype-devel-2.3.11-6.el6_2.9',
+                        'pulseaudio-libs-devel.i686',
+                        'gstreamer-devel.i686', 'gstreamer-plugins-base-devel.i686',
+                        # Packages already installed in the mock environment, as x86_64
+                        # packages.
+                        'glibc-devel.i686', 'libgcc.i686', 'libstdc++-devel.i686',
+                        # yum likes to install .x86_64 -devel packages that satisfy .i686
+                        # -devel packages dependencies. So manually install the dependencies
+                        # of the above packages.
+                        'ORBit2-devel.i686', 'atk-devel.i686', 'cairo-devel.i686',
+                        'check-devel.i686', 'dbus-devel.i686', 'dbus-glib-devel.i686',
+                        'fontconfig-devel.i686', 'glib2-devel.i686',
+                        'hal-devel.i686', 'libICE-devel.i686', 'libIDL-devel.i686',
+                        'libSM-devel.i686', 'libXau-devel.i686', 'libXcomposite-devel.i686',
+                        'libXcursor-devel.i686', 'libXdamage-devel.i686', 'libXdmcp-devel.i686',
+                        'libXext-devel.i686', 'libXfixes-devel.i686', 'libXft-devel.i686',
+                        'libXi-devel.i686', 'libXinerama-devel.i686', 'libXrandr-devel.i686',
+                        'libXrender-devel.i686', 'libXxf86vm-devel.i686', 'libdrm-devel.i686',
+                        'libidn-devel.i686', 'libpng-devel.i686', 'libxcb-devel.i686',
+                        'libxml2-devel.i686', 'pango-devel.i686', 'perl-devel.i686',
+                        'pixman-devel.i686', 'zlib-devel.i686',
+                        # Freetype packages need to be installed be version, because a newer
+                        # version is available, but we don't want it for Firefox builds.
+                        'freetype-2.3.11-6.el6_1.8.i686', 'freetype-devel-2.3.11-6.el6_1.8.i686',
+                        'freetype-2.3.11-6.el6_1.8.x86_64',
                         ],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
@@ -220,14 +242,16 @@ PLATFORM_VARS = {
                         'alsa-lib-devel', 'libcurl-devel',
                         'wireless-tools-devel', 'libX11-devel',
                         'libXt-devel', 'mesa-libGL-devel',
-                        'gnome-vfs2-devel', 'wget',
+                        'gnome-vfs2-devel', 'GConf2-devel', 'wget',
                         'mpfr', # required for system compiler
                         'xorg-x11-font*', # fonts required for PGO
                         'imake', # required for makedepend!?!
-                        'gcc45_0moz3', 'yasm', 'ccache', # <-- from releng repo
+                        'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'yasm', 'ccache', # <-- from releng repo
                         'valgrind',
                         'pulseaudio-libs-devel',
-                        'freetype-2.3.11-6.el6_1.8', 'freetype-devel-2.3.11-6.el6_1.8',
+                        'gstreamer-devel', 'gstreamer-plugins-base-devel',
+                        'freetype-2.3.11-6.el6_1.8.x86_64',
+                        'freetype-devel-2.3.11-6.el6_1.8.x86_64',
                         ],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
@@ -412,21 +436,44 @@ PLATFORM_VARS = {
             'talos_masters': None,
             'tooltool_manifest_src': 'mail/config/tooltool-manifests/linux32/releng.manifest',
             'use_mock': True,
-            'mock_target': 'mozilla-centos6-i386',
+            'mock_target': 'mozilla-centos6-x86_64',
             'mock_packages': \
                        ['autoconf213', 'python', 'zip', 'mozilla-python27-mercurial', 'git', 'ccache',
-                        'glibc-static', 'libstdc++-static', 'perl-Test-Simple', 'perl-Config-General',
-                        'gtk2-devel', 'libnotify-devel', 'yasm',
-                        'alsa-lib-devel', 'libcurl-devel',
-                        'wireless-tools-devel', 'libX11-devel',
-                        'libXt-devel', 'mesa-libGL-devel',
-                        'gnome-vfs2-devel', 'wget',
-                        'mpfr', # required for system compiler
-                        'xorg-x11-font*', # fonts required for PGO
-                        'imake', # required for makedepend!?!
-                        'gcc45_0moz3', 'yasm', 'ccache', # <-- from releng repo
-                        'pulseaudio-libs-devel',
-                        'freetype-2.3.11-6.el6_2.9', 'freetype-devel-2.3.11-6.el6_2.9',
+                        'glibc-static.i686', 'libstdc++-static.i686',
+                        'perl-Test-Simple', 'perl-Config-General',
+                        'gtk2-devel.i686', 'libnotify-devel.i686', 'yasm',
+                        'alsa-lib-devel.i686', 'libcurl-devel.i686',
+                        'wireless-tools-devel.i686', 'libX11-devel.i686',
+                        'libXt-devel.i686', 'mesa-libGL-devel.i686',
+                        'gnome-vfs2-devel.i686', 'GConf2-devel.i686', 'wget',
+                        'mpfr',  # required for system compiler
+                        'xorg-x11-font*',  # fonts required for PGO
+                        'imake',  # required for makedepend!?!
+                        'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'yasm', 'ccache',  # <-- from releng repj
+                        'valgrind',
+                        'pulseaudio-libs-devel.i686',
+                        'gstreamer-devel.i686', 'gstreamer-plugins-base-devel.i686',
+                        # Packages already installed in the mock environment,
+                        # as x86_64 packages.
+                        'glibc-devel.i686', 'libgcc.i686', 'libstdc++-devel.i686',
+                        # yum likes to install .x86_64 -devel packages that satisfy .i686
+                        # -devel packages dependencies. So manually install the
+                        # dependencies of the above packages.
+                        'ORBit2-devel.i686', 'atk-devel.i686', 'cairo-devel.i686',
+                        'check-devel.i686', 'dbus-devel.i686', 'dbus-glib-devel.i686',
+                        'fontconfig-devel.i686', 'glib2-devel.i686',
+                        'hal-devel.i686', 'libICE-devel.i686', 'libIDL-devel.i686',
+                        'libSM-devel.i686', 'libXau-devel.i686', 'libXcomposite-devel.i686',
+                        'libXcursor-devel.i686', 'libXdamage-devel.i686',
+                        'libXdmcp-devel.i686', 'libXext-devel.i686',
+                        'libXfixes-devel.i686', 'libXft-devel.i686',
+                        'libXi-devel.i686', 'libXinerama-devel.i686', 'libXrandr-devel.i686',
+                        'libXrender-devel.i686', 'libXxf86vm-devel.i686', 'libdrm-devel.i686',
+                        'libidn-devel.i686', 'libpng-devel.i686', 'libxcb-devel.i686',
+                        'libxml2-devel.i686', 'pango-devel.i686', 'perl-devel.i686',
+                        'pixman-devel.i686', 'zlib-devel.i686',
+                        'freetype-2.3.11-6.el6_1.8.i686', 'freetype-devel-2.3.11-6.el6_1.8.i686',
+                        'freetype-2.3.11-6.el6_1.8.x86_64',
                         ],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
@@ -479,13 +526,15 @@ PLATFORM_VARS = {
                         'alsa-lib-devel', 'libcurl-devel',
                         'wireless-tools-devel', 'libX11-devel',
                         'libXt-devel', 'mesa-libGL-devel',
-                        'gnome-vfs2-devel', 'wget',
+                        'gnome-vfs2-devel', 'GConf2-devel', 'wget',
                         'mpfr', # required for system compiler
                         'xorg-x11-font*', # fonts required for PGO
                         'imake', # required for makedepend!?!
-                        'gcc45_0moz3', 'yasm', 'ccache', # <-- from releng repo
+                        'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'yasm', 'ccache', # <-- from releng repo
                         'pulseaudio-libs-devel',
-                        'freetype-2.3.11-6.el6_1.8', 'freetype-devel-2.3.11-6.el6_1.8',
+                        'freetype-2.3.11-6.el6_1.8.x86_64',
+                        'freetype-devel-2.3.11-6.el6_1.8.x86_64',
+                        'gstreamer-devel', 'gstreamer-plugins-base-devel',
                         ],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
@@ -1006,6 +1055,32 @@ for b in ['comm-release', 'comm-esr17']:
             BRANCHES[b]['platforms'][p]['mock_packages'] = \
                 [x for x in BRANCHES[b]['platforms'][p]['mock_packages'] if x != 'pulseaudio-libs-devel']
 
+# MERGE DAY building 32-bit linux in a x86_64 env rides the trains
+# MERGE DAY remove branches from this list when gecko 24 merges into them.
+for branch in ("comm-aurora", "comm-beta", "comm-release", "comm-esr17"):
+    for platform in ['linux', 'linux-debug']:
+        BRANCHES[branch]['platforms'][platform]['mock_target'] = \
+            'mozilla-centos6-i386'
+        BRANCHES[branch]['platforms'][platform]['mock_packages'] = \
+            ['autoconf213', 'python', 'zip', 'mozilla-python27-mercurial',
+             'git', 'ccache', 'glibc-static', 'libstdc++-static',
+             'perl-Test-Simple', 'perl-Config-General',
+             'gtk2-devel', 'libnotify-devel', 'yasm',
+             'alsa-lib-devel', 'libcurl-devel',
+             'wireless-tools-devel', 'libX11-devel',
+             'libXt-devel', 'mesa-libGL-devel',
+             'gnome-vfs2-devel', 'GConf2-devel', 'wget',
+             'mpfr', # required for system compiler
+             'xorg-x11-font*', # fonts required for PGO
+             'imake', # required for makedepend!?!
+             'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'yasm', 'ccache', # <-- from releng repo
+             'pulseaudio-libs-devel',
+             'freetype-2.3.11-6.el6_2.9',
+             'freetype-devel-2.3.11-6.el6_2.9',
+            ]
+        if not platform.endswith("-debug"):
+            BRANCHES[branch]["platforms"][platform]["mock_packages"] += \
+                ["valgrind"]
 
 if __name__ == "__main__":
     import sys
