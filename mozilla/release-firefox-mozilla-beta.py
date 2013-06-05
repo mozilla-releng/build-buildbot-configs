c0ade83fd76e776613f09b9abdd5f41d7936ae11
ffxbld <none@none>
2013-06-05 06:16:30 -0700

    Update release config for Firefox-22.0b4-build1
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -20,11 +20,11 @@ releaseConfig['messagePrefix']       = '[release] '
 releaseConfig['productName']         = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '22.0b3'
+releaseConfig['version']             = '22.0b4'
 releaseConfig['appVersion']          = '22.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_22_0b3'
+releaseConfig['baseTag']             = 'FIREFOX_22_0b4'
 releaseConfig['partialUpdates']      = {
 
     '22.0b2': {
@@ -33,10 +33,10 @@ releaseConfig['partialUpdates']      = {
         'baseTag': 'FIREFOX_22_0b2',
     },
 
-    '22.0b1': {
+    '22.0b3': {
         'appVersion': '22.0',
         'buildNumber': 1,
-        'baseTag': 'FIREFOX_22_0b1',
+        'baseTag': 'FIREFOX_22_0b3',
     },
 
 }
@@ -48,7 +48,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '8ca75debb97a',
+        'revision': '80d4a7fac73e',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
