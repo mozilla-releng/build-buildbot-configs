a9611015703541bb05f7eb5a56c3616d168c99b2
Ben Hearsum <bhearsum@mozilla.com>
2013-06-24 11:12:22 -0400

    Land merge day bugs (865814, 877336, and 883866) to default.
    Done in one changeset because my Mercurial branch strategies suck.

--- mozilla/config.py
+++ mozilla/config.py
@@ -1758,6 +1758,8 @@ BRANCHES['mozilla-esr17']['aus2_base_upload_dir_l10n'] = '/opt/aus2/incoming/2/F
 BRANCHES['mozilla-esr17']['enable_blocklist_update'] = True
 BRANCHES['mozilla-esr17']['enable_valgrind'] = False
 BRANCHES['mozilla-esr17']['enabled_products'] = ['firefox']
+# mock disabled block start
+# linux platforms
 BRANCHES['mozilla-esr17']['platforms']['linux']['use_mock'] = False
 BRANCHES['mozilla-esr17']['platforms']['linux64']['use_mock'] = False
 BRANCHES['mozilla-esr17']['platforms']['linux-debug']['use_mock'] = False
@@ -1774,25 +1776,9 @@ del BRANCHES['mozilla-esr17']['platforms']['linux']['env']['PATH']
 del BRANCHES['mozilla-esr17']['platforms']['linux64']['env']['PATH']
 del BRANCHES['mozilla-esr17']['platforms']['linux-debug']['env']['PATH']
 del BRANCHES['mozilla-esr17']['platforms']['linux64-debug']['env']['PATH']
+# mock disabled block stop
 BRANCHES['mozilla-esr17']['platforms']['win32']['l10n_slaves'] = SLAVES['win32']
 BRANCHES['mozilla-esr17']["run_make_alive_tests"] = False
-BRANCHES['mozilla-esr17']['platforms']['linux']['env']['LD_LIBRARY_PATH'] = '/tools/gcc-4.3.3/installed/lib'
-BRANCHES['mozilla-esr17']['platforms']['linux']['unittest-env'] = {
-    'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib',
-}
-BRANCHES['mozilla-esr17']['platforms']['linux64']['env']['LD_LIBRARY_PATH'] = '/tools/gcc-4.3.3/installed/lib64'
-BRANCHES['mozilla-esr17']['platforms']['linux64']['unittest-env'] = {
-    'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib64',
-}
-BRANCHES['mozilla-esr17']['platforms']['linux-debug']['env']['LD_LIBRARY_PATH'] = '/tools/gcc-4.3.3/installed/lib:%s/dist/bin' % OBJDIR
-BRANCHES['mozilla-esr17']['platforms']['linux-debug']['unittest-env'] = {
-    'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib',
-}
-BRANCHES['mozilla-esr17']['platforms']['linux64-debug']['env']['LD_LIBRARY_PATH'] = '/tools/gcc-4.3.3/installed/lib64:%s/dist/bin' % OBJDIR
-BRANCHES['mozilla-esr17']['platforms']['linux64-debug']['unittest-env'] = {
-    'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib64',
-}
-
 
 ######## mozilla-b2g18
 BRANCHES['mozilla-b2g18']['repo_path'] = 'releases/mozilla-b2g18'
@@ -2018,6 +2004,31 @@ for branch in ACTIVE_PROJECT_BRANCHES:
                                                                              BRANCHES[branch]['platforms'][platform]['dep_signing_servers'])
     BRANCHES[branch]['enable_valgrind'] = False
 
+# Bug 578880, remove the following block after gcc-4.5 switch
+branches = BRANCHES.keys()
+branches.extend(ACTIVE_PROJECT_BRANCHES)
+for branch in branches:
+    if 'linux' in BRANCHES[branch]['platforms']:
+        BRANCHES[branch]['platforms']['linux']['env']['LD_LIBRARY_PATH'] = '/tools/gcc-4.3.3/installed/lib'
+        BRANCHES[branch]['platforms']['linux']['unittest-env'] = {
+            'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib',
+        }
+    if 'linux64' in BRANCHES[branch]['platforms']:
+        BRANCHES[branch]['platforms']['linux64']['env']['LD_LIBRARY_PATH'] = '/tools/gcc-4.3.3/installed/lib64'
+        BRANCHES[branch]['platforms']['linux64']['unittest-env'] = {
+            'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib64',
+        }
+    if 'linux-debug' in BRANCHES[branch]['platforms']:
+        BRANCHES[branch]['platforms']['linux-debug']['env']['LD_LIBRARY_PATH'] = '/tools/gcc-4.3.3/installed/lib:%s/dist/bin' % OBJDIR
+        BRANCHES[branch]['platforms']['linux-debug']['unittest-env'] = {
+            'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib',
+        }
+    if 'linux64-debug' in BRANCHES[branch]['platforms']:
+        BRANCHES[branch]['platforms']['linux64-debug']['env']['LD_LIBRARY_PATH'] = '/tools/gcc-4.3.3/installed/lib64:%s/dist/bin' % OBJDIR
+        BRANCHES[branch]['platforms']['linux64-debug']['unittest-env'] = {
+            'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib64',
+        }
+
 # MERGE DAY
 # When Firefox 18 merges into these branches, they can be removed from the list
 for b in ('mozilla-esr17',):
@@ -2046,7 +2057,7 @@ for b in BRANCHES:
 
 # MERGE DAY building 32-bit linux in a x86_64 env rides the trains
 # MERGE DAY remove branches from this list when gecko 24 merges into them.
-for branch in ("mozilla-aurora", "mozilla-beta", "mozilla-release",
+for branch in ("mozilla-beta", "mozilla-release",
                "mozilla-b2g18", "mozilla-b2g18_v1_0_1",
                "mozilla-b2g18_v1_1_0_hd", "mozilla-esr17"):
     for platform in ['linux', 'linux-debug']:
@@ -2075,7 +2086,7 @@ for branch in ("mozilla-aurora", "mozilla-beta", "mozilla-release",
 
 # MERGE DAY building android in a x86_64 env rides the trains
 # MERGE DAY remove branches from this list when gecko 24 merges into them.
-for b in ("mozilla-aurora", "mozilla-beta", "mozilla-release",
+for b in ("mozilla-beta", "mozilla-release",
           "mozilla-b2g18", "mozilla-b2g18_v1_0_1", "mozilla-b2g18_v1_1_0_hd",
           "mozilla-esr17"):
     for plat in ['android', 'android-armv6', 'android-noion',
