526ad8e78118688802976ccb04e76ca00c4aba59
Edmund Wong <ewong@pw-wspx.org>
2013-07-22 11:57:48 +0800

    Bug 894212 - Update configs for SeaMonkey 2.20b2. r=Callek
    --HG--
    branch : seamonkey-production

--- seamonkey/release-comm-beta.py
+++ seamonkey/release-comm-beta.py
@@ -8,16 +8,16 @@ releaseConfig['productName']                = 'seamonkey'
 releaseConfig['brandName']                  = 'SeaMonkey'
 releaseConfig['appName']                    = 'suite'
 #  Current version info
-releaseConfig['version']                    = '2.20b1'
+releaseConfig['version']                    = '2.20b2'
 releaseConfig['appVersion']                 = '2.20'
 releaseConfig['milestone']                  = '23.0'
 releaseConfig['buildNumber']                = 1
-releaseConfig['baseTag']                    = 'SEAMONKEY_2_20b1'
+releaseConfig['baseTag']                    = 'SEAMONKEY_2_20b2'
 #  Old version info
-releaseConfig['oldVersion']                 = '2.19b2'
-releaseConfig['oldAppVersion']              = '2.19'
+releaseConfig['oldVersion']                 = '2.20b1'
+releaseConfig['oldAppVersion']              = '2.20'
 releaseConfig['oldBuildNumber']             = 1
-releaseConfig['oldBaseTag']                 = 'SEAMONKEY_2_19b2'
+releaseConfig['oldBaseTag']                 = 'SEAMONKEY_2_20b1'
 releaseConfig['oldRepoPath']                = 'releases/comm-beta'
 #  Next (nightly) version info
 #     not yet available
@@ -26,14 +26,14 @@ releaseConfig['skip_tag']                   = False
 releaseConfig['relbranchPrefix']            = 'SEA_COMM'
 releaseConfig['sourceRepoName']             = 'comm-beta' # buildbot branch name
 releaseConfig['sourceRepoPath']             = 'releases/comm-beta'
-releaseConfig['sourceRepoRevision']         = 'e68ad6208404'
+releaseConfig['sourceRepoRevision']         = '4b022ddb9be4'
 releaseConfig['relbranchOverride']          = ''
 #releaseConfig['productVersionFile']        = 'suite/config/version-20.txt'
 releaseConfig['productVersionFile']         = ''
 #   Mozilla
 releaseConfig['mozillaRepoPath']            = 'releases/mozilla-beta'
-releaseConfig['mozillaRepoRevision']        = 'FIREFOX_23_0b3_BUILD1'
-releaseConfig['mozillaRelbranchOverride']   = 'GECKO230b3_2013070320_RELBRANCH' # put Gecko relbranch here that we base upon
+releaseConfig['mozillaRepoRevision']        = 'FIREFOX_23_0b7_BUILD1'
+releaseConfig['mozillaRelbranchOverride']   = 'GECKO230b7_2013071818_RELBRANCH' # put Gecko relbranch here that we base upon
 #   Inspector
 releaseConfig['inspectorRepoPath']          = 'dom-inspector' # leave empty if inspector is not to be tagged
 releaseConfig['inspectorRepoRevision']      = '32de4c4a4ce7'
