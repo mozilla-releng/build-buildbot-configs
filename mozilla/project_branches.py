27ea186adca95a8e20706c6b3fa12012f0759ebe
Nick Thomas <nthomas@mozilla.com>
2013-08-07 16:46:23 +1200

    Bug 902281, force a complete for ux too, r=aki

--- mozilla/project_branches.py
+++ mozilla/project_branches.py
@@ -173,7 +173,7 @@ PROJECT_BRANCHES = {
         'mozconfig_dir': 'ux',
         'enable_nightly': True,
         'create_snippet': True,
-        'create_partial': True,
+        'create_partial': False,
         'pgo_strategy': 'periodic',
         'platforms': {
             'macosx64-debug': {
