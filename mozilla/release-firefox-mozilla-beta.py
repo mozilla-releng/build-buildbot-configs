80954cd4a33f41cd9ed3e13252aff42fec89b888
ffxbld <none@none>
2013-08-29 13:07:32 -0700

    Update release config for Firefox-24.0b7-build1
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -20,23 +20,23 @@ releaseConfig['messagePrefix']       = '[release] '
 releaseConfig['productName']         = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '24.0b6'
+releaseConfig['version']             = '24.0b7'
 releaseConfig['appVersion']          = '24.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_24_0b6'
+releaseConfig['baseTag']             = 'FIREFOX_24_0b7'
 releaseConfig['partialUpdates']      = {
-
-    '24.0b4': {
-        'appVersion': '24.0',
-        'buildNumber': 1,
-        'baseTag': 'FIREFOX_24_0b4',
-    },
 
     '24.0b5': {
         'appVersion': '24.0',
         'buildNumber': 1,
         'baseTag': 'FIREFOX_24_0b5',
+    },
+
+    '24.0b6': {
+        'appVersion': '24.0',
+        'buildNumber': 1,
+        'baseTag': 'FIREFOX_24_0b6',
     },
 
 }
@@ -48,7 +48,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '998fae3a6eba',
+        'revision': '5bdf21972312',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
@@ -109,7 +109,7 @@ releaseConfig['ausUser']             = 'ffxbld'
 releaseConfig['ausSshKey']           = 'auspush'
 releaseConfig['releaseNotesUrl']     = None
 releaseConfig['testOlderPartials']   = False
-releaseConfig['promptWaitTime']      = None
+releaseConfig['promptWaitTime']      = 86400
 releaseConfig['updateVerifyChunks']  = 4
 releaseConfig['verifyConfigs']       = {
     'linux':  'mozBeta-firefox-linux.cfg',
