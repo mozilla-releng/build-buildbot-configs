682b1e0fca9035939065ecd0ad623d86eb11fe8f
Chris Cooper <ccooper@deadsquid.com>
2013-10-29 14:34:01 -0400

    Bug 919841 - Add HP builders to try pool - r=armenzg

--- mozilla/production_config.py
+++ mozilla/production_config.py
@@ -31,7 +31,8 @@ TRY_MAC64      = []
 TRY_WIN32_IXS  = []
 TRY_WIN64_IXS  = ['w64-ix-slave%02i' % x for x in range(30,75) if x not in [42,43]]
 TRY_WIN64_REV2 = ['w64-ix-slave%03i' % x for x in [3,110]]
-TRY_MOCK_DL120G7 = ['bld-centos6-hp-%03d' % x for x in range(24,43)]
+TRY_MOCK_DL120G7 = ['bld-centos6-hp-%03d' % x for x in range(24,43)] + \
+                   ['b-linux64-hp-%03d' % x for x in range(1, 7)]
 TRY_MOCK_IX      = ['bld-linux64-ix-%03d' % x for x in range(49, 74)]
 TRY_LION         = ['bld-lion-r5-%03d' % x for x in range(16,41)]
 if set(TRY_WIN64_REV2).intersection(set(TRY_WIN64_IXS)):
