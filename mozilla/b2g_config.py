33295c5a6fe4c06eb84b0a43722857d903d424be
John Hopkins <jhopkins@mozilla.com>
2013-10-31 17:19:42 -0400

    Bug 933411 - designate bm66 as rev2 master, enable mixed pool on m-i and b2g-i. r=rail

--- mozilla/b2g_config.py
+++ mozilla/b2g_config.py
@@ -1135,8 +1135,8 @@ BRANCHES['try']['platforms']['emulator-jb-debug']['mozharness_config']['extra_ar
 # MERGE DAY
 # Migrate branches to win64-rev2 platform (bug 918414)
 disabled_branches = set([x for x in BRANCHES.keys() if x not in PROJECT_BRANCHES.keys()] + ['b2g-inbound', 'mozilla-inbound'])
-mixed_masters = ['buildbot-master56']
-mixed_branches = ['try']
+mixed_masters = ['buildbot-master56','buildbot-master66']
+mixed_branches = ['try','mozilla-inbound','b2g-inbound']
 win64_mix_size = 2
 for b in mixed_branches:
     if b not in disabled_branches:
