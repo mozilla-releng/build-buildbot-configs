dd4c56bb780833ac84c3438db9edd116698ced52
ffxbld <none@none>
2013-11-18 20:34:12 -0800

    Update release config for Firefox-26.0b6-build1

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -20,23 +20,23 @@ releaseConfig['messagePrefix']       = '[release] '
 releaseConfig['productName']         = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '26.0b5'
+releaseConfig['version']             = '26.0b6'
 releaseConfig['appVersion']          = '26.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_26_0b5'
+releaseConfig['baseTag']             = 'FIREFOX_26_0b6'
 releaseConfig['partialUpdates']      = {
-
-    '26.0b3': {
-        'appVersion': '26.0',
-        'buildNumber': 1,
-        'baseTag': 'FIREFOX_26_0b3',
-    },
 
     '26.0b4': {
         'appVersion': '26.0',
         'buildNumber': 1,
         'baseTag': 'FIREFOX_26_0b4',
+    },
+
+    '26.0b5': {
+        'appVersion': '26.0',
+        'buildNumber': 1,
+        'baseTag': 'FIREFOX_26_0b5',
     },
 
 }
@@ -48,7 +48,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '9ba6a44a6dfa',
+        'revision': '266f8698dbb8',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
