4df42528933f99a9ef2840fd2c86e59f69106c3f
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2013-11-28 12:10:37 -0500

    Bug 942299 - Adjust list of snow leopard machines to match reality. r=bhearsum

--- mozilla-tests/production_config.py
+++ mozilla-tests/production_config.py
@@ -10,10 +10,8 @@ SLAVES = {
         if x not in [10, 17]]), # bug 861326 & bug 747734
     'win7-ix': dict([("t-w732-ix-%03i" % x, {}) for x in range(1,131)]),
     'win8': dict([("t-w864-ix-%03i" % x, {}) for x in range(1,131)]),
-    'snowleopard': dict([("talos-r4-snow-%03i" % x, {}) for x in range(1,174) \
+    'snowleopard':dict([("talos-r4-snow-%03i" % x, {}) for x in range(1,171) \
         if x not in [81, 84]]), # bug 729090, bug 795558
-    'mountainlion': dict([("talos-mtnlion-r5-%03i" % x, {}) for x in range(1,90) \
-        if x not in [87]]), # bug 786994
     'tegra_android': dict([('tegra-%03i' % x, {'http_port': '30%03i' % x, 'ssl_port': '31%03i' % x}) \
         for x in range(31,371) \
         if x not in range(122,129) + [30,33,34,43,44,49,65,69,77,131,137,143,147,\
