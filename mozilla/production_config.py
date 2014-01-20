28170507aa14e641e8f618e16c3055fc4171a721
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2014-01-20 13:43:51 -0500

    Bug 933768 - Add b-2008-ix machines to the pool. r=jhopkins

--- mozilla/production_config.py
+++ mozilla/production_config.py
@@ -5,7 +5,8 @@ LINUX64_IXS    = ['linux64-ix-slave%02i' % x for x in range(3, 7)]
 WIN32_IXS      = ['mw32-ix-slave%02i' % x for x in range(2,13)]
 WIN64_IXS      = ['w64-ix-slave%03i' % x for x in range(144,158)]
 WIN64_REV2     = ['w64-ix-slave%02i' % x for x in range(6,30) + range(42,44) + range(75,100) if x not in [22,80,85,86]] + \
-                 ['w64-ix-slave%03i' % x for x in range(100,110) + range(111,144)]
+                 ['w64-ix-slave%03i' % x for x in range(100,110) + range(111,144)] + \
+                 ['b-2008-ix-%04i' % x for x in range(1,18)]
 MOCK_DL120G7   = ['bld-centos6-hp-%03d' % x for x in range(6,24)] # 5 staging, 17 prod, 17 try
 LINUX64_EC2    = ['bld-linux64-ec2-%03d' % x for x in range(1, 900)]
 MOCK_IX        = ['bld-linux64-ix-%03d' % x for x in range(27, 38)]
