1e4093c644d990aec8cee9a2f0de4e805e7f5ef2
Hal Wine <hwine@mozilla.com>
2014-01-23 17:56:08 -0800

    bug 963375 - backout FFx/fennec 27.0b9 build 1 updates; r=bustage

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -20,11 +20,11 @@ releaseConfig['productName']         = 'fennec'
 releaseConfig['appName']             = 'mobile'
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '27.0b9'
+releaseConfig['version']             = '27.0b8'
 releaseConfig['appVersion']          = '27.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_27_0b9'
+releaseConfig['baseTag']             = 'FENNEC_27_0b8'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -33,7 +33,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '4b60f871d7f2',
+        'revision': '90107551b8f5',
         'relbranch': None,
         'bumpFiles': {
             'mobile/android/confvars.sh': {
