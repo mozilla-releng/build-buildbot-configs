0ccb42e55cc7d6e12b1a743c01cb9ac5b53ca89e
ffxbld <none@none>
2014-02-05 15:36:34 -0800

    Update release config for Firefox-28.0b1-build2
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -25,7 +25,7 @@ releaseConfig['appName']             = 'browser'
 releaseConfig['version']             = '28.0b1'
 releaseConfig['appVersion']          = '28.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 1
+releaseConfig['buildNumber']         = 2
 releaseConfig['baseTag']             = 'FIREFOX_28_0b1'
 releaseConfig['partialUpdates']      = {
 
@@ -50,7 +50,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '60c04d0987f1',
+        'revision': 'cd1bfc373cfb',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
