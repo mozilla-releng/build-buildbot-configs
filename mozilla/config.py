f7c475aeff647fda759011b456c6143a20343f5c
Justin Wood <Callek@gmail.com>
2014-02-12 15:55:25 -0500

    Bug 971841 - install ant on try builders. r=bhearsum

--- mozilla/config.py
+++ mozilla/config.py
@@ -1010,7 +1010,7 @@ PLATFORM_VARS = {
                               'glibc-static', 'openssh-clients', 'mpfr',
                               "gcc472_0moz1", "gcc473_0moz1", 'wget', 'glibc.i686',
                               'libstdc++.i686', 'zlib.i686',
-                              'freetype-2.3.11-6.el6_1.8.x86_64'],
+                              'freetype-2.3.11-6.el6_1.8.x86_64', 'ant'],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
                 ('/home/cltbld/.hgrc', '/builds/.hgrc'),
@@ -1076,7 +1076,7 @@ PLATFORM_VARS = {
                               'glibc-static', 'openssh-clients', 'mpfr', 'bc',
                               "gcc472_0moz1", "gcc473_0moz1", 'wget', 'glibc.i686',
                               'libstdc++.i686', 'zlib.i686',
-                              'freetype-2.3.11-6.el6_1.8.x86_64'],
+                              'freetype-2.3.11-6.el6_1.8.x86_64', 'ant'],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
                 ('/home/cltbld/.hgrc', '/builds/.hgrc'),
@@ -1140,7 +1140,7 @@ PLATFORM_VARS = {
                               'java-1.6.0-openjdk-devel', 'zlib-devel',
                               'glibc-static', 'openssh-clients', 'mpfr', 'bc',
                               "gcc472_0moz1", "gcc473_0moz1", 'glibc.i686', 'libstdc++.i686',
-                              'zlib.i686', 'freetype-2.3.11-6.el6_1.8.x86_64'],
+                              'zlib.i686', 'freetype-2.3.11-6.el6_1.8.x86_64', 'ant'],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
                 ('/home/cltbld/.hgrc', '/builds/.hgrc'),
@@ -1209,7 +1209,7 @@ PLATFORM_VARS = {
                               'java-1.6.0-openjdk-devel', 'zlib-devel',
                               'glibc-static', 'openssh-clients', 'mpfr',
                               'wget', 'glibc.i686', 'libstdc++.i686',
-                              'zlib.i686', 'freetype-2.3.11-6.el6_1.8.x86_64'],
+                              'zlib.i686', 'freetype-2.3.11-6.el6_1.8.x86_64', 'ant'],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
                 ('/home/cltbld/.hgrc', '/builds/.hgrc'),
@@ -1276,7 +1276,7 @@ PLATFORM_VARS = {
                               'java-1.6.0-openjdk-devel', 'zlib-devel',
                               'glibc-static', 'openssh-clients', 'mpfr',
                               'wget', 'glibc.i686', 'libstdc++.i686',
-                              'zlib.i686', 'freetype-2.3.11-6.el6_1.8.x86_64'],
+                              'zlib.i686', 'freetype-2.3.11-6.el6_1.8.x86_64', 'ant'],
             'mock_copyin_files': [
                 ('/home/cltbld/.ssh', '/home/mock_mozilla/.ssh'),
                 ('/home/cltbld/.hgrc', '/builds/.hgrc'),
@@ -2243,6 +2243,19 @@ for name, branch in items_before(BRANCHES, 'gecko_version', 24):
                 [x for x in pc['mock_packages'] if x not in (
                     'gstreamer-devel', 'gstreamer-plugins-base-devel',
                     'gstreamer-devel.i686', 'gstreamer-plugins-base-devel.i686',
+                )]
+
+# ant test on try
+## ant rides the trains (Bug 971841)
+# for name, branch in items_before(BRANCHES, 'gecko_version', 30):
+for name, branch in BRANCHES.items():
+    if "try" in name:
+        continue # Remove this condition when we switch to riding trains
+    for plat, pc in branch['platforms'].items():
+        if 'mock_packages' in pc and "android" in plat:
+            pc['mock_packages'] = \
+                [x for x in pc['mock_packages'] if x not in (
+                    'ant',
                 )]
 
 for name, branch in items_before(BRANCHES, 'gecko_version', 22):
