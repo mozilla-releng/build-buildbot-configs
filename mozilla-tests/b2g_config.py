9f7222d7ee3de0a923cfba069902e2e747ed9717
Andrew Halberstadt <ahalberstadt@mozilla.com>
2014-02-24 14:57:23 -0500

    Bug 973063 - Use proper manifest path in emulator-jb mochitests, r=armenzg

--- mozilla-tests/b2g_config.py
+++ mozilla-tests/b2g_config.py
@@ -1639,7 +1639,7 @@ PLATFORM_UNITTEST_VARS = {
                         '--cfg', 'b2g/emulator_automation_config.py',
                         '--test-suite', 'mochitest',
                         '--this-chunk', '1', '--total-chunks', '1',
-                        '--test-manifest', 'tests/mochitest/emulator-jb.ini',
+                        '--test-manifest', 'tests/mochitest/manifests/emulator-jb.ini',
                         '--no-update',
                     ],
                 },
