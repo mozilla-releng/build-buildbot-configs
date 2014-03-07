1d29078f696977deeab41f6094f2c9eb3d1cca59
Ben Hearsum <bhearsum@mozilla.com>
2014-03-07 11:56:52 -0500

    bug 980976: buildbot-master82 doesn't have rev2 machines enabled for a bunch of branches. r=rail

--- mozilla/config.py
+++ mozilla/config.py
@@ -2180,7 +2180,8 @@ for b in BRANCHES.keys():
 
 # Migrate branches to win64-rev2 platform (bug 918414)
 disabled_branches = set([x for x in BRANCHES.keys() if x not in PROJECT_BRANCHES.keys() + ['try', 'mozilla-central', 'mozilla-aurora', 'mozilla-beta', 'mozilla-release']] + ['b2g-inbound', 'mozilla-inbound'])
-win64_rev1_masters = ['buildbot-master82']
+# Disabled for bug 980976
+win64_rev1_masters = []#['buildbot-master82']
 mixed_branches = ['mozilla-inbound', 'b2g-inbound']
 for b in mixed_branches:
     if b not in disabled_branches:
