0c1b7d8632f5aa0dce7287c8e6bba6294f4ac1b5
Aki Sasaki <asasaki@mozilla.com>
2014-03-18 12:39:52 -0700

    bug 984705 - turn off inari updates. r=rail

--- mozilla/b2g_config.py
+++ mozilla/b2g_config.py
@@ -592,7 +592,7 @@ PLATFORM_VARS = {
             'script_name': 'scripts/b2g_build.py',
             # b2g_build.py will checkout gecko from hg and look up a tooltool manifest given by the
             # --target name below
-            'extra_args': ['--target', 'inari', '--config', 'b2g/releng-private-updates.py',
+            'extra_args': ['--target', 'inari', '--config', 'b2g/releng-otoro.py',
                            '--gaia-languages-file', 'locales/languages_dev.json',
                            '--gecko-languages-file', 'gecko/b2g/locales/all-locales'],
             'reboot_command': ['bash', '-c', 'sudo reboot; sleep 600'],
