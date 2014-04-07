cf6e730f629a85b5998208f5af8efeebe8733938
Phil Ringnalda <philringnalda@gmail.com>
2014-04-07 07:12:27 -0700

    Bug 992690 - Increase maxtime for browser-chrome-2 to 7200 seconds, r=armenzg

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -503,7 +503,7 @@ MOCHITEST_BC_3 = [
         'script_path': 'scripts/desktop_unittest.py',
         'extra_args': ['--mochitest-suite', 'browser-chrome-2'],
         'blob_upload': True,
-        'script_maxtime': 6000,
+        'script_maxtime': 7200,
     }),
     ('mochitest-browser-chrome-3', {
         'use_mozharness': True,
