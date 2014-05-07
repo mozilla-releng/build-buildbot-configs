23d2c9de5dbafe680ff5a00e7d080254563ec02d
Rail Aliiev <rail@mozilla.com>
2014-05-07 13:38:09 -0400

    Bug 986477 - Don't require puppet or DNS to launch new instances. r=catlee

--- mozilla/production_config.py
+++ mozilla/production_config.py
@@ -9,7 +9,8 @@ WIN64_REV2     = ['w64-ix-slave%02i' % x for x in range(6,30) + range(42,44) + r
                  ['b-2008-sm-%04d' % x for x in range(1, 65)]
 MOCK_DL120G7   = ['bld-centos6-hp-%03d' % x for x in range(6,10) + range(12,14) + range(15,20)]
 LINUX64_EC2    = ['bld-linux64-ec2-%03d' % x for x in range(1, 50) + range(301, 350)] + \
-                 ['bld-linux64-spot-%03d' % x for x in range(1, 200) + range(300,500)]
+                 ['bld-linux64-spot-%03d' % x for x in range(1, 200) + range(300,500)] + \
+                 ['bld-linux64-spot-%d' % x for x in range(1000, 1100)]
 MOCK_IX        = ['bld-linux64-ix-%03d' % x for x in range(28, 38)] + \
                  ['b-linux64-ix-%04d' % x for x in range(1, 12)]
 
@@ -35,7 +36,8 @@ TRY_MAC64      = []
 TRY_WIN32_IXS  = []
 TRY_WIN64_IXS  = []
 TRY_LINUX64_EC2 = ['try-linux64-ec2-%03d' % x for x in range(1, 60) + range(301,340)] + \
-    ['try-linux64-spot-%03d' % x for x in range(1, 200) + range(300,500)]
+    ['try-linux64-spot-%03d' % x for x in range(1, 200) + range(300,500)] + \
+    ['try-linux64-spot-%d' % x for x in range(1000, 1100)]
 TRY_WIN64_REV2 = ['w64-ix-slave%02i' % x for x in [3] + range(32,75) if x not in [42,43]] + \
                  ['w64-ix-slave%03i' % x for x in [110] + range(159,171)] + \
                  ['b-2008-ix-%04i' % x for x in range(18, 65) + range(173,15)]
