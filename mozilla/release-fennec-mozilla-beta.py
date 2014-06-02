5de51f245155f6753829d48ea54c08e0a82d4dcd
ffxbld <none@none>
2014-06-02 16:20:46 -0700

    Update release config for Fennec-30.0b10-build1
    --HG--
    branch : production

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -22,11 +22,11 @@ releaseConfig['productName']         = 'fennec'
 releaseConfig['appName']             = 'mobile'
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '30.0b8'
+releaseConfig['version']             = '30.0b10'
 releaseConfig['appVersion']          = '30.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_30_0b8'
+releaseConfig['baseTag']             = 'FENNEC_30_0b10'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextMilestone']       = releaseConfig['milestone']
@@ -35,7 +35,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '91e40e5e4d26',
+        'revision': 'cf8a98b27b09',
         'relbranch': None,
         'bumpFiles': {
             'mobile/android/confvars.sh': {
