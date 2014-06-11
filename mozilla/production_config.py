22f5797c7e25e38a7dee7e2765f8cf03bbda2be4
Chris Cooper <ccooper@deadsquid.com>
2014-06-11 17:40:22 -0400

    Bug 1022431 - fix try host range for train C - r=Callek

--- mozilla/production_config.py
+++ mozilla/production_config.py
@@ -6,8 +6,7 @@ WIN64_IXS      = []
 WIN64_REV2     = ['w64-ix-slave%03i' % x for x in range(123,148) + range(156, 158)] + \
                  ['b-2008-ix-%04i' % x for x in range(1,18) + range(65,89) + range(90,137) + range(161,173)] + \
                  ['b-2008-sm-%04d' % x for x in range(33, 65)]
-MOCK_DL120G7   = ['b-linux64-hp-%04i' % x for x in range(13,17) + range(30,36)] +\
-                 ['b-linux64-hp-%04d' % x for x in range(25,30)]
+MOCK_DL120G7   = ['b-linux64-hp-%04d' % x for x in range(25,36)]
 LINUX64_EC2    = ['bld-linux64-ec2-%03d' % x for x in range(1, 50) + range(301, 350)] + \
                  ['bld-linux64-spot-%03d' % x for x in range(1, 200) + range(300,500)] + \
                  ['bld-linux64-spot-%d' % x for x in range(1000, 1100)]
@@ -40,8 +39,7 @@ TRY_LINUX64_EC2 = ['try-linux64-ec2-%03d' % x for x in range(1, 60) + range(301,
 TRY_WIN64_REV2 = ['w64-ix-slave%03i' % x for x in range(160,171)] + \
                  ['b-2008-ix-%04i' % x for x in range(18, 65) + range(30, 42) + range(173,182)] + \
                  ['b-2008-sm-%04d' % x for x in range(1, 33)]
-TRY_MOCK_DL120G7 = ['b-linux64-hp-%04d' % x for x in range(1, 13) + range(17, 20)] + \
-                   ['b-linux64-ix-%04d' % x for x in range(12, 14)]
+TRY_MOCK_DL120G7 = ['b-linux64-hp-%04d' % x for x in range(1, 20)]
 TRY_MOCK_IX      = ['bld-linux64-ix-%03d' % x for x in range(49,51)]
 TRY_LION         = ['bld-lion-r5-%03d' % x for x in range(16,40) + [87]]
 if set(TRY_WIN64_REV2).intersection(set(TRY_WIN64_IXS)):
