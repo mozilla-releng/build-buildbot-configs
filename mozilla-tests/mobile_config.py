560eadef78e0810f4a4770a9196619793acc4211
kmoir@mozilla.com <kmoir@mozilla.com>
2014-07-29 08:54:28 -0400

    Bug 1042835 - disable armv6 builds and tests for gecko < 32 r=Callek

--- mozilla-tests/mobile_config.py
+++ mozilla-tests/mobile_config.py
@@ -1627,7 +1627,22 @@ for name, branch in items_before(BRANCHES, 'gecko_version', 33):
             continue
         BRANCHES[name]['platforms']['android-armv6']['ubuntu64_vm_armv6_large'] = {
             'opt_unittest_suites': [],
-        }    
+        }
+        BRANCHES[name]['platforms']['android-armv6']['ubuntu64_vm_armv6_mobile'] = {
+            'opt_unittest_suites': [],
+        }
+
+#Bug 1042835 - disable armv6 builds and tests for gecko < 32
+for name, branch in items_at_least(BRANCHES, 'gecko_version', 32):
+    # Loop removes it from any branch that gets beyond here
+    for platform in branch['platforms']:
+        if not platform in PLATFORMS:
+            continue
+        if not platform == ('android-armv6'):
+            continue
+        BRANCHES[name]['platforms']['android-armv6']['ubuntu64_vm_armv6_large'] = {
+            'opt_unittest_suites': [],
+        }
         BRANCHES[name]['platforms']['android-armv6']['ubuntu64_vm_armv6_mobile'] = {
             'opt_unittest_suites': [],
         }
