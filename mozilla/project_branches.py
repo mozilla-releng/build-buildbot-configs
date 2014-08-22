648c942a7b4cb1130a5b41f25764d582588104d0
Massimo Gervasini <mgervasini@mozilla.com>
2014-08-22 10:10:26 +0200

    Bug 740142 - Enable nightlies on ash. r=catlee

--- mozilla/project_branches.py
+++ mozilla/project_branches.py
@@ -92,6 +92,7 @@ PROJECT_BRANCHES = {
     #},
     'ash': {
         'desktop_mozharness_repacks_enabled': True,
+        'enable_nightly': True,
         'mozharness_repo_path': 'build/ash-mozharness',
         'mozharness_repo': 'https://hg.mozilla.org/build/ash-mozharness',
         'mozharness_tag': 'default',
