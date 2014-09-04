77259abfb9e5f60bebb8a6eee5f0a926f3f16e9a
Phil Ringnalda <philringnalda@gmail.com>
2014-09-04 13:35:48 -0700

    Bug 106240 - Run b2g tests on jamun, r=catlee

--- mozilla-tests/b2g_config.py
+++ mozilla-tests/b2g_config.py
@@ -36,6 +36,7 @@ BRANCHES = {
     # 'birch': {},
     'cedar': {},
     'cypress': {},
+    'jamun': {},
     'pine': {},
     'fx-team': {},
     'graphics': {},
@@ -1688,6 +1689,7 @@ BRANCHES['pine']['platforms']['linux32_gecko']['ubuntu32_vm-b2gdt']['opt_unittes
 BRANCHES['cypress']['branch_name'] = "Cypress"
 BRANCHES['cypress']['repo_path'] = "projects/cypress"
 BRANCHES['cypress']['mozharness_tag'] = "default"
+BRANCHES['jamun']['repo_path'] = "projects/jamun"
 BRANCHES['fx-team']['repo_path'] = "integration/fx-team"
 BRANCHES['graphics']['repo_path'] = "projects/graphics"
 BRANCHES['mozilla-b2g28_v1_3t']['repo_path'] = "releases/mozilla-b2g28_v1_3t"
