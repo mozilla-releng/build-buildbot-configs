ead7c9b14465595b2fe3fa6000c94acbb98aec36
Jonathan Griffin <jgriffin@mozilla.com>
2014-10-10 11:59:29 -0700

    Bug 1067628 - Add Windows Mulet builds and nightlies to trunk branches, r=jlund

--- mozilla/b2g_config.py
+++ mozilla/b2g_config.py
@@ -811,6 +811,7 @@ PLATFORM_VARS = {
         'src_mozconfig': 'b2g/dev/config/mozconfigs/win32/mulet',
         'enable_dep': True,
         'profiled_build': False,
+        'enable_nightly': True,
         'builds_before_reboot': b2g_localconfig.BUILDS_BEFORE_REBOOT,
         'build_space': 13,
         'upload_symbols': False,
@@ -1794,13 +1795,16 @@ for name, branch in items_before(BRANCHES, 'gecko_version', 34):
     if 'linux64-mulet' in branch['platforms']:
         del branch['platforms']['linux64-mulet']
 
-# OSX and Win32 Mulet only on cedar for now (bug 1067628)
+# Enable win32 mulet in gecko 35+
+for name, branch in items_before(BRANCHES, 'gecko_version', 35):
+    if 'win32-mulet' in branch['platforms']:
+        del branch['platforms']['win32-mulet']
+
+# OSX Mulet only on cedar for now (bug 1067628)
 for branch in BRANCHES:
     if branch not in ('cedar',):
         if 'macosx64-mulet' in BRANCHES[branch]['platforms']:
             del BRANCHES[branch]['platforms']['macosx64-mulet']
-        if 'win32-mulet' in BRANCHES[branch]['platforms']:
-            del BRANCHES[branch]['platforms']['win32-mulet']
 
 # tarako is for B2G 1.3t only (gecko28)
 for branch in BRANCHES:
