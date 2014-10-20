cbd2610af169f8afad5c211d13e3a305a2025256
Chris Cooper <ccooper@deadsquid.com>
2014-10-20 11:16:04 -0400

    Bug 1068453 - Use linux-perf env for jetpack tests - r=jlund

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -1578,9 +1578,21 @@ PROJECTS = {
     'jetpack': {
         'branches': ['fx-team'],
         'platforms': {
-            'ubuntu64_vm': {'ext': 'linux-x86_64.tar.bz2', 'debug': True},
-            'ubuntu64-asan_vm': {'ext': 'linux-x86_64-asan.tar.bz2', 'debug': False},
-            'ubuntu32_vm': {'ext': 'linux-i686.tar.bz2', 'debug': True},
+            'ubuntu64_vm': {
+                'ext': 'linux-x86_64.tar.bz2',
+                'env': 'linux-perf',
+                'debug': True
+            },
+            'ubuntu64-asan_vm': {
+                'ext': 'linux-x86_64-asan.tar.bz2',
+                'env': 'linux-perf',
+                'debug': False
+            },
+            'ubuntu32_vm': {
+                'ext': 'linux-i686.tar.bz2',
+                'env': 'linux-perf',
+                'debug': True
+            },
             'snowleopard': {'ext': '(mac|mac64).dmg', 'debug': True},
             'mountainlion': {'ext': '(mac|mac64).dmg', 'debug': True},
             'xp-ix': {
