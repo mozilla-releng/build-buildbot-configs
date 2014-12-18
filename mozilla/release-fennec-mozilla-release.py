c415d60c04a809b76ffd22e83c8f6699d6ad66be
ffxbld <none@none>
2014-12-17 19:05:48 -0800

    Update release config for Fennec-34.0.1-build1
    --HG--
    branch : production

--- mozilla/release-fennec-mozilla-release.py
+++ mozilla/release-fennec-mozilla-release.py
@@ -23,11 +23,11 @@ releaseConfig['stage_product']       = 'mobile'
 releaseConfig['appName']             = 'mobile'
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '34.0'
-releaseConfig['appVersion']          = '34.0'
+releaseConfig['version']             = '34.0.1'
+releaseConfig['appVersion']          = '34.0.1'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 3
-releaseConfig['baseTag']             = 'FENNEC_34_0'
+releaseConfig['buildNumber']         = 1
+releaseConfig['baseTag']             = 'FENNEC_34_0_1'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['version']
 releaseConfig['nextMilestone']       = releaseConfig['version']
@@ -36,7 +36,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-release',
         'path': 'releases/mozilla-release',
-        'revision': '1433202c33b9',
+        'revision': '8e81c96d0749',
         'relbranch': None,
         'bumpFiles': {
             'mobile/android/confvars.sh': {
@@ -111,7 +111,7 @@ releaseConfig['mozconfigs']          = {
     'android': 'mobile/android/config/mozconfigs/android/release',
     'android-x86': 'mobile/android/config/mozconfigs/android-x86/release',
 }
-releaseConfig['releaseChannel']      = 'release'
+releaseConfig['releaseChannel']        = 'release'
 
 # Misc configuration
 releaseConfig['enable_repo_setup']       = False
@@ -157,11 +157,12 @@ releaseConfig['partialUpdates']      = {}
 releaseConfig['bouncerServer']       = 'download.mozilla.org'
 releaseConfig['localTestChannel']      = 'betatest'
 releaseConfig['cdnTestChannel']        = 'releasetest'
+releaseConfig['testChannelRuleIds']    = []
+releaseConfig['releaseChannelRuleIds'] = []
+
 releaseConfig['tuxedoServerUrl']     = 'https://bounceradmin.mozilla.com/api'
 releaseConfig['bouncer_submitter_config'] = 'releases/bouncer_fennec.py'
 releaseConfig['bouncerServer']       = 'download.mozilla.org'
-releaseConfig['testChannelRuleIds']    = []
-releaseConfig['releaseChannelRuleIds'] = []
 releaseConfig['bouncer_aliases'] = {
     'Fennec-%(version)s': 'fennec-latest',
 }
