f7f034e8510eb0277ad04fe778aec8164be26c45
Ben Hearsum <bhearsum@mozilla.com>
2015-02-23 09:50:31 -0500

    bug 1121160: Release automation support for split-apk builds - update beta release configs for split apks. r=jlund

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -67,14 +67,14 @@ releaseConfig['otherReposToTag']     = {
 }
 
 # Platform configuration
-releaseConfig['enUSPlatforms']        = ('android', 'android-x86')
+releaseConfig['enUSPlatforms']        = ('android-api-9', 'android-api-11', 'android-x86')
 releaseConfig['notifyPlatforms']      = releaseConfig['enUSPlatforms']
 releaseConfig['unittestPlatforms']    = ()
 releaseConfig['talosTestPlatforms']   = ()
 releaseConfig['enableUnittests']      = False
 
 # L10n configuration
-releaseConfig['l10nPlatforms']       = ('android',)
+releaseConfig['l10nPlatforms']       = ('android-api-9', 'android-api-11')
 releaseConfig['l10nNotifyPlatforms'] = releaseConfig['l10nPlatforms']
 releaseConfig['mergeLocales']        = True
 releaseConfig['enableMultiLocale']   = True
@@ -98,7 +98,8 @@ releaseConfig['partnerRepackPlatforms'] = ()
 
 # mozconfigs
 releaseConfig['mozconfigs']          = {
-    'android': 'mobile/android/config/mozconfigs/android/release',
+    'android-api-9': 'mobile/android/config/mozconfigs/android-api-9-10-constrained/release',
+    'android-api-11': 'mobile/android/config/mozconfigs/android-api-11/release',
     'android-x86': 'mobile/android/config/mozconfigs/android-x86/release',
 }
 releaseConfig['releaseChannel']      = 'beta'
@@ -115,16 +116,23 @@ releaseConfig['enableUpdatePackaging']    = False
 releaseConfig['balrog_api_root']          = None
 
 releaseConfig['single_locale_options'] = {
-    'android': [
+    'android-api-9': [
         '--cfg',
-        'single_locale/release_mozilla-beta_android.py',
+        'single_locale/release_mozilla-beta_android_api_9.py',
+        '--tag-override', '%s_RELEASE' % releaseConfig['baseTag'],
+    ],
+    'android-api-11': [
+        '--cfg',
+        'single_locale/release_mozilla-beta_android_api_11.py',
         '--tag-override', '%s_RELEASE' % releaseConfig['baseTag'],
     ],
 }
 
 releaseConfig['multilocale_config'] = {
     'platforms': {
-        'android':
+        'android-api-9':
+            'multi_locale/release_mozilla-beta_android.json',
+        'android-api-11':
             'multi_locale/release_mozilla-beta_android.json',
         'android-x86':
             'multi_locale/release_mozilla-beta_android-x86.json',
@@ -141,7 +149,7 @@ releaseConfig['enableSigningAtBuildTime'] = True
 releaseConfig['enablePartialMarsAtBuildTime'] = False
 releaseConfig['autoGenerateChecksums'] = False
 releaseConfig['use_mock'] = True
-releaseConfig['mock_platforms'] = ('android', 'android-x86', 'linux')
+releaseConfig['mock_platforms'] = ('android-api-9', 'android-api-11', 'android-x86', 'linux')
 releaseConfig['ftpSymlinkName'] = 'latest-beta'
 releaseConfig['enableAutomaticPushToMirrors'] = True
 releaseConfig['localTestChannel']      = 'beta-localtest'
