87346226dba067ef0cf4a5c90260550894b4e1c1
ffxbld <none@none>
2015-03-09 18:51:16 -0700

    Update release config for Firefox-37.0b4-build1

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -23,23 +23,23 @@ releaseConfig['productName']         = 'firefox'
 releaseConfig['stage_product']       = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '37.0b3'
+releaseConfig['version']             = '37.0b4'
 releaseConfig['appVersion']          = '37.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_37_0b3'
+releaseConfig['baseTag']             = 'FIREFOX_37_0b4'
 releaseConfig['partialUpdates']      = {
-
-    '37.0b1': {
-        'appVersion': '37.0',
-        'buildNumber': 1,
-        'baseTag': 'FIREFOX_37_0b1',
-    },
 
     '37.0b2': {
         'appVersion': '37.0',
         'buildNumber': 1,
         'baseTag': 'FIREFOX_37_0b2',
+    },
+
+    '37.0b3': {
+        'appVersion': '37.0',
+        'buildNumber': 1,
+        'baseTag': 'FIREFOX_37_0b3',
     },
 
 }
@@ -54,7 +54,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '00bad6e2ffbc',
+        'revision': '89b593b91e5e',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
