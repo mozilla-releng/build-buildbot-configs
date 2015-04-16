bde0839a85e74af43d349e10f793a3cd383f63e7
ffxbld <none@none>
2015-04-16 14:04:50 -0700

    Update release config for Firefox-38.0b5-build1

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -21,11 +21,11 @@ releaseConfig['productName']         = 'firefox'
 releaseConfig['stage_product']       = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '38.0b4'
+releaseConfig['version']             = '38.0b5'
 releaseConfig['appVersion']          = '38.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_38_0b4'
+releaseConfig['baseTag']             = 'FIREFOX_38_0b5'
 releaseConfig['partialUpdates']      = {
 
     '38.0b3': {
@@ -40,10 +40,10 @@ releaseConfig['partialUpdates']      = {
         'baseTag': 'FIREFOX_38_0b2',
     },
 
-    '38.0b1': {
+    '38.0b4': {
         'appVersion': '38.0',
         'buildNumber': 1,
-        'baseTag': 'FIREFOX_38_0b1',
+        'baseTag': 'FIREFOX_38_0b4',
     },
 
 }
@@ -58,7 +58,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '05508ccf3ae8',
+        'revision': '45a5eaa7813b',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
