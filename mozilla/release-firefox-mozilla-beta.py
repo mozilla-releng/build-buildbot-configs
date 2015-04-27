8a3b17ee563e19b37212af5be725d7cca1f94bbf
ffxbld <none@none>
2015-04-26 17:16:20 -0700

    Update release config for Firefox-38.0b8-build1

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -21,11 +21,11 @@ releaseConfig['productName']         = 'firefox'
 releaseConfig['stage_product']       = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '38.0b7'
+releaseConfig['version']             = '38.0b8'
 releaseConfig['appVersion']          = '38.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 2
-releaseConfig['baseTag']             = 'FIREFOX_38_0b7'
+releaseConfig['buildNumber']         = 1
+releaseConfig['baseTag']             = 'FIREFOX_38_0b8'
 releaseConfig['partialUpdates']      = {
 
     '38.0b6': {
@@ -40,12 +40,6 @@ releaseConfig['partialUpdates']      = {
         'baseTag': 'FIREFOX_38_0b5',
     },
 
-    '38.0b4': {
-        'appVersion': '38.0',
-        'buildNumber': 1,
-        'baseTag': 'FIREFOX_38_0b4',
-    },
-
 }
 # win64 support
 releaseConfig['HACK_first_released_version'] = {'win64': '37.0b2'}
@@ -58,7 +52,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-release',
-        'revision': '8a2c933394da',
+        'revision': '8fc6195511e5',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
