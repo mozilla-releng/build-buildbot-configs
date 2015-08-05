1bd1466caac83977b0da44a0209d2aca23fee6b2
Chris Cooper <ccooper@deadsquid.com>
2015-08-05 11:44:26 -0400

    Bug 1191005 - enable desktop nightlies for larch - r=kmoir

--- mozilla/project_branches.py
+++ mozilla/project_branches.py
@@ -233,8 +233,12 @@ PROJECT_BRANCHES = {
         }
     },
     'larch': {
+        'enable_nightly': True,
         'lock_platforms': True,
         'platforms': {
+            'linux64': {},
+            'macosx64': {},
+            'win64': {},
         },
     },
     'maple': {
