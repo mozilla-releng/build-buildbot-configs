7f8805e20c5a7c43f27282c063cee03093c7af31
Ryan VanderMeulen <ryanvm@gmail.com>
2015-08-17 17:09:51 -0400

    Bug 1156421 - Split mochitest-dt into 4 chunks on ASAN. r=bhearsum
    --HG--
    extra : rebase_source : 2a9310ff5528309b47ac67f3f3ad5cb608cf10ba

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -2131,6 +2131,18 @@ for platform in PLATFORMS.keys():
                     debug_suites = [x for x in debug_suites if x[0] and x[0] != 'mochitest-devtools-chrome']
                     BRANCHES[name]['platforms'][platform][slave_platform]['debug_unittest_suites'] = debug_suites + MOCHITEST_DT_8[:]
 
+# Bug 1156421 - Use more chunks for mochitest-dt on ASAN
+for platform in PLATFORMS.keys():
+    for name, branch in items_at_least(BRANCHES, 'gecko_version', 40):
+        for slave_platform in PLATFORMS[platform]['slave_platforms']:
+            if slave_platform not in ('ubuntu64-asan_vm',):
+                continue
+            if platform in BRANCHES[name]['platforms']:
+                if slave_platform in BRANCHES[name]['platforms'][platform]:
+                    opt_suites = BRANCHES[name]['platforms'][platform][slave_platform]['opt_unittest_suites']
+                    opt_suites = [x for x in opt_suites if x[0] and x[0] != 'mochitest-devtools-chrome']
+                    BRANCHES[name]['platforms'][platform][slave_platform]['opt_unittest_suites'] = opt_suites + MOCHITEST_DT_4[:]
+
 # Bug 1156357 - Enable mochitest-push to ride the trains
 for platform in PLATFORMS.keys():
     for name, branch in items_at_least(BRANCHES, 'gecko_version', 40):
