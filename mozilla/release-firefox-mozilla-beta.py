6fdfe5d2f918241badac3ffef497880fe96a7e96
ffxbld <none@none>
2015-11-02 02:34:15 -0800

    Update release config for Firefox-43.0b1-build1
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -21,18 +21,12 @@ releaseConfig['productName']         = 'firefox'
 releaseConfig['stage_product']       = 'firefox'
 releaseConfig['appName']             = 'browser'
 #  Current version info
-releaseConfig['version']             = '42.0b9'
-releaseConfig['appVersion']          = '42.0'
+releaseConfig['version']             = '43.0b1'
+releaseConfig['appVersion']          = '43.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FIREFOX_42_0b9'
+releaseConfig['baseTag']             = 'FIREFOX_43_0b1'
 releaseConfig['partialUpdates']      = {
-
-    '42.0b6': {
-        'appVersion': '42.0',
-        'buildNumber': 1,
-        'baseTag': 'FIREFOX_42_0b6',
-    },
 
     '42.0b7': {
         'appVersion': '42.0',
@@ -44,6 +38,12 @@ releaseConfig['partialUpdates']      = {
         'appVersion': '42.0',
         'buildNumber': 1,
         'baseTag': 'FIREFOX_42_0b8',
+    },
+
+    '42.0b9': {
+        'appVersion': '42.0',
+        'buildNumber': 1,
+        'baseTag': 'FIREFOX_42_0b9',
     },
 
 }
@@ -61,7 +61,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': '894bc1fcf2a9',
+        'revision': '25cea6767faa',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
