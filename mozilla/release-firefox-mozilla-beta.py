188d0d25e085539d3f7ac23f019a5497b8ffdb78
ffxbld <none@none>
2015-11-12 14:29:08 -0800

    Update release config for Firefox-43.0b3-build2
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-beta.py
+++ mozilla/release-firefox-mozilla-beta.py
@@ -23,7 +23,7 @@ releaseConfig['appName']             = 'browser'
 releaseConfig['version']             = '43.0b3'
 releaseConfig['appVersion']          = '43.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 1
+releaseConfig['buildNumber']         = 2
 releaseConfig['baseTag']             = 'FIREFOX_43_0b3'
 releaseConfig['partialUpdates']      = {
 
@@ -60,7 +60,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': 'e4629e309487',
+        'revision': '51f0f13e7985',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
