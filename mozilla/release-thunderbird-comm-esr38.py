e126629048a140cbd822ba4dc5be80ae33745c3b
ffxbld <none@none>
2015-11-12 23:34:44 -0800

    Update release config for Thunderbird-38.4.0-build2
    --HG--
    branch : production

--- mozilla/release-thunderbird-comm-esr38.py
+++ mozilla/release-thunderbird-comm-esr38.py
@@ -26,7 +26,7 @@ releaseConfig['mozilla_srcdir']      = 'mozilla'
 releaseConfig['version']             = '38.4.0'
 releaseConfig['appVersion']          = '38.4.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 1
+releaseConfig['buildNumber']         = 2
 releaseConfig['baseTag']             = 'THUNDERBIRD_38_4_0'
 releaseConfig['partialUpdates']      = {
 
@@ -57,7 +57,7 @@ releaseConfig['sourceRepositories']  = {
     'comm': {
         'name': 'comm-esr38',
         'path': 'releases/comm-esr38',
-        'revision': 'aa6319b6a6a6',
+        'revision': '502ef538ce9b',
         'relbranch': None,
         'bumpFiles': {
             'mail/config/version.txt': {
@@ -69,7 +69,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-esr38',
         'path': 'releases/mozilla-esr38',
-        'revision': 'bea414cb5af0',
+        'revision': '3c13c7dca621',
         'relbranch': None,
         'bumpFiles': {
             'config/milestone.txt': {
