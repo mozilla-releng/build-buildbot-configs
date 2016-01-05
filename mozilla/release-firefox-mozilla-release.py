9366c1abc0a08d71814fcf873041401d755da44d
ffxbld <none@none>
2016-01-05 12:19:49 -0800

    Update release config for Firefox-43.0.4-build2
    --HG--
    branch : production

--- mozilla/release-firefox-mozilla-release.py
+++ mozilla/release-firefox-mozilla-release.py
@@ -23,7 +23,7 @@ releaseConfig['appName']             = 'browser'
 releaseConfig['version']             = '43.0.4'
 releaseConfig['appVersion']          = '43.0.4'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 1
+releaseConfig['buildNumber']         = 2
 releaseConfig['baseTag']             = 'FIREFOX_43_0_4'
 releaseConfig['partialUpdates']      = {
 
@@ -59,7 +59,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-release',
         'path': 'releases/mozilla-release',
-        'revision': '413d833d300a',
+        'revision': 'd83046b974f3',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
