409fd5ef8088cd698c0e9940c5362549d2607fa9
Vlad Ciobancai <vlad.ciobancai@softvision.ro>
2016-01-06 09:54:21 -0800

    Bug 1232780 - Schedule e10s mochitest-plain on win7-opt, r=jgriffin

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -2460,6 +2460,7 @@ for name, branch in items_at_least(BRANCHES, 'gecko_version', 44):
 # Bug 1215233 - Enable more e10s tests on Windows 7 only
 #   Turn on mochitest-gl-e10s - bug 1221102
 #   Turn on mochitest-e10s-devtools-chrome - bug 1221499
+#   Turn on mochitest-e10s - bug 1232780
 for name, branch in items_at_least(BRANCHES, 'gecko_version', trunk_gecko_version):
     for platform in PLATFORMS.keys():
         if platform not in branch['platforms']:
@@ -2470,7 +2471,7 @@ for name, branch in items_at_least(BRANCHES, 'gecko_version', trunk_gecko_versio
                     branch['platforms'][platform][slave_platform]['debug_unittest_suites'] += \
                         MOCHITEST_WEBGL_E10S + MOCHITEST_DT_8_E10S
                     branch['platforms'][platform][slave_platform]['opt_unittest_suites'] += \
-                        MOCHITEST_WEBGL_E10S + MOCHITEST_DT_8_E10S
+                        MOCHITEST_WEBGL_E10S + MOCHITEST_DT_8_E10S + MOCHITEST_E10S
 
 # Bug 1200437
 # Use 7 chunks for m-bc on branches > trunk, excluding twigs, 3 chunks elsewhere
