46d55e03ba8fea9f3217f5d824317948600cff82
Alin Selagea <alin.selagea@softvision.ro>
2016-03-14 16:30:25 +0200

    Bug 1255812 - Move 10 XP test machines to the Windows 8 pool r=kmoir

--- mozilla-tests/production_config.py
+++ mozilla-tests/production_config.py
@@ -16,7 +16,7 @@ SLAVES = {
     'win64_vm': {},
 }
 
-for i in range(1, 133):  # XP slaves from 153 to 173 moved to Win 7 pool // Bug 1226729 // Bug 1239785
+for i in range(1, 123):  # XP slaves from 123 onwards moved to Win7 and Win8 pools // Bug 1226729 // Bug 1239785 // Bug 1255812
     SLAVES['xp-ix']['t-xp32-ix-%03i' % i] = {}
 
 for i in range(1, 282):   # Move some XP test machines to the Windows 7 pool from 234 to 253 // Bug 1226729 // Bug 1239785 // Bug 1252258
@@ -26,7 +26,7 @@ for i in range(1, 282):   # Move some XP test machines to the Windows 7 pool fro
 for i in range(1, 400):
     SLAVES['win7-all']['t-w732-spot-%03i' % i] = {}
 
-for i in range(1, 102) + range(103, 195):  # Omit 102 for win10 // Bug 1191481
+for i in range(1, 102) + range(103, 205):  # Omit 102 for win10 // Bug 1191481 // Bug 1255812
     SLAVES['win8']['t-w864-ix-%03i' % i] = {}
 
 for i in range(1, 3) + range(11, 12):  # Bug 1252258
