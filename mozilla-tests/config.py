a18c75df29b1d27da3817ba6f541e2556cd3b985
Joel Maher <jmaher@mozilla.com>
2016-04-12 16:00:43 -0400

    Bug 1253341 - Run duplicate Talos jobs in AWS for Linux (large instances). r?rail
    MozReview-Commit-ID: 3yvez20rMaE

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -3158,15 +3158,15 @@ for branch in set(BRANCHES.keys()) - set(win7_vm_inactive_branches):
             del BRANCHES[branch]['platforms'][platform]['win7-ix']
 
 # Bug 1253341 - run talos jobs on AWS
-for branch in ():
-    BRANCHES[branch]['platforms']['linux64']['talos_slave_platforms'] = ['ubuntu64_hw', 'ubuntu64_vm']
+for branch in ('try',):
+    BRANCHES[branch]['platforms']['linux64']['talos_slave_platforms'] = ['ubuntu64_hw', 'ubuntu64_vm_large']
     # Add ubuntu64_vm to the talos suites
     for test, test_config in BRANCHES[branch].items():
         if not test.endswith('_tests'):
             continue
         tests, merge, extra, platforms = test_config
-        if 'ubuntu64_hw' in platforms and 'ubuntu64_vm' not in platforms:
-            platforms = platforms + ['ubuntu64_vm']
+        if 'ubuntu64_hw' in platforms and 'ubuntu64_vm_large' not in platforms:
+            platforms = platforms + ['ubuntu64_vm_large']
             BRANCHES[branch][test] = (tests, merge, extra, platforms)
 
 if __name__ == "__main__":
