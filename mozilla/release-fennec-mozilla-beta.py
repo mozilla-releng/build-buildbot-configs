3a4403c5881870f7b0dc4f87b773d31d33538630
Nick Thomas <nthomas@mozilla.com>
2016-08-03 12:03:02 +1200

    Backed out changeset ba3b5f33ccee
    --HG--
    branch : production

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -20,11 +20,11 @@ releaseConfig['stage_product']       = 'mobile'
 releaseConfig['appName']             = 'mobile'
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '49.0b1'
-releaseConfig['appVersion']          = '49.0'
+releaseConfig['version']             = '48.0b11'
+releaseConfig['appVersion']          = '48.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_49_0b1'
+releaseConfig['baseTag']             = 'FENNEC_48_0b11'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextVersion']         = releaseConfig['version']
@@ -34,7 +34,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': 'f21e48559ecd',
+        'revision': '42a797ef3647',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version_display.txt': {
