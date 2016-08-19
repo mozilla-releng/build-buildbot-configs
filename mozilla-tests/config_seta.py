e7f9cfa304b31eeff548cbb5c48ad06e0ebc15c2
kmoir@mozilla.com <kmoir@mozilla.com>
2016-08-19 13:10:11 -0400

    Bug 1286550 - consume windows 7 vm data in seta bbconfigs  r=me

--- mozilla-tests/config_seta.py
+++ mozilla-tests/config_seta.py
@@ -32,7 +32,7 @@ seta_platforms = {"Rev4 MacOSX Snow Leopard 10.6": ("macosx64", ["snowleopard"])
                   }
 
 # platforms and tests to exclude from configs because they are deprecated or lacking data
-platform_exclusions = ["android-2-3-armv7-api9", "Android 4.2 x86 Emulator", "Windows 7 VM 32-bit", "Windows 7 VM-GFX 32-bit", "Android 4.3 armv7 API 15+"]
+platform_exclusions = ["android-2-3-armv7-api9", "Android 4.2 x86 Emulator", "Android 4.3 armv7 API 15+"]
 test_exclusions = re.compile('\[funsize\]|\[TC\]')
 
 # define seta branches and default values for skipcount and skiptimeout
