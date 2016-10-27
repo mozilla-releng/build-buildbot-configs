4ed7990ce4a216e6bbf0625c27256cdc6e7fd5ea
Alin Selagea <aselagea@mozilla.com>
2016-10-27 10:40:08 +0300

    Bug 1308544 - Enable automation jobs on Cedar twig, r=kmoir

--- mozilla/project_branches.py
+++ mozilla/project_branches.py
@@ -73,7 +73,27 @@ PROJECT_BRANCHES = {
         'branch_projects': ['spidermonkey_tier_1'],
     },
     #'birch': {},  # Bug 1010674
-    #'cedar': {},  # Bug 1272005
+# Bug 1308544 - Enable automation jobs on Cedar twig
+    'cedar': {
+        'enable_perproduct_builds': False,
+        'lock_platforms': True,
+        'mozharness_tag': 'default',
+        'enable_talos': True,
+        'talos_suites': {
+            'other': 1,
+            'svgr': 1,
+            'tp5o': 1,
+            'other-e10s': 1,
+            'svgr-e10s': 1,
+            'tp5o-e10s': 1,
+        },
+        'enable_opt_unittests': True,
+        'platforms': {
+            'linux64': {},
+            'macosx64': {},
+            'win64': {},
+        },
+    },
     'cypress': {
         'lock_platforms': True,
         'platforms': {
