7d578b357fc8ff9951e1ea50a1617648823c9cab
ffxbld <none@none>
2016-11-23 22:24:12 -0800

    Update release config for Fennec-50.0.1-build1
    --HG--
    branch : production

--- mozilla/release-fennec-mozilla-release.py
+++ mozilla/release-fennec-mozilla-release.py
@@ -20,11 +20,11 @@ releaseConfig['stage_product']       = 'mobile'
 releaseConfig['appName']             = 'mobile'
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '50.0'
-releaseConfig['appVersion']          = '50.0'
+releaseConfig['version']             = '50.0.1'
+releaseConfig['appVersion']          = '50.0.1'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_50_0'
+releaseConfig['baseTag']             = 'FENNEC_50_0_1'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['version']
 releaseConfig['nextMilestone']       = releaseConfig['version']
@@ -33,7 +33,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-release',
         'path': 'releases/mozilla-release',
-        'revision': 'dc617d65c9f0',
+        'revision': 'd936940d5476',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version.txt': {
