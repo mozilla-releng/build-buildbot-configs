93e8287757e4bb43e509497922dfb6fc0bac8c01
ffxbld <none@none>
2017-01-16 18:24:33 -0800

    Update release config for Fennec-51.0b15-build1

--- mozilla/release-fennec-mozilla-beta.py
+++ mozilla/release-fennec-mozilla-beta.py
@@ -20,11 +20,11 @@ releaseConfig['stage_product']       = 'mobile'
 releaseConfig['appName']             = 'mobile'
 releaseConfig['relbranchPrefix']     = 'MOBILE'
 #  Current version info
-releaseConfig['version']             = '51.0b13'
+releaseConfig['version']             = '51.0b15'
 releaseConfig['appVersion']          = '51.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
 releaseConfig['buildNumber']         = 1
-releaseConfig['baseTag']             = 'FENNEC_51_0b13'
+releaseConfig['baseTag']             = 'FENNEC_51_0b15'
 #  Next (nightly) version info
 releaseConfig['nextAppVersion']      = releaseConfig['appVersion']
 releaseConfig['nextVersion']         = releaseConfig['version']
@@ -34,7 +34,7 @@ releaseConfig['sourceRepositories']  = {
     'mobile': {
         'name': 'mozilla-beta',
         'path': 'releases/mozilla-beta',
-        'revision': 'ce55e4d27603',
+        'revision': '4d23adbd1004',
         'relbranch': None,
         'bumpFiles': {
             'browser/config/version_display.txt': {
