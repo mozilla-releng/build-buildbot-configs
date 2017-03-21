d886521c5624fe764b7b0e59f0c920db3fb5dfd3
ffxbld <none@none>
2017-03-21 16:00:26 -0700

    Update release config for Thunderbird-52.0-build4
    --HG--
    branch : production

--- mozilla/release-thunderbird-comm-esr52.py
+++ mozilla/release-thunderbird-comm-esr52.py
@@ -26,7 +26,7 @@ releaseConfig['mozilla_srcdir']      = 'mozilla'
 releaseConfig['version']             = '52.0'
 releaseConfig['appVersion']          = '52.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 3
+releaseConfig['buildNumber']         = 4
 releaseConfig['baseTag']             = 'THUNDERBIRD_52_0'
 releaseConfig['partialUpdates']      = {
 
@@ -57,7 +57,7 @@ releaseConfig['sourceRepositories']  = {
     'comm': {
         'name': 'comm-esr52',
         'path': 'releases/comm-esr52',
-        'revision': '3a668cd51d27',
+        'revision': '528196659245',
         'relbranch': None,
         'bumpFiles': {
             'mail/config/version.txt': {
