f8ec93103ef979d5fd9f519cb9a8ddbe043bacab
Alin Selagea <aselagea@mozilla.com>
2017-03-23 12:49:27 +0200

    Bug 1309812 - Windows 7 VM reftest jobs are not running by default if 'Windows 7' is not explicitly specified, r=aselagea (patch by spacurar)

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -108,7 +108,7 @@ PLATFORMS['win32']['xp_ix'] = {'name': "Windows XP 32-bit",
 PLATFORMS['win32']['win7_ix'] = {'name': "Windows 7 32-bit"}
 PLATFORMS['win32']['win7_vm'] = {'name': "Windows 7 VM 32-bit"}
 PLATFORMS['win32']['win7_vm_gfx'] = {'name': "Windows 7 VM-GFX 32-bit",
-                                     'try_by_default': False}
+                                     'try_by_default': True}
 PLATFORMS['win32']['stage_product'] = 'firefox'
 PLATFORMS['win32']['mozharness_config'] = {
     'mozharness_python': ['c:/mozilla-build/python27/python', '-u'],
