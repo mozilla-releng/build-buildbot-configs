25e19a4ec9b98c48fac85cb19169aa33e622f446
ffxbld <none@none>
2017-04-28 15:06:36 -0700

    Update release config for Thunderbird-52.1.0-build2
    --HG--
    branch : production

--- mozilla/release-thunderbird-comm-esr52.py
+++ mozilla/release-thunderbird-comm-esr52.py
@@ -26,7 +26,7 @@ releaseConfig['mozilla_srcdir']      = 'mozilla'
 releaseConfig['version']             = '52.1.0'
 releaseConfig['appVersion']          = '52.1.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 1
+releaseConfig['buildNumber']         = 2
 releaseConfig['baseTag']             = 'THUNDERBIRD_52_1_0'
 releaseConfig['partialUpdates']      = {
 
@@ -67,7 +67,7 @@ releaseConfig['sourceRepositories']  = {
     'mozilla': {
         'name': 'mozilla-esr52',
         'path': 'releases/mozilla-esr52',
-        'revision': '3ea0e0752031',
+        'revision': '894ba3787a8f',
         'relbranch': None,
         'bumpFiles': {
             'config/milestone.txt': {
