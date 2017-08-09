542ed3bbb3d1e4877abda9c5d98956ef2487fbaa
Justin Wood <Callek@gmail.com>
2017-08-09 14:57:14 -0400

    Fix up my bad hg stuff
    MozReview-Commit-ID: GWabdPSGVZq

--- mozilla-tests/config.py
+++ mozilla-tests/config.py
@@ -314,23 +314,13 @@ SUITES = {
         'suites': GRAPH_CONFIG + ['--activeTests', 'bloom_basic:bloom_basic_ref', '--filter', 'ignore_first:5', '--filter', 'median'],
         'options': ({}, ALL_TALOS_PLATFORMS),
     },
-    'quantum-pageload-e10s': {
-        'enable_by_default': True,
-        'suites': GRAPH_CONFIG + ['--activeTests', 'Quantum_1', '--filter', 'ignore_first:5', '--filter', 'median'],
-        'options': ({}, WIN_ONLY),
-    },
-    'quantum-pageload-stylo-e10s': {
-        'enable_by_default': True,
-        'suites': GRAPH_CONFIG + ['--activeTests', 'Quantum_1', '--filter', 'ignore_first:5', '--filter', 'median', '--stylo'],
-        'options': ({}, WIN_ONLY),
-    },
     'tp6-e10s': {
-        'enable_by_default': False,
+        'enable_by_default': True,
         'suites': GRAPH_CONFIG + ['--activeTests', 'Quantum_1', '--filter', 'ignore_first:5', '--filter', 'median'],
         'options': ({}, WIN_ONLY),
     },
     'tp6-stylo-e10s': {
-        'enable_by_default': False,
+        'enable_by_default': True,
         'suites': GRAPH_CONFIG + ['--activeTests', 'Quantum_1', '--filter', 'ignore_first:5', '--filter', 'median', '--stylo'],
         'options': ({}, WIN_ONLY),
     },
@@ -340,7 +330,7 @@ SUITES = {
         'options': ({}, WIN_ONLY),
     },
     'perf-reftest-singletons-e10s': {
-        'enable_by_default': False,
+        'enable_by_default': True,
         'suites': GRAPH_CONFIG + ['--activeTests', 'bloom_basic_singleton', '--filter', 'ignore_first:5', '--filter', 'median'],
         'options': ({}, ALL_TALOS_PLATFORMS),
     },
@@ -2546,18 +2536,6 @@ for platform in PLATFORMS.keys():
                         BRANCHES[name]['platforms'][platform][slave_platform]['debug_unittest_suites'] += \
                             MOCHITEST_GPU + MOCHITEST_CLIPBOARD
 
-# Enable talos perf-reftest-singeltons on 56+
-for name, branch in items_at_least(BRANCHES, 'gecko_version', 56):
-    if branch.get('enable_talos') is False:
-        continue
-    branch['perf-reftest-singletons-e10s_tests'] = (1, False, {}, ALL_TALOS_PLATFORMS)
-    branch['quantum-pageload-stylo-e10s_tests'] = (1, False, {}, WIN_ONLY)
-    branch['tp6-stylo-threads-e10s_tests'] = (1, False, {}, WIN_ONLY)
-
-    if name in ['try']:
-        branch['tp6-stylo-e10s_tests'] = (1, False, {}, WIN_ONLY)
-        branch['tp6-e10s_tests'] = (1, False, {}, WIN_ONLY)
-
 ### Test suites that only run on Try ###
 
 ride_trains_branches = []
@@ -3047,14 +3025,13 @@ for name, branch in items_at_least(BRANCHES, 'gecko_version', 56):
             branch['platforms'][platform][slave_platform]['debug_unittest_suites'] = tests
 
 for name, branch in items_at_least(BRANCHES, 'gecko_version', 57):
+    if platform not in PLATFORMS:
+        continue
     for platform in branch['platforms']:
-        if platform not in PLATFORMS:
-            continue
         for slave_platform in PLATFORMS[platform]['slave_platforms']:
             if slave_platform not in branch['platforms'][platform]:
                 continue
-            for test_type in ['opt_unittest_suites', 'debug_unittest_suites']:
-                for idx, test in enumerate(branch['platforms'][platform][slave_platform][test_type]):
+            for idx, test in enumerate(branch['platforms'][platform][slave_platform][test_type]):
                     if "mochitest-clipboard" not in test[0]:
                         continue
                     test = deepcopy(test)
