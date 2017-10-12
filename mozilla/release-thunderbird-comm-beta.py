6ca9450c03320bad4f57f9d1ad397273e19f2bd2
ffxbld <none@none>
2017-10-12 12:19:20 -0700

    Update release config for Thunderbird-56.0b4-build12
    --HG--
    branch : production

--- mozilla/release-thunderbird-comm-beta.py
+++ mozilla/release-thunderbird-comm-beta.py
@@ -25,7 +25,7 @@ releaseConfig['mozilla_srcdir']      = 'mozilla'
 releaseConfig['version']             = '56.0b4'
 releaseConfig['appVersion']          = '56.0'
 releaseConfig['milestone']           = releaseConfig['appVersion']
-releaseConfig['buildNumber']         = 11
+releaseConfig['buildNumber']         = 12
 releaseConfig['baseTag']             = 'THUNDERBIRD_56_0b4'
 releaseConfig['partialUpdates']      = {
 
